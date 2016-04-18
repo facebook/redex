b8f2d7bc349aa5d0bebfda95ecade576d6cb408b
Satnam Singh <satnam@raintown.org>
2016-04-18 11:31:25 -0700

    Redex synth example

---
+++ docs/examples/Synth/synth-example/src/main/java/com/facebook/redex/examples/synth/MainActivity.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2016-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+package com.facebook.redex.examples.synth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textView = (TextView) findViewById(R.id.message);
+        textView.setText("Redex Synth Example\n");
+
+        Alpha a = new Alpha(12);
+        Alpha.Beta b = a.new Beta();
+        textView.append("Double Alpha(12) = " + b.doubleAlpha() + "\n");
+    }
+}
