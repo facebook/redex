7b92602c8627613c7aa1b78d2db64a06788c6ebe
Satnam Singh <satnamsingh@fb.com>
2016-05-27 13:24:56 -0700

    Re-express unit/integ test using match library for synth
    Summary: Add a match library base test for synth
    
    Reviewed By: newobj
    
    Differential Revision: D3349723
    
    fbshipit-source-id: 98e9f29b91955200b76645bf6970a88ddba9d885

--- test/integ/SynthTest.cpp
+++ test/integ/SynthTest.cpp
@@ -8,10 +8,10 @@
  */
 
 #include <cstdint>
-#include <iostream>
 #include <cstdlib>
-#include <memory>
 #include <gtest/gtest.h>
+#include <iostream>
+#include <memory>
 #include <string>
 #include <unistd.h>
 
@@ -26,30 +26,44 @@
 #include "ReBindRefs.h"
 #include "Synth.h"
 
+#include "Match.h"
+
 // NOTE: this is not really a unit test.
 
 /*
-To understand this test one needs to also look at the file
-Java source file Alpha.java in the same directory.
-This Java source file compiled and a corresponding Dex file
-is created which is an input to this test. This test runs
-the prelimninary ReBindRefsPass pass and then the SynthPass
-which is the subject of this test.
+ To understand this test one needs to also look at the file
+ Java source file Alpha.java in the same directory.
+ This Java source file compiled and a corresponding Dex file
+ is created which is an input to this test. This test runs
+ the prelimninary ReBindRefsPass pass and then the SynthPass
+ which is the subject of this test.
 
-The Alpha class has an inner class Beta and there is an access
-inside Beta to a static field of Alpha which induces a synthetic
-wrapper. This test makes sure this wrapper method is removed.
+ The Alpha class has an inner class Beta and there is an access
+ inside Beta to a static field of Alpha which induces a synthetic
+ wrapper. This test makes sure this wrapper method is removed.
 
-The Gamma class has an inner class Delta which has a non-concrete
-access to a field that is declared elesewhere. This test checks
-to make sure we do not optimize such synthetic getters.
+ The Gamma class has an inner class Delta which has a non-concrete
+ access to a field that is declared elesewhere. This test checks
+ to make sure we do not optimize such synthetic getters.
 
-*/
+ */
+
+template <typename P>
+bool assert_classes(const DexClasses& classes,
+                    const m::match_t<DexClass, P>& p) {
+  for (const auto& cls : classes) {
+    if (p.matches(cls)) {
+      return true;
+    }
+  }
+  return false;
+}
 
 TEST(SynthTest1, synthetic) {
   g_redex = new RedexContext();
 
-  const char* dexfile = "synth-test-class.dex";
+  // For testing with XCode set the working directory to the buck-out directory.
+  const char* dexfile = "gen/native/redex/test/integ/synth-test-dex/synth.dex";
   if (access(dexfile, R_OK) != 0) {
     dexfile = std::getenv("dexfile");
     ASSERT_NE(nullptr, dexfile);
@@ -58,11 +72,10 @@ TEST(SynthTest1, synthetic) {
   std::vector<DexClasses> dexen;
   dexen.emplace_back(load_classes_from_dex(dexfile));
   DexClasses& classes = dexen.back();
-  std::cout << "Loaded classes: " << classes.size() << std::endl ;
+  std::cout << "Loaded classes: " << classes.size() << std::endl;
 
   std::vector<Pass*> passes = {
-    new ReBindRefsPass(),
-    new SynthPass(),
+      new ReBindRefsPass(), new SynthPass(),
   };
 
   std::vector<KeepRule> null_rules;
@@ -78,27 +91,25 @@ TEST(SynthTest1, synthetic) {
     // Make sure the synthetic method has been removed.
     if (strcmp(class_name, "Lcom/facebook/redextest/Alpha;") == 0) {
       for (const auto& method : cls->get_dmethods()) {
-        ASSERT_STRNE("access$000",  method->get_name()->c_str());
+        ASSERT_STRNE("access$000", method->get_name()->c_str());
       }
     }
 
     // Make sure there are no references to the synthetic method.
     if (strcmp(class_name, "Lcom/facebook/redextest/Alpha$Beta;") == 0) {
-      for (const auto& method : cls->get_dmethods()) {
-        for (const auto& method : cls->get_vmethods()) {
-          const auto& code = method->get_code();
-          const auto& opcodes = code->get_instructions();
-          for (auto& inst : opcodes) {
-            std::cout << SHOW(inst) << std::endl;
-            if (is_invoke(inst->opcode())) {
-              auto invoke = static_cast<DexOpcodeMethod*>(inst);
-              const auto clazz =
-                  invoke->get_method()->get_class()->get_name()->c_str();
-              const auto n =  invoke->get_method()->get_name()->c_str();
-              auto invocation = std::string(clazz) + "." + std::string(n);
-              ASSERT_STRNE("Lcom/facebook/redextest/Alpha;.access$000",
-              invocation.c_str());
-            }
+      for (const auto& method : cls->get_vmethods()) {
+        const auto& code = method->get_code();
+        const auto& opcodes = code->get_instructions();
+        for (auto& inst : opcodes) {
+          std::cout << SHOW(inst) << std::endl;
+          if (is_invoke(inst->opcode())) {
+            auto invoke = static_cast<DexOpcodeMethod*>(inst);
+            const auto clazz =
+                invoke->get_method()->get_class()->get_name()->c_str();
+            const auto n = invoke->get_method()->get_name()->c_str();
+            auto invocation = std::string(clazz) + "." + std::string(n);
+            ASSERT_STRNE("Lcom/facebook/redextest/Alpha;.access$000",
+                         invocation.c_str());
           }
         }
       }
@@ -116,6 +127,14 @@ TEST(SynthTest1, synthetic) {
       }
       ASSERT_TRUE(gamma_synth_found);
     }
+
+    // Tests re-expressed using the match library.
+
+    auto has_alpha_access_gone =
+        m::named<DexClass>("Lcom/facebook/redextest/Alpha;") &&
+        !m::any_dmethods(m::named<DexMethod>("access$000"));
+
+    ASSERT_TRUE(assert_classes(classes, has_alpha_access_gone));
   }
 
   delete g_redex;
