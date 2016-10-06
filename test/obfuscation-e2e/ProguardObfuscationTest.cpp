65454787eda97457971ef1ae722898dbaff8842b
Don Wong <donwong@fb.com>
2016-10-06 11:45:49 -0700

    Reverted commit D3971645
    Summary: Refactor obfuscation testing to make it easier to separate tests for methods and fields without repeating code. All the common setup is now in ProguardObfuscationTest.* and the test just calls the appropriate *_is_renamed method.
    
    Reviewed By: satnam6502
    
    Differential Revision: D3971645
    
    fbshipit-source-id: c5fbfebff55c251ab5a94e7d53dc720fb5b5749d

--- test/obfuscation-e2e/ProguardObfuscationTest.cpp
+++ test/obfuscation-e2e/ProguardObfuscationTest.cpp
@@ -7,32 +7,28 @@
  * of patent rights can be found in the PATENTS file in the same directory.
  */
 
-#include "ProguardObfuscationTest.h"
+#include <cstdint>
+#include <cstdlib>
+#include <gtest/gtest.h>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <array>
 
-ProguardObfuscationTest::ProguardObfuscationTest(
-    const char* dexfile,
-    const char* mapping_file) :
-  proguard_map(std::string(mapping_file)) {
-  dexen.emplace_back(load_classes_from_dex(dexfile));
-}
+#include "DexClass.h"
+#include "DexInstruction.h"
+#include "DexLoader.h"
+#include "Match.h"
+#include "ProguardConfiguration.h"
+#include "ProguardMap.h"
+#include "ProguardMatcher.h"
+#include "ProguardParser.h"
+#include "ReachableClasses.h"
+#include "RedexContext.h"
 
-bool ProguardObfuscationTest::configure_proguard(
-    const char* configuration_file) {
-  redex::ProguardConfiguration pg_config;
-  redex::proguard_parser::parse_file(configuration_file, &pg_config);
-
-  if (!pg_config.ok) {
-    return false;
-  }
-  Scope scope = build_class_scope(dexen);
-  apply_deobfuscated_names(dexen, proguard_map);
-  process_proguard_rules(proguard_map, pg_config, scope);
-  return true;
-}
-
-DexClass* ProguardObfuscationTest::find_class_named(
-    const std::string& name) {
-  DexClasses& classes = dexen.front();
+DexClass* find_class_named(const ProguardMap& proguard_map,
+                           const DexClasses& classes,
+                           const std::string name) {
   auto mapped_search_name = std::string(proguard_map.translate_class(name));
   auto it = std::find_if(
       classes.begin(), classes.end(), [&mapped_search_name](DexClass* cls) {
@@ -45,9 +41,10 @@ DexClass* ProguardObfuscationTest::find_class_named(
   }
 }
 
-bool ProguardObfuscationTest::field_is_renamed(
-    const std::list<DexField*>& fields,
-    const std::string& name) {
+// Returns true if the specified field is renamed.
+bool field_is_renamed(const ProguardMap& proguard_map,
+                      const std::list<DexField*> fields,
+                      const std::string name) {
   for (const auto& field : fields) {
     auto deobfuscated_name = proguard_map.deobfuscate_field(proguard_name(field));
     if (name == std::string(field->c_str()) || (name == deobfuscated_name)) {
@@ -55,4 +52,61 @@ bool ProguardObfuscationTest::field_is_renamed(
     }
   }
   return false;
+}
+
+/**
+ * Check renaming has been properly applied.
+ */
+TEST(ProguardTest, obfuscation) {
+  g_redex = new RedexContext();
+
+  const char* dexfile = std::getenv("pg_config_e2e_dexfile");
+  ASSERT_NE(nullptr, dexfile);
+
+  std::vector<DexClasses> dexen;
+  dexen.emplace_back(load_classes_from_dex(dexfile));
+  DexClasses& classes = dexen.back();
+
+  // Load the Proguard map
+  const char* mapping_file = std::getenv("pg_config_e2e_mapping");
+  ASSERT_NE(nullptr, mapping_file);
+  ProguardMap proguard_map((std::string(mapping_file)));
+
+  const char* configuraiton_file = std::getenv("pg_config_e2e_pgconfig");
+  ASSERT_NE(nullptr, configuraiton_file);
+  redex::ProguardConfiguration pg_config;
+  redex::proguard_parser::parse_file(configuraiton_file, &pg_config);
+  ASSERT_TRUE(pg_config.ok);
+
+  Scope scope = build_class_scope(dexen);
+  apply_deobfuscated_names(dexen, proguard_map);
+  process_proguard_rules(proguard_map, pg_config, scope);
+
+  // Make sure the fields class Alpha are renamed.
+  const std::string alphaName = "Lcom/facebook/redex/test/proguard/Alpha;";
+  const std::array<std::string, 4> fieldNames = {
+    ".wombat:I",
+    ".numbat:I",
+    ".omega:Ljava/lang/String;",
+    ".theta:Ljava/util/List;"};
+  auto alpha = find_class_named(
+      proguard_map, classes, alphaName);
+  ASSERT_NE(nullptr, alpha);
+  for (const std::string fieldName : fieldNames) {
+    ASSERT_TRUE(field_is_renamed(
+        proguard_map,
+        alpha->get_ifields(),
+        alphaName + fieldName)) << alphaName + fieldName << " not obfuscated";
+  }
+
+  // Make sure the fields in the class Beta are not renamed.
+  auto beta = find_class_named(
+      proguard_map, classes, "Lcom/facebook/redex/test/proguard/Beta;");
+  ASSERT_NE(nullptr, beta);
+  ASSERT_FALSE(field_is_renamed(
+      proguard_map,
+      beta->get_ifields(),
+      "Lcom/facebook/redex/test/proguard/Beta;.wombatBeta:I"));
+
+  delete g_redex;
 }
