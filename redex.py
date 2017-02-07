ca4b245ef18edeef60087088f2d611b1c100b81e
Bert Maher <bertrand@fb.com>
2017-02-07 14:10:57 -0800

    Fix iteration over remaining items in pgmap
    Summary:
    We were using `itervalues()`, which just gets the values, when we
    really wanted (key, value) pairs.
    
    Reviewed By: satnam6502
    
    Differential Revision: D4523843
    
    fbshipit-source-id: 1cb43bfdffa2a3b3b571e84bdc0cbd6255c3a1bb

--- redex.py
+++ redex.py
@@ -283,9 +283,8 @@ def update_proguard_mapping_file(pg_map, redex_map, output_file):
                     print(line.rstrip(), file=output)
             else:
                 print(line.rstrip(), file=output)
-        if len(redex_dict) != 0:
-            for unmangled, mangled in redex_dict.itervalues():
-                print('%s -> %s:' % (unmangled, mangled), file=output)
+        for unmangled, mangled in redex_dict.iteritems():
+            print('%s -> %s:' % (unmangled, mangled), file=output)
 
 
 def copy_file_to_out_dir(tmp, apk_output_path, name, human_name, out_name):
