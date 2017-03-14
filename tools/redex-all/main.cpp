a39a49c060aba96fb2c85a9e1e1fac131d9aa973
Jez Ng <jezng@fb.com>
2017-03-14 01:03:50 -0700

    Exit if we fail to load a JAR
    Summary:
    It's probably an error. At the very least it prevents reproducible
    builds
    
    Reviewed By: minjang
    
    Differential Revision: D4703926
    
    fbshipit-source-id: cc9e5a294a7bbeafb39fe992d676ecd903cfbba8

--- tools/redex-all/main.cpp
+++ tools/redex-all/main.cpp
@@ -454,6 +454,7 @@ int main(int argc, char* argv[]) {
               fprintf(stderr,
                       "ERROR: Library jar could not be loaded: %s\n",
                       library_jar.c_str());
+              exit(1);
             }
           }
         }
