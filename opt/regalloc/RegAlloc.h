48a7769f5d87d13c6cac0d2f31d196ca62485b73
Jez Ng <jezng@fb.com>
2017-05-27 22:09:35 -0700

    Delete naive register allocator code
    Summary:
    It's superseded by {D5113824}. The old one was not particular fast
    either and definitely buggy, so there's no real point in keeping it around
    
    Differential Revision: D5144448
    
    fbshipit-source-id: d4126fb8cc67cc87110903c7138f40c136d36781

--- opt/regalloc/RegAlloc.h
+++ opt/regalloc/RegAlloc.h
@@ -9,67 +9,12 @@
 
 #pragma once
 
-#include "DexInstruction.h"
-#include "RegisterKind.h"
+#include <cstdio>
 
-using reg_t = uint16_t;
-
-IRInstruction* gen_move(RegisterKind kind, reg_t dest, reg_t src);
-
-/*
- * Dex opcodes vary in the maximum register slot they can address. For those
- * opcodes that can only address low regs, we insert moves before and after
- * them to shuffle their srcs / dests from high to low and back. For methods
- * that need this shuffling, we reserve a few low registers (starting from v0)
- * for this purpose.
- *
- * Opcodes that have /range equivalents are treated specially. These opcodes
- * can have up to 5 src registers, so reserving space for them at the bottom of
- * the register frame would leave fewer "unreserved" low registers and increase
- * the likelihood of inserting more move instructions. However, since these
- * have range equivalents that can address up to 16-bit register slots, we
- * instead reserve space for them at the top of the register frame (right
- * under the registers for the method arguments.)
- */
-class HighRegMoveInserter {
- public:
-  struct Stats {
-    size_t moves_inserted {0};
-    size_t range_conversions {0};
-    size_t bytes_added {0};
-    void add_move(IRInstruction*);
-  };
-  struct SwapInfo {
-    // number of low registers reserved for opcodes that cannot address their
-    // high register arguments
-    size_t low_reg_swap {0};
-    // number of high registers reserved for opcodes that we convert to /range
-    // form
-    size_t range_swap {0};
-    SwapInfo() = default;
-    SwapInfo(size_t lrs, size_t rs): low_reg_swap(lrs), range_swap(rs) {}
-    bool operator==(const SwapInfo& that) const {
-      return low_reg_swap == that.low_reg_swap && range_swap == that.range_swap;
-    }
-  };
-
-  static SwapInfo reserve_swap(DexMethod* method);
-  void insert_moves(IRCode*, const SwapInfo&);
-  const Stats& get_stats() const { return m_stats; }
- private:
-  void handle_rangeable(IRCode* mt,
-                        InstructionIterator& it,
-                        const KindVec& reg_kinds,
-                        reg_t range_start);
-  static size_t low_reg_space_needed(IRCode* code);
-  static size_t range_space_needed(IRCode* code);
-  static void increment_all_regs(IRCode*, size_t size);
-
-  Stats m_stats;
-};
+#include "PassManager.h"
 
 class RegAllocPass : public Pass {
-public:
+ public:
   RegAllocPass() : Pass("RegAllocPass") {}
   virtual void configure_pass(const PassConfig&) override {
     if (!RedexContext::assume_regalloc()) {
--- test/unit/RegAllocTest.cpp
+++ test/unit/RegAllocTest.cpp
@@ -33,20 +33,10 @@ std::ostream& operator<<(std::ostream& os, const IRInstruction& to_show) {
   return os << show(&to_show);
 }
 
-std::ostream& operator<<(std::ostream& os,
-                         const HighRegMoveInserter::SwapInfo& info) {
-  return os << "SwapInfo(" << info.low_reg_swap << ", " << info.range_swap
-            << ")";
-}
-
 struct RegAllocTest : testing::Test {
-  RegAllocTest() {
-    g_redex = new RedexContext();
-  }
+  RegAllocTest() { g_redex = new RedexContext(); }
 
-  ~RegAllocTest() {
-    delete g_redex;
-  }
+  ~RegAllocTest() { delete g_redex; }
 };
 
 class InstructionList {
@@ -121,152 +111,6 @@ TEST_F(RegAllocTest, RegTypeInvoke) {
 
   auto static_insn = dasm(OPCODE_INVOKE_STATIC, method, {0_v});
   EXPECT_EQ(regalloc::src_reg_type(static_insn, 0), RegisterType::NORMAL);
-}
-
-TEST_F(RegAllocTest, InsertMoveDest) {
-  using namespace dex_asm;
-  DexMethod* method =
-      DexMethod::make_method("Lfoo;", "InsertMoveDest", "V", {});
-  method->make_concrete(ACC_STATIC, false);
-  method->set_code(std::make_unique<IRCode>(method, 18));
-  auto* code = method->get_code();
-  auto array_ty = DexType::make_type("[I");
-  code->push_back(dasm(OPCODE_CONST_4, {0_v, 1_L}));
-  code->push_back(dasm(OPCODE_NEW_ARRAY, array_ty, {18_v, 1_v}));
-  code->push_back(dasm(OPCODE_RETURN_VOID));
-
-  HighRegMoveInserter move_inserter;
-  auto swap_info = HighRegMoveInserter::reserve_swap(method);
-  EXPECT_EQ(swap_info, HighRegMoveInserter::SwapInfo(1, 0));
-  move_inserter.insert_moves(code, swap_info);
-
-  InstructionList expected_insns {
-    dasm(OPCODE_CONST_4, {1_v, 1_L}),
-    dasm(OPCODE_NEW_ARRAY, array_ty, {0_v, 2_v}),
-    dasm(OPCODE_MOVE_OBJECT_FROM16, {19_v, 0_v}),
-    dasm(OPCODE_RETURN_VOID)
-  };
-  EXPECT_TRUE(expected_insns.matches(InstructionIterable(code)));
-}
-
-TEST_F(RegAllocTest, InsertMoveSrc) {
-  using namespace dex_asm;
-  std::vector<const char*> arg_types(17, "Ljava/lang/Object;");
-  DexMethod* method =
-      DexMethod::make_method("Lfoo;", "InsertMoveSrc", "V", arg_types);
-  method->make_concrete(ACC_STATIC, false);
-  method->set_code(std::make_unique<IRCode>(method, 0));
-  auto* code = method->get_code();
-  auto if_ = new MethodItemEntry(dasm(OPCODE_IF_EQ, {15_v, 16_v}));
-  code->push_back(*if_);
-  code->push_back(dasm(OPCODE_INSTANCE_OF, get_object_type(), {0_v, 15_v}));
-  code->push_back(dasm(OPCODE_RETURN_VOID));
-  auto target = new BranchTarget();
-  target->type = BRANCH_SIMPLE;
-  target->src = if_;
-  code->push_back(target);
-  code->push_back(dasm(OPCODE_RETURN_VOID));
-
-  HighRegMoveInserter move_inserter;
-  auto swap_info = HighRegMoveInserter::reserve_swap(method);
-  EXPECT_EQ(swap_info, HighRegMoveInserter::SwapInfo(2, 0));
-  move_inserter.insert_moves(code, swap_info);
-
-  InstructionList expected_insns {
-    dasm(IOPCODE_LOAD_PARAM_OBJECT, {2_v}),
-    dasm(IOPCODE_LOAD_PARAM_OBJECT, {3_v}),
-    dasm(IOPCODE_LOAD_PARAM_OBJECT, {4_v}),
-    dasm(IOPCODE_LOAD_PARAM_OBJECT, {5_v}),
-    dasm(IOPCODE_LOAD_PARAM_OBJECT, {6_v}),
-    dasm(IOPCODE_LOAD_PARAM_OBJECT, {7_v}),
-    dasm(IOPCODE_LOAD_PARAM_OBJECT, {8_v}),
-    dasm(IOPCODE_LOAD_PARAM_OBJECT, {9_v}),
-    dasm(IOPCODE_LOAD_PARAM_OBJECT, {10_v}),
-    dasm(IOPCODE_LOAD_PARAM_OBJECT, {11_v}),
-    dasm(IOPCODE_LOAD_PARAM_OBJECT, {12_v}),
-    dasm(IOPCODE_LOAD_PARAM_OBJECT, {13_v}),
-    dasm(IOPCODE_LOAD_PARAM_OBJECT, {14_v}),
-    dasm(IOPCODE_LOAD_PARAM_OBJECT, {15_v}),
-    dasm(IOPCODE_LOAD_PARAM_OBJECT, {16_v}),
-    dasm(IOPCODE_LOAD_PARAM_OBJECT, {17_v}),
-    dasm(IOPCODE_LOAD_PARAM_OBJECT, {18_v}),
-    dasm(OPCODE_MOVE_OBJECT_FROM16, {0_v, 17_v}),
-    dasm(OPCODE_MOVE_OBJECT_FROM16, {1_v, 18_v}),
-    dasm(OPCODE_IF_EQ, {0_v, 1_v}),
-    dasm(OPCODE_MOVE_OBJECT_FROM16, {0_v, 17_v}),
-    dasm(OPCODE_INSTANCE_OF, get_object_type(), {2_v, 0_v}),
-    dasm(OPCODE_RETURN_VOID),
-    dasm(OPCODE_RETURN_VOID)
-  };
-  EXPECT_TRUE(expected_insns.matches(InstructionIterable(code)));
-}
-
-TEST_F(RegAllocTest, ConvertToRangeForOneSrc) {
-  using namespace dex_asm;
-  DexMethod* method =
-      DexMethod::make_method("Lfoo;", "ConvertToRangeForOneSrc", "V", {});
-  method->make_concrete(ACC_STATIC, false);
-  method->set_code(std::make_unique<IRCode>(method, 17));
-  auto* code = method->get_code();
-  code->push_back(dasm(OPCODE_NEW_INSTANCE, get_object_type(), {16_v}));
-  // can be converted to invoke-virtual/range without needing to move the
-  // register
-  code->push_back(
-      dasm(OPCODE_INVOKE_VIRTUAL,
-           DexMethod::make_method("Ljava/lang/Object", "hashCode", "V", {}),
-           {16_v}));
-  code->push_back(dasm(OPCODE_RETURN_VOID));
-
-  HighRegMoveInserter move_inserter;
-  auto swap_info = HighRegMoveInserter::reserve_swap(method);
-  EXPECT_EQ(swap_info, HighRegMoveInserter::SwapInfo(0, 0));
-  move_inserter.insert_moves(code, swap_info);
-
-  InstructionList expected_insns{
-    dasm(OPCODE_NEW_INSTANCE, get_object_type(), {16_v}),
-    dasm(OPCODE_INVOKE_VIRTUAL,
-         DexMethod::make_method("Ljava/lang/Object", "hashCode", "V", {}),
-         {16_v}),
-    dasm(OPCODE_RETURN_VOID)
-  };
-  EXPECT_TRUE(expected_insns.matches(InstructionIterable(code)));
-}
-
-TEST_F(RegAllocTest, ConvertToRangeForTwoSrc) {
-  using namespace dex_asm;
-  DexMethod* method =
-      DexMethod::make_method("Lfoo;", "ConvertToRangeForTwoSrc", "V", {});
-  method->make_concrete(ACC_STATIC, false);
-  method->set_code(std::make_unique<IRCode>(method, 19));
-  auto* code = method->get_code();
-  code->push_back(dasm(OPCODE_NEW_INSTANCE, get_object_type(), {16_v}));
-  code->push_back(dasm(OPCODE_NEW_INSTANCE, get_object_type(), {17_v}));
-  // the register args to invoke don't map directly to a range, so we'll insert
-  // move opcodes to shuffle them into place
-  code->push_back(
-      dasm(OPCODE_INVOKE_VIRTUAL,
-           DexMethod::make_method(
-               "Ljava/lang/Object", "equals", "B", {"Ljava/lang/Object"}),
-           {17_v, 16_v}));
-  code->push_back(dasm(OPCODE_RETURN_VOID));
-
-  HighRegMoveInserter move_inserter;
-  auto swap_info = HighRegMoveInserter::reserve_swap(method);
-  EXPECT_EQ(swap_info, HighRegMoveInserter::SwapInfo(0, 2));
-  move_inserter.insert_moves(code, swap_info);
-
-  InstructionList expected_insns {
-    dasm(OPCODE_NEW_INSTANCE, get_object_type(), {16_v}),
-    dasm(OPCODE_NEW_INSTANCE, get_object_type(), {17_v}),
-    dasm(OPCODE_MOVE_OBJECT_FROM16, {19_v, 17_v}),
-    dasm(OPCODE_MOVE_OBJECT_FROM16, {20_v, 16_v}),
-    dasm(OPCODE_INVOKE_VIRTUAL,
-         DexMethod::make_method(
-             "Ljava/lang/Object", "equals", "B", {"Ljava/lang/Object"}),
-         {19_v, 20_v}),
-    dasm(OPCODE_RETURN_VOID)
-  };
-  EXPECT_TRUE(expected_insns.matches(InstructionIterable(code)));
 }
 
 TEST_F(RegAllocTest, LiveRangeSingleBlock) {
