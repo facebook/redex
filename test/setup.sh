98e38b0a1049a5cb63b5dfede6f31396cef3e1f3
Justin Hendrick <jhendrick@fb.com>
2017-06-30 13:50:11 -0700

    Fix open source tests
    Summary:
    Google moved gtest to a different website and the zip archive has a different
    top level directory name
    
    closes #177
    
    See https://github.com/facebook/redex/issues/177
    
    Reviewed By: int3
    
    Differential Revision: D5354047
    
    fbshipit-source-id: 2fa57c91433b545f4aa4b73366acb0ca8c9f73e2

--- test/setup.sh
+++ test/setup.sh
@@ -4,7 +4,8 @@ set -e
 
 pushd test
 test -e gtest-1.7.0.zip || {
-    curl -O https://googletest.googlecode.com/files/gtest-1.7.0.zip
+    curl https://codeload.github.com/google/googletest/zip/release-1.7.0 \
+         -o gtest-1.7.0.zip
     unzip -o gtest-1.7.0.zip
 }
 popd
