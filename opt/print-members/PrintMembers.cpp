eb502ce8b86141c9c836859b67ada125a51ee2b1
Justin Hendrick <jhendrick@fb.com>
2017-07-25 20:05:33 -0700

    small improvements to PrintMembersPass
    Summary: I wanted to print an IRCode object of a specific method
    
    Reviewed By: emma0303
    
    Differential Revision: D5494006
    
    fbshipit-source-id: c2af711b9adc57619f55c51217e7f4430d8606e8

--- opt/print-members/PrintMembers.cpp
+++ opt/print-members/PrintMembers.cpp
@@ -12,26 +12,45 @@
 #include "DexUtil.h"
 #include "Show.h"
 
-void PrintMembersPass::run_pass(
-  DexStoresVector& stores,
-  ConfigFiles&,
-  PassManager&
-) {
+void PrintMembersPass::run_pass(DexStoresVector& stores,
+                                ConfigFiles&,
+                                PassManager&) {
   for (auto const& dex : DexStoreClassesIterator(stores)) {
     for (auto const& cls : dex) {
-      printf("class: %s\n", SHOW(cls));
-      for (auto const& m : cls->get_sfields()) {
-        printf("sfield: %s\n", SHOW(m));
+      if (m_config.only_these_classes.empty() ||
+          m_config.only_these_classes.count(cls) > 0) {
+        printf("class: %s\n", SHOW(cls));
+
+        if (m_config.show_sfields) {
+          for (auto const& m : cls->get_sfields()) {
+            printf("sfield: %s\n", SHOW(m));
+          }
+        }
+
+        if (m_config.show_ifields) {
+          for (auto const& m : cls->get_ifields()) {
+            printf("ifield: %s\n", SHOW(m));
+          }
+        }
+
+        for (auto const& m : cls->get_dmethods()) {
+          handle_method(m, "dmethod");
+        }
+        for (auto const& m : cls->get_vmethods()) {
+          handle_method(m, "vmethod");
+        }
       }
-      for (auto const& m : cls->get_ifields()) {
-        printf("ifield: %s\n", SHOW(m));
-      }
-      for (auto const& m : cls->get_dmethods()) {
-        printf("dmethod: %s\n", SHOW(m));
-      }
-      for (auto const& m : cls->get_vmethods()) {
-        printf("vmethod: %s\n", SHOW(m));
-      }
+    }
+  }
+}
+
+void PrintMembersPass::handle_method(DexMethod* m, const char* type) {
+  if (m_config.only_these_methods.empty() ||
+      m_config.only_these_methods.count(m) > 0) {
+    printf("%s: %s\n", type, SHOW(m));
+    const IRCode* code = m->get_code();
+    if (m_config.show_code && code != nullptr) {
+      printf("%s\n", SHOW(code));
     }
   }
 }
--- opt/print-members/PrintMembers.h
+++ opt/print-members/PrintMembers.h
@@ -9,11 +9,48 @@
 
 #pragma once
 
+#include "DexClass.h"
+#include "DexUtil.h"
 #include "Pass.h"
 
 class PrintMembersPass : public Pass {
  public:
   PrintMembersPass() : Pass("PrintMembersPass") {}
 
+  virtual void configure_pass(const PassConfig& pc) override {
+    pc.get("show_code", false, m_config.show_code);
+    pc.get("show_sfields", true, m_config.show_sfields);
+    pc.get("show_ifields", true, m_config.show_ifields);
+
+    std::vector<std::string> classes;
+    pc.get("only_these_classes", {}, classes);
+    for (std::string& name : classes) {
+      DexClass* c =
+          type_class(DexType::get_type(DexString::get_string(name.c_str())));
+      if (c != nullptr) {
+        m_config.only_these_classes.insert(c);
+      }
+    }
+
+    std::vector<std::string> methods;
+    pc.get("only_these_methods", {}, methods);
+    for (std::string& name : methods) {
+      DexMethod* m = DexMethod::get_method(name);
+      if (m != nullptr) {
+        m_config.only_these_methods.insert(m);
+      }
+    }
+  }
+
   virtual void run_pass(DexStoresVector&, ConfigFiles&, PassManager&) override;
+
+ private:
+  void handle_method(DexMethod* m, const char* type);
+  struct Config {
+    bool show_code;
+    bool show_sfields;
+    bool show_ifields;
+    std::unordered_set<DexClass*> only_these_classes;
+    std::unordered_set<DexMethod*> only_these_methods;
+  } m_config;
 };
