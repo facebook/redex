a66cf16dc55b0ca9129190e39cc31a34493c2177
Jez Ng <jezng@fb.com>
2017-08-24 14:43:26 -0700

    Parallelize localDCE and part of Synth
    Reviewed By: emma0303
    
    Differential Revision: D5696602
    
    fbshipit-source-id: 7dc57f24cdad1cfbd30c910a735c9119d87f6010

--- opt/local-dce/LocalDce.cpp
+++ opt/local-dce/LocalDce.cpp
@@ -22,7 +22,7 @@
 #include "IRCode.h"
 #include "IRInstruction.h"
 #include "Transform.h"
-#include "Walkers.h"
+#include "ParallelWalkers.h"
 
 namespace {
 
@@ -347,21 +347,6 @@ class LocalDce {
     }
   }
 
- public:
-  void run(const Scope& scope) {
-    walk_methods(scope,
-                 [&](DexMethod* m) {
-                   if (!m->get_code()) {
-                     return;
-                   }
-                   dce(m);
-                 });
-    TRACE(DCE, 1, "Dead instructions: %lu\n", m_stats.dead_instruction_count);
-    TRACE(DCE, 1, "Unreachable instructions: %lu\n",
-          m_stats.unreachable_instruction_count);
-  }
-
- private:
   std::unordered_set<DexMethod*> m_pure_methods;
   Stats m_stats;
 };
@@ -381,9 +366,23 @@ void LocalDcePass::run_pass(DexStoresVector& stores,
     return;
   }
   auto scope = build_class_scope(stores);
-  LocalDce ldce;
-  ldce.run(scope);
-  const auto& stats = ldce.get_stats();
+  auto stats = walk_methods_parallel<std::nullptr_t, LocalDce::Stats>(
+      scope,
+      [&](std::nullptr_t, DexMethod* m) {
+        auto* code = m->get_code();
+        if (code == nullptr) {
+          return LocalDce::Stats();
+        }
+        LocalDce ldce;
+        ldce.dce(m);
+        return ldce.get_stats();
+      },
+      [](LocalDce::Stats a, LocalDce::Stats b) {
+        a.dead_instruction_count += b.dead_instruction_count;
+        a.unreachable_instruction_count += b.unreachable_instruction_count;
+        return a;
+      },
+      [](int) { return nullptr; });
   mgr.incr_metric(METRIC_DEAD_INSTRUCTIONS, stats.dead_instruction_count);
   mgr.incr_metric(METRIC_UNREACHABLE_INSTRUCTIONS,
                   stats.unreachable_instruction_count);
