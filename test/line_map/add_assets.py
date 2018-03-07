4e94393442e9239295641df733a242137c72d447
Jez Ng <jezng@fb.com>
2018-03-06 16:37:56 -0800

    Add license headers to Python files
    Reviewed By: emma0303
    
    Differential Revision: D7175471
    
    fbshipit-source-id: 15bd39226236fb46c5f5cf4c07de3df96128a273

--- test/line_map/add_assets.py
+++ test/line_map/add_assets.py
@@ -1,6 +1,13 @@
 #! /usr/bin/env python
 # @lint-ignore-every PYTHON3COMPATIMPORTS1
 
+# Copyright (c) 2016-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
+
 import argparse
 import os
 import subprocess
