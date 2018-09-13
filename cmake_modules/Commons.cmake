df3b7b49104db5814cf8a7ec57b9c036a825159f
Tapan Thaker <tapanth@uber.com>
2018-09-13 12:20:12 -0700

    Create ReDex binary with dependencies statically linked (#346)
    Summary:
    Create an option to generate ReDex binaries with dependencies statically linked.
    Statically linked libraries will reduce the overhead of installing dependencies like Boost, JsonCpp & LibZ before using ReDex.
    
    This PR adds a argument `BUILD_TYPE` that can be set to `Static` or `Shared` while configuring the build using CMake.
    
    For e.g.
    `cmake .. -DBUILD_TYPE=Static && cmake --build .`
    
    `BUILD_TYPE` defaults to `Shared`
    
    Results:
    
    **Shared**
    ```
    otool -L redex-all
    redex-all:
    	/usr/local/opt/boost/lib/libboost_system-mt.dylib (compatibility version 0.0.0, current version 0.0.0)
    	/usr/local/opt/boost/lib/libboost_regex-mt.dylib (compatibility version 0.0.0, current version 0.0.0)
    	/usr/local/opt/boost/lib/libboost_filesystem-mt.dylib (compatibility version 0.0.0, current version 0.0.0)
    	/usr/local/opt/boost/lib/libboost_program_options-mt.dylib (compatibility version 0.0.0, current version 0.0.0)
    	/usr/local/opt/boost/lib/libboost_iostreams-mt.dylib (compatibility version 0.0.0, current version 0.0.0)
    	/usr/local/opt/boost/lib/libboost_thread-mt.dylib (compatibility version 0.0.0, current version 0.0.0)
    	/usr/local/opt/boost/lib/libboost_chrono-mt.dylib (compatibility version 0.0.0, current version 0.0.0)
    	/usr/local/opt/boost/lib/libboost_date_time-mt.dylib (compatibility version 0.0.0, current version 0.0.0)
    	/usr/local/opt/boost/lib/libboost_atomic-mt.dylib (compatibility version 0.0.0, current version 0.0.0)
    	/usr/local/opt/jsoncpp/lib/libjsoncpp.19.dylib (compatibility version 19.0.0, current version 1.8.4)
    	/usr/local/opt/zlib/lib/libz.1.dylib (compatibility version 1.0.0, current version 1.2.11)
    	/usr/lib/libc++.1.dylib (compatibility version 1.0.0, current version 400.9.0)
    	/usr/lib/libSystem.B.dylib (compatibility version 1.0.0, current version 1252.50.4)
    ```
    
    **Static**
    ```
    $ otool -L redex-all
    redex-all:
    	/usr/lib/libc++.1.dylib (compatibility version 1.0.0, current version 400.9.0)
    	/usr/lib/libSystem.B.dylib (compatibility version 1.0.0, current version 1252.50.4)
    ```
    
    Depends On: #345
    Pull Request resolved: https://github.com/facebook/redex/pull/346
    
    Reviewed By: minjang
    
    Differential Revision: D9758157
    
    Pulled By: justinjhendrick
    
    fbshipit-source-id: 4b259147e3c890065dc57941096948ecd2df67ae

--- cmake_modules/Commons.cmake
+++ cmake_modules/Commons.cmake
@@ -25,25 +25,53 @@ macro(set_common_cxx_flags_for_redex)
 endmacro()
 
 macro(add_dependent_packages_for_redex)
+
+    message("-- ENABLE_STATIC ${ENABLE_STATIC}")
+
+    if(ENABLE_STATIC)
+        set(Boost_USE_STATIC_LIBS ON)
+        set(Boost_USE_STATIC_RUNTIME ON)
+        set(Boost_USE_MULTITHREADED ON)
+    endif()
+
     find_package(Boost 1.56.0 REQUIRED COMPONENTS system regex filesystem program_options iostreams thread)
     print_dirs("${Boost_INCLUDE_DIRS}" "Boost_INCLUDE_DIRS")
     print_dirs("${Boost_LIBRARIES}" "Boost_LIBRARIES")
 
-    set(Boost_USE_STATIC_LIBS ON)
-    set(Boost_USE_STATIC_RUNTIME ON)
-    set(Boost_USE_MULTITHREADED ON)
-
     find_package(JsonCpp 0.10.5 REQUIRED)
     print_dirs("${JSONCPP_INCLUDE_DIRS}" "JSONCPP_INCLUDE_DIRS")
-    print_dirs("${JSONCPP_LIBRARY}" "JSONCPP_LIBRARY")
+
+    if(ENABLE_STATIC)
+        set(REDEX_JSONCPP_LIBRARY ${JSONCPP_LIBRARY_STATIC})
+    else()
+        set(REDEX_JSONCPP_LIBRARY ${JSONCPP_LIBRARY})
+    endif()
+
+    message("-- REDEX_JSONCPP_LIBRARY: ${REDEX_JSONCPP_LIBRARY}")
 
     if (NOT MSVC)
         set(JSONCPP_INCLUDE_DIRS "${JSONCPP_INCLUDE_DIRS}/jsoncpp")
     endif ()
 
+    if (APPLE)
+        #Static library is not installed on default path in MacOS because it conflicts with Xcode Version
+        set(ZLIB_HOME "/usr/local/opt/zlib/")
+    endif ()
+
     find_package(ZLIB REQUIRED)
+
+    print_dirs(${ZLIB_STATIC_LIB} "ZLIB_STATIC_LIB")
+    print_dirs(${ZLIB_SHARED_LIB} "ZLIB_SHARED_LIB")
+    
+    if (ENABLE_STATIC)
+        set(REDEX_ZLIB_LIBRARY ${ZLIB_STATIC_LIB})
+    else ()
+        set(REDEX_ZLIB_LIBRARY ${ZLIB_SHARED_LIB})
+    endif ()
+
     print_dirs("${ZLIB_INCLUDE_DIRS}" "ZLIB_INCLUDE_DIRS")
-    print_dirs("${ZLIB_LIBRARIES}" "ZLIB_LIBRARIES")
+    print_dirs("${REDEX_ZLIB_LIBRARY}" "REDEX_ZLIB_LIBRARY")
+
 endmacro()
 
 function(set_link_whole target_name lib_name)
