b541c985c42e6ab876bd9f94136e3454f57260dc
Emma Sevastian <emmasevastian@fb.com>
2018-09-25 12:27:54 -0700

    Update gather_types to account for fields and methods
    Summary:
    As title.
    
    gather_types per instruction was only accounting for types in "type" opcodes. For fields or methods, it wouldn't account for field's type, class etc. I think it should include at least those 2. Maybe other things I am missing? cc int3
    
    Reviewed By: int3
    
    Differential Revision: D10014918
    
    fbshipit-source-id: f27579f2c614b5f21f1a7e971d04ee4103d15db9

--- libredex/DexClass.cpp
+++ libredex/DexClass.cpp
@@ -1210,3 +1210,25 @@ uint32_t DexCode::size() const {
   }
   return size;
 }
+
+void IRInstruction::gather_types(std::vector<DexType*>& ltype) const {
+  switch (opcode::ref(opcode())) {
+  case opcode::Ref::None:
+  case opcode::Ref::String:
+  case opcode::Ref::Literal:
+  case opcode::Ref::Data:
+    break;
+
+  case opcode::Ref::Type:
+    ltype.push_back(m_type);
+    break;
+
+  case opcode::Ref::Field:
+    m_field->gather_types_shallow(ltype);
+    break;
+
+  case opcode::Ref::Method:
+    m_method->gather_types_shallow(ltype);
+    break;
+  }
+}
