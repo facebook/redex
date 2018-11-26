8b8487121d9867a643cbfceac2f7d08116952416
Justin Hendrick <jhendrick@fb.com>
2018-11-26 14:23:07 -0800

    Remove pre-verify for StripDebugInfo
    Summary: d8 doesn't generate prologues when `--release` is passed to it
    
    Reviewed By: int3
    
    Differential Revision: D13129033
    
    fbshipit-source-id: 66d91d48b68552416fd136bf5c5d4081ed11aff2

--- test/instr/StripDebugInfoTest.cpp
+++ test/instr/StripDebugInfoTest.cpp
@@ -13,34 +13,6 @@
 
 #include "VerifyUtil.h"
 
-TEST_F(PreVerify, StripDebugInfo) {
-  bool found_prologue_end = false;
-  for (const auto& cls : classes) {
-    std::vector<DexMethodRef*> methods;
-    cls->gather_methods(methods);
-    for (auto dm : methods) {
-      if (!dm->is_def()) continue;
-      auto code = static_cast<DexMethod*>(dm)->get_dex_code();
-      if (!code) continue;
-      auto debug_item = code->get_debug_item();
-      if (!debug_item) continue;
-      for (const auto& e : debug_item->get_entries()) {
-        if (e.type == DexDebugEntryType::Instruction) {
-          DexDebugInstruction* dbg_op = e.insn.get();
-          if (!dbg_op) continue;
-          auto op = dbg_op->opcode();
-          if (op == DBG_SET_PROLOGUE_END) {
-            found_prologue_end = true;
-            break;
-          }
-        }
-      }
-      if (found_prologue_end) break;
-    }
-  }
-  EXPECT_TRUE(found_prologue_end);
-}
-
 TEST_F(PostVerify, StripDebugInfo) {
   for (const auto& cls : classes) {
     std::vector<DexMethodRef*> methods;
