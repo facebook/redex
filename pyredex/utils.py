eb00b52e16c5618b9183c028ba33c6320b0b53bc
Jez Ng <jezng@fb.com>
2019-01-29 12:55:01 -0800

    Better error message when jarsigner isn't found
    Reviewed By: emma0303, justinjhendrick
    
    Differential Revision: D13864652
    
    fbshipit-source-id: 8f0f44f4c35594a6d9aaae1fe91b9bc392515d9b

--- pyredex/utils.py
+++ pyredex/utils.py
@@ -3,6 +3,9 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+from __future__ import print_function
+
+import errno
 import glob
 import shutil
 import subprocess
@@ -39,13 +42,26 @@ def remove_temp_dirs():
 
 
 def sign_apk(keystore, keypass, keyalias, apk):
-    subprocess.check_call(
-        [
-            'jarsigner', '-sigalg', 'SHA1withRSA', '-digestalg', 'SHA1',
-            '-keystore', keystore, '-storepass', keypass, apk, keyalias
-        ],
-        stdout=sys.stderr
-    )
+    try:
+        subprocess.check_call(
+            [
+                'jarsigner', '-sigalg', 'SHA1withRSA', '-digestalg', 'SHA1',
+                '-keystore', keystore, '-storepass', keypass, apk, keyalias
+            ],
+            stdout=sys.stderr
+        )
+    except OSError as e:
+        # Future migration note: In Python 3, this will throw a
+        # FileNotFoundError instead. We could also use shutil.which().
+        if e.errno == errno.ENOENT:
+            FAIL_COLOR = '\033[91m'
+            END_COLOR = '\033[0m'
+            print(
+                ('{}Failed to execute jarsigner: please check if jarsigner '
+                 'is on your $PATH.{}').format(FAIL_COLOR, END_COLOR),
+                file=sys.stderr,
+            )
+        raise
 
 
 def remove_comments_from_line(l):
