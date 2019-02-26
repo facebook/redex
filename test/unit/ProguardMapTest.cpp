7107060d86a0c6c2f14239dcd3138e9e74412ee3
Evelio Tarazona Caceres <eveliotc@fb.com>
2019-02-26 15:06:29 -0800

    Fix sanitizer failure for LineNumbers test
    Summary: Sanitizer runs were failing. Example report: #1628621
    
    Reviewed By: int3
    
    Differential Revision: D14234174
    
    fbshipit-source-id: a9820e1cd96f629e41f4ce93202c8dea384ce642

--- test/unit/ProguardMapTest.cpp
+++ test/unit/ProguardMapTest.cpp
@@ -61,17 +61,6 @@ TEST(ProguardMapTest, HandlesGeneratedComments) {
   EXPECT_EQ("LA;.a:I", pm.translate_field("Lcom/foo/bar;.do1:I"));
 }
 
-inline static ProguardLineRange& line(uint32_t s,
-                                      uint32_t e,
-                                      uint32_t os,
-                                      uint32_t oe) {
-  return *new ProguardLineRange(s, e, os, oe);
-}
-
-inline static ProguardLineRange& line(uint32_t s, uint32_t e) {
-  return line(s, e, 0, 0);
-}
-
 TEST(ProguardMapTest, LineNumbers) {
   std::stringstream ss(
       "com.foo.bar -> A:\n"
@@ -126,34 +115,44 @@ TEST(ProguardMapTest, LineNumbers) {
             pm.translate_method("Landroid/support/v4/app/Fragment;.stuff:(Lcom/"
                                 "foo/bar;Lcom/foo/bar;)Lcom/foo/bar;"));
 
-  EXPECT_THAT(pm.method_lines("LA;.<init>:()V"),
-              AllOf(SizeIs(1), UnorderedElementsAre(Pointee(line(3, 3)))));
-  EXPECT_THAT(pm.method_lines("LA;.a:()Ljava/io/File;"),
-              AllOf(SizeIs(1), UnorderedElementsAre(Pointee(line(0, 0)))));
-  EXPECT_THAT(pm.method_lines("LA;.b:()V"),
-              AllOf(SizeIs(1), UnorderedElementsAre(Pointee(line(3, 0)))));
-  EXPECT_THAT(pm.method_lines("LA;.k:()Z"),
-              AllOf(SizeIs(7),
-                    UnorderedElementsAre(Pointee(line(1, 1, 490, 490)),
-                                         Pointee(line(1, 1, 275, 0)),
-                                         Pointee(line(2, 2, 490, 0)),
-                                         Pointee(line(2, 2, 275, 0)),
-                                         Pointee(line(3, 3, 491, 491)),
-                                         Pointee(line(3, 3, 275, 0)),
-                                         Pointee(line(4, 4, 275, 275)))));
+  EXPECT_THAT(
+      pm.method_lines("LA;.<init>:()V"),
+      AllOf(SizeIs(1),
+            UnorderedElementsAre(Pointee(ProguardLineRange(3, 3, 0, 0)))));
+  EXPECT_THAT(
+      pm.method_lines("LA;.a:()Ljava/io/File;"),
+      AllOf(SizeIs(1),
+            UnorderedElementsAre(Pointee(ProguardLineRange(0, 0, 0, 0)))));
+  EXPECT_THAT(
+      pm.method_lines("LA;.b:()V"),
+      AllOf(SizeIs(1),
+            UnorderedElementsAre(Pointee(ProguardLineRange(3, 0, 0, 0)))));
+  EXPECT_THAT(
+      pm.method_lines("LA;.k:()Z"),
+      AllOf(SizeIs(7),
+            UnorderedElementsAre(Pointee(ProguardLineRange(1, 1, 490, 490)),
+                                 Pointee(ProguardLineRange(1, 1, 275, 0)),
+                                 Pointee(ProguardLineRange(2, 2, 490, 0)),
+                                 Pointee(ProguardLineRange(2, 2, 275, 0)),
+                                 Pointee(ProguardLineRange(3, 3, 491, 491)),
+                                 Pointee(ProguardLineRange(3, 3, 275, 0)),
+                                 Pointee(ProguardLineRange(4, 4, 275, 275)))));
   EXPECT_THAT(
       pm.method_lines("LA;.k:()J"),
-      AllOf(SizeIs(1), UnorderedElementsAre(Pointee(line(2, 2, 66, 66)))));
-  EXPECT_THAT(pm.method_lines("LA;.o:()V"),
-              AllOf(SizeIs(7),
-                    UnorderedElementsAre(Pointee(line(1, 2, 282, 283)),
-                                         Pointee(line(3, 3, 385, 385)),
-                                         Pointee(line(4, 5, 286, 287)),
-                                         Pointee(line(6, 6, 289, 289)),
-                                         Pointee(line(7, 7, 382, 382)),
-                                         Pointee(line(8, 8, 385, 385)),
-                                         Pointee(line(9, 9, 387, 387)))));
+      AllOf(SizeIs(1),
+            UnorderedElementsAre(Pointee(ProguardLineRange(2, 2, 66, 66)))));
+  EXPECT_THAT(
+      pm.method_lines("LA;.o:()V"),
+      AllOf(SizeIs(7),
+            UnorderedElementsAre(Pointee(ProguardLineRange(1, 2, 282, 283)),
+                                 Pointee(ProguardLineRange(3, 3, 385, 385)),
+                                 Pointee(ProguardLineRange(4, 5, 286, 287)),
+                                 Pointee(ProguardLineRange(6, 6, 289, 289)),
+                                 Pointee(ProguardLineRange(7, 7, 382, 382)),
+                                 Pointee(ProguardLineRange(8, 8, 385, 385)),
+                                 Pointee(ProguardLineRange(9, 9, 387, 387)))));
   EXPECT_THAT(
       pm.method_lines("Landroid/support/v4/app/Fragment;.o:(LA;LA;)LA;"),
-      AllOf(SizeIs(1), UnorderedElementsAre(Pointee(line(1, 10)))));
+      AllOf(SizeIs(1),
+            UnorderedElementsAre(Pointee(ProguardLineRange(1, 10, 0, 0)))));
 }
