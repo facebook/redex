373f2dd4c66e3b112d10fe9d8026f49ff79a6cda
Wei Zhang (Devinfra) <zwei@fb.com>
2019-04-09 14:30:52 -0700

    Promo D14816402 to stable
    Differential Revision: D14855400
    
    fbshipit-source-id: 2e8f3f29dedb2aed43a9c37462e41530f38bb316

--- opt/reduce-gotos/ReduceGotos.h
+++ opt/reduce-gotos/ReduceGotos.h
@@ -12,6 +12,11 @@
 class ReduceGotosPass : public Pass {
  public:
   struct Stats {
+    size_t removed_switches{0};
+    size_t reduced_switches{0};
+    size_t remaining_trivial_switches{0};
+    size_t removed_sparse_switch_cases{0};
+    size_t removed_packed_switch_cases{0};
     size_t replaced_gotos_with_returns{0};
     size_t removed_trailing_moves{0};
     size_t inverted_conditional_branches{0};
--- test/unit/ReduceGotosTest.cpp
+++ test/unit/ReduceGotosTest.cpp
@@ -19,7 +19,12 @@ void test(const std::string& code_str,
           const std::string& expected_str,
           size_t expected_replaced_gotos_with_returns,
           size_t expected_removed_trailing_moves,
-          size_t expected_inverted_conditional_branches) {
+          size_t expected_inverted_conditional_branches,
+          size_t expected_removed_switches = 0,
+          size_t expected_reduced_switches = 0,
+          size_t expected_remaining_trivial_switches = 0,
+          size_t expected_removed_sparse_switch_cases = 0,
+          size_t expected_removed_packed_switch_cases = 0) {
   g_redex = new RedexContext();
 
   auto code = assembler::ircode_from_string(code_str);
@@ -31,12 +36,128 @@ void test(const std::string& code_str,
   EXPECT_EQ(expected_removed_trailing_moves, stats.removed_trailing_moves);
   EXPECT_EQ(expected_inverted_conditional_branches,
             stats.inverted_conditional_branches);
+  EXPECT_EQ(expected_removed_switches, stats.removed_switches);
+  EXPECT_EQ(expected_reduced_switches, stats.reduced_switches);
+  EXPECT_EQ(expected_remaining_trivial_switches,
+            stats.remaining_trivial_switches);
+  EXPECT_EQ(expected_removed_sparse_switch_cases,
+            stats.removed_sparse_switch_cases);
+  EXPECT_EQ(expected_removed_packed_switch_cases,
+            stats.removed_packed_switch_cases);
 
   EXPECT_EQ(assembler::to_s_expr(code.get()),
             assembler::to_s_expr(expected.get()));
 
   delete g_redex;
 };
+
+TEST(ReduceGotosTest, packed_switch_useless) {
+  auto code_str = R"(
+    (
+      (packed-switch v0 (:b :a))
+      (:a)
+      (:b)
+      (return-void)
+    )
+  )";
+  const auto& expected_str = R"(
+    (
+      (return-void)
+    )
+  )";
+  test(code_str, expected_str, 0, 0, 0, 1, 0, 0, 0, 2);
+}
+
+TEST(ReduceGotosTest, sparse_switch_useless) {
+  auto code_str = R"(
+    (
+      (sparse-switch v0 (:b :a))
+      (:a 0)
+      (:b 1)
+      (return-void)
+    )
+  )";
+  const auto& expected_str = R"(
+    (
+      (return-void)
+    )
+  )";
+  test(code_str, expected_str, 0, 0, 0, 1, 0, 0, 2, 0);
+}
+
+TEST(ReduceGotosTest, sparse_switch_reducible) {
+  auto code_str = R"(
+    (
+      (sparse-switch v0 (:a :b :c))
+      (:b 1)
+      (return-void)
+
+      (:a 0)
+      (:c 16)
+      (return-void)
+    )
+  )";
+  const auto& expected_str = R"(
+    (
+      (sparse-switch v0 (:a :c))
+      (return-void)
+
+      (:c 16)
+      (:a 0)
+      (return-void)
+    )
+  )";
+  test(code_str, expected_str, 0, 0, 0, 0, 1, 0, 1, 0);
+}
+
+TEST(ReduceGotosTest, packed_switch_reducible) {
+  auto code_str = R"(
+    (
+      (packed-switch v0 (:a :b :c))
+      (:a 0)
+      (return-void)
+
+      (:b 1)
+      (:c 2)
+      (return-void)
+    )
+  )";
+  const auto& expected_str = R"(
+    (
+      (packed-switch v0 (:b :c))
+      (return-void)
+
+      (:c 2)
+      (:b 1)
+      (return-void)
+    )
+  )";
+  test(code_str, expected_str, 0, 0, 0, 0, 1, 0, 0, 1);
+}
+
+TEST(ReduceGotosTest, trivial_remaining_switch) {
+  auto code_str = R"(
+    (
+      (sparse-switch v0 (:a :b :c))
+      (:a 0)
+      (:b 1)
+      (return-void)
+
+      (:c 16)
+      (return-void)
+    )
+  )";
+  const auto& expected_str = R"(
+    (
+      (packed-switch v0 (:c))
+      (return-void)
+
+      (:c 16)
+      (return-void)
+    )
+  )";
+  test(code_str, expected_str, 0, 0, 0, 0, 1, 1, 2, 0);
+}
 
 TEST(ReduceGotosTest, trivial) {
   const auto& code_str = R"(
