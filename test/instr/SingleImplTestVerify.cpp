deff19b226e8b23795e30e05c55eeacc5f810be5
Feng Liu <fengliu@fb.com>
2019-04-11 12:40:07 -0700

    Test cases
    Summary:
    Only a few test cases.
    Current SingleImpl does not support removing interface `I_1`.
    
    Reviewed By: thezhangwei
    
    Differential Revision: D14854651
    
    fbshipit-source-id: cd4963a93732496e6b28f2346cc161e6a88f740f

---
+++ test/instr/SingleImplTestVerify.cpp
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <gtest/gtest.h>
+
+#include "verify/VerifyUtil.h"
+
+TEST_F(PreVerify, test) {
+  ASSERT_NE(find_class_named(classes, "Lcom/facebook/redextest/I_1;"), nullptr);
+  ASSERT_NE(find_class_named(classes, "Lcom/facebook/redextest/I_2;"), nullptr);
+  ASSERT_NE(find_class_named(classes, "Lcom/facebook/redextest/I_3;"), nullptr);
+}
+
+TEST_F(PostVerify, test) {
+  ASSERT_NE(find_class_named(classes, "Lcom/facebook/redextest/I_1;"), nullptr);
+  ASSERT_EQ(find_class_named(classes, "Lcom/facebook/redextest/I_2;"), nullptr);
+  ASSERT_EQ(find_class_named(classes, "Lcom/facebook/redextest/I_3;"), nullptr);
+}
