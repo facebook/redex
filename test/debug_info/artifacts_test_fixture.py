02b0fbde3b310972caa17601dd08f97a8626bae0
Yuxuan Chen <ych@fb.com>
2019-05-31 09:09:34 -0700

    add debug info test for redex generated artifacts
    Summary: I had to rename debug-info to debug_info because python import supports directory names that are python identifiers.
    
    Reviewed By: int3
    
    Differential Revision: D15539449
    
    fbshipit-source-id: e4f7844970e6b84b25e7471730f3905c966022af

---
+++ test/debug_info/artifacts_test_fixture.py
@@ -0,0 +1,50 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import json
+import os
+import shutil
+import subprocess
+import tempfile
+import unittest
+
+
+class ArtifactsTestFixture(unittest.TestCase):
+    def setUp(self):
+        self.config = {"redex": {"passes": ["RegAllocPass"]}}
+        self.tmp = tempfile.mkdtemp()
+
+    def tearDown(self):
+        shutil.rmtree(self.tmp)
+
+    def run_redex(self):
+        config_file = os.path.join(self.tmp, "config")
+        with open(config_file, "w") as f:
+            json.dump(self.config, f)
+
+        subprocess.check_call(
+            [
+                os.environ["REDEX_SCRIPT"],
+                "-P",
+                os.environ["PG_CONFIG"],
+                "-c",
+                config_file,
+                "-j",
+                os.environ["SDK_PATH"] + "platforms/android-25/android.jar",
+                "--redex-binary",
+                os.environ["REDEX_BINARY"],
+                "-o",
+                os.path.join(self.tmp, "out.apk"),
+                os.environ["INPUT_APK"],
+            ]
+        )
+
+    def assert_artifact_exists(self, filename):
+        path = os.path.join(self.tmp, filename)
+        self.assertTrue(os.path.exists(path), "%s does not exist" % filename)
+
+    def assert_artifact_does_not_exist(self, filename):
+        path = os.path.join(self.tmp, filename)
+        self.assertFalse(os.path.exists(path), "%s exists" % filename)
--- test/debug_info/test_debug_info_kind.py
+++ test/debug_info/test_debug_info_kind.py
@@ -3,16 +3,10 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-import json
-import os
-import shutil
-import subprocess
-import tempfile
-import unittest
+from native.redex.test.debug_info.artifacts_test_fixture import ArtifactsTestFixture
 
 
-class TestDebugInfoKind(unittest.TestCase):
-
+class TestDebugInfoKind(ArtifactsTestFixture):
     """
     This test suite checks that Redex emits the right symbol files for each
     possible setting of debug_info_kind.
@@ -32,41 +26,12 @@ class TestDebugInfoKind(unittest.TestCase):
         METHOD_MAPPING,
     }
 
-    def setUp(self):
-        self.config = {"redex": {"passes": ["RegAllocPass"]}}
-        self.tmp = tempfile.mkdtemp()
-
-    def tearDown(self):
-        shutil.rmtree(self.tmp)
-
-    def run_redex(self):
-        config_file = os.path.join(self.tmp, "config")
-        with open(config_file, "w") as f:
-            json.dump(self.config, f)
-
-        subprocess.check_call(
-            [
-                os.environ["REDEX_SCRIPT"],
-                "-P",
-                os.environ["PG_CONFIG"],
-                "-c",
-                config_file,
-                "--redex-binary",
-                os.environ["REDEX_BINARY"],
-                "-o",
-                os.path.join(self.tmp, "out.apk"),
-                os.environ["INPUT_APK"],
-            ]
-        )
-
     def check_artifacts(self, should_exist):
         for fn in should_exist:
-            path = os.path.join(self.tmp, fn)
-            self.assertTrue(os.path.exists(path), "%s does not exist" % fn)
+            self.assert_artifact_exists(fn)
 
         for fn in self.POSSIBLE_SYMBOL_FILES - should_exist:
-            path = os.path.join(self.tmp, fn)
-            self.assertFalse(os.path.exists(path), "%s exists" % fn)
+            self.assert_artifact_does_not_exist(fn)
 
     def test_no_custom_symbolication(self):
         self.run_redex()
