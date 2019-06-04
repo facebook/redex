972e9a15eeb4b16c10dc0a2584aaa8f75350cf96
Jez Ng <jezng@fb.com>
2019-06-04 10:38:55 -0700

    Fix FinalInline's mis-encoding of static field values
    Summary:
    FinalInlineV2 was taking the values of the fields at <clinit> exit and making
    them the encoded values of those fields. Then it would re-run constant
    propagation on the <clinit> which would typically discover that the sputs that
    wrote to those fields were now redundant, since they were writing the values
    identical to the encoded ones.
    
    The problem was that sometimes the clinit would read the existing encoded
    values before writing a new value into the field. Not accounting for this meant
    that the field read would now obtain a different value after FinalInline ran.
    
    The "full" fix for this would be to avoid encoding values for fields whose
    sgets are not all dominated by sputs. But since it's rare to have fields read
    in their own class' static initializers, I'm just blacklisting all such fields.
    
    Differential Revision: D15590869
    
    fbshipit-source-id: 4225c84c1dea3a639f4ade341b62c96edf8d4aa9

--- test/unit/FinalInlineV2Test.cpp
+++ test/unit/FinalInlineV2Test.cpp
@@ -13,16 +13,29 @@
 #include "IRCode.h"
 #include "RedexTest.h"
 
-struct FinalInlineTest : public RedexTest {};
+struct FinalInlineTest : public RedexTest {
+ public:
+  void SetUp() override {
+    m_cc = std::make_unique<ClassCreator>(DexType::make_type("LFoo;"));
+    m_cc->set_super(get_object_type());
+  }
+
+ protected:
+  DexField* create_field_with_value(const char* name, uint32_t value) {
+    auto field = static_cast<DexField*>(DexField::make_field(name));
+    auto encoded_value = DexEncodedValue::zero_for_type(get_int_type());
+    encoded_value->value(value);
+    field->make_concrete(ACC_PUBLIC | ACC_STATIC, encoded_value);
+    m_cc->add_field(field);
+    return field;
+  }
+
+  std::unique_ptr<ClassCreator> m_cc;
+};
 
 TEST_F(FinalInlineTest, encodeValues) {
-  ClassCreator cc(DexType::make_type("LFoo;"));
-  cc.set_super(get_object_type());
-  auto field_bar = static_cast<DexField*>(DexField::make_field("LFoo;.bar:I"));
-  field_bar->make_concrete(ACC_PUBLIC | ACC_STATIC,
-                           DexEncodedValue::zero_for_type(get_int_type()));
-  cc.add_field(field_bar);
-  cc.add_method(assembler::method_from_string(R"(
+  auto field = create_field_with_value("LFoo;.bar:I", 0);
+  m_cc->add_method(assembler::method_from_string(R"(
     (method (public static) "LFoo;.<clinit>:()V"
      (
       (const v0 1)
@@ -31,10 +44,101 @@ TEST_F(FinalInlineTest, encodeValues) {
      )
     )
   )"));
-  auto cls = cc.create();
+  auto cls = m_cc->create();
 
   FinalInlinePassV2::run({cls});
 
   EXPECT_EQ(cls->get_clinit(), nullptr);
-  EXPECT_EQ(field_bar->get_static_value()->value(), 1);
+  EXPECT_EQ(field->get_static_value()->value(), 1);
+}
+
+TEST_F(FinalInlineTest, fieldSetInLoop) {
+  auto field_bar = create_field_with_value("LFoo;.bar:I", 0);
+  m_cc->add_method(assembler::method_from_string(R"(
+    (method (public static) "LFoo;.<clinit>:()V"
+     (
+      (:loop)
+      (sget "LFoo;.bar:I")
+      (move-result-pseudo v0)
+      (add-int/lit8 v0 v0 1)
+      (sput v0 "LFoo;.bar:I")
+      (const v1 10)
+      (if-ne v0 v1 :loop)
+      (return-void)
+     )
+    )
+  )"));
+  auto cls = m_cc->create();
+
+  auto original = assembler::to_s_expr(cls->get_clinit()->get_code());
+  FinalInlinePassV2::run({cls});
+  EXPECT_EQ(assembler::to_s_expr(cls->get_clinit()->get_code()), original);
+  EXPECT_EQ(field_bar->get_static_value()->value(), 0);
+}
+
+TEST_F(FinalInlineTest, fieldConditionallySet) {
+  auto field_bar = create_field_with_value("LFoo;.bar:I", 0);
+  auto field_baz = create_field_with_value("LFoo;.baz:I", 0);
+  m_cc->add_method(assembler::method_from_string(R"(
+    (method (public static) "LFoo;.<clinit>:()V"
+     (
+      (sget "LUnknown;.field:I")
+      (move-result-pseudo v0)
+      (if-eqz v0 :true)
+      (const v1 1)
+      (sput v1 "LFoo;.bar:I")
+      (:true)
+      ; bar may be 0 or 1 here
+      (sget "LFoo;.bar:I")
+      (move-result-pseudo v0)
+      (sput v0 "LFoo;.baz:I")
+      (sput v1 "LFoo;.bar:I")
+      ; bar is always 1 on exit
+      (return-void)
+     )
+    )
+  )"));
+  auto cls = m_cc->create();
+
+  auto original = assembler::to_s_expr(cls->get_clinit()->get_code());
+  FinalInlinePassV2::run({cls});
+  EXPECT_EQ(assembler::to_s_expr(cls->get_clinit()->get_code()), original);
+  EXPECT_EQ(field_bar->get_static_value()->value(), 0);
+  EXPECT_EQ(field_baz->get_static_value()->value(), 0);
+}
+
+TEST_F(FinalInlineTest, dominatedSget) {
+  auto field_bar = create_field_with_value("LFoo;.bar:I", 0);
+  auto field_baz = create_field_with_value("LFoo;.baz:I", 0);
+  m_cc->add_method(assembler::method_from_string(R"(
+    (method (public static) "LFoo;.<clinit>:()V"
+     (
+      (const v0 1)
+      (sput v0 "LFoo;.bar:I")
+      (sget "LFoo;.bar:I")
+      (move-result-pseudo v0)
+      (sput v0 "LFoo;.baz:I")
+      (return-void)
+     )
+    )
+  )"));
+  auto cls = m_cc->create();
+
+  // This could be further optimized to remove the sput to the field bar. This
+  // test illustrates that we are being overly conservative if a field is
+  // ever read in its <clinit>. In practice though this rarely occurs.
+  auto expected = assembler::ircode_from_string(R"(
+    (
+      (const v0 1)
+      (sput v0 "LFoo;.bar:I")
+      (return-void)
+    )
+  )");
+
+  auto original = assembler::to_s_expr(cls->get_clinit()->get_code());
+  FinalInlinePassV2::run({cls});
+  EXPECT_EQ(assembler::to_s_expr(cls->get_clinit()->get_code()),
+            assembler::to_s_expr(expected.get()));
+  EXPECT_EQ(field_bar->get_static_value()->value(), 0);
+  EXPECT_EQ(field_baz->get_static_value()->value(), 1);
 }
