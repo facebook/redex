eab39608733107bf3ea450567853eb32832236cc
Yuxuan Chen <ych@fb.com>
2019-07-24 15:48:06 -0700

    Adding test for method param reflection analysis
    Summary: Test for the refined reflection analysis
    
    Reviewed By: emma0303
    
    Differential Revision: D16353323
    
    fbshipit-source-id: 03cc27710a7151c5a67187b242adc5a7bb2dec50

--- test/instr/ReflectionAnalysisTest.java
+++ test/instr/ReflectionAnalysisTest.java
@@ -13,16 +13,23 @@
 package com.facebook.redextest;
 
 import org.junit.Test;
+import java.lang.Integer;
+import java.lang.reflect.Method;
 
 import static org.fest.assertions.api.Assertions.assertThat;
+import androidx.annotation.Nullable;
 
 public class ReflectionAnalysisTest {
+  private static Class[] sClassArray;
 
   class Foo {}
   class Bar {}
+  class Baz {}
 
   class Reflector {
     Class<? extends Foo> mFooClazz;
+
+    private Class[] mClassArray;
 
     String getClassJoinSame(boolean flag) throws ClassNotFoundException {
       Class cls = null;
@@ -88,6 +95,71 @@ public class ReflectionAnalysisTest {
         str = "";
       }
       return Class.forName(str);
+    }
+
+    @Nullable
+    Method getMethodWithParam() {
+      Class<?> baz = Baz.class;
+      Class[] empty = new Class[0];
+      Class[] types = new Class[2];
+      types[0] = Integer.class;
+      types[1] = double.class;
+      Method m1 = null;
+      Method m2 = null;
+      Method m3 = null;
+      try {
+        m1 = baz.getDeclaredMethod("test", empty);
+        m2 = baz.getDeclaredMethod("test", types);
+        m3 = baz.getDeclaredMethod("test2", types);
+      } catch (NoSuchMethodException e) { return null; }
+      return m1;
+    }
+
+    private void updateClassArray(Class[] array) {}
+
+    Method getMethodWithParamOriginal() throws NoSuchMethodException {
+      Class<?> baz = Baz.class;
+      Class[] types = new Class[2];
+      types[0] = Integer.class;
+      types[1] = double.class;
+      return baz.getDeclaredMethod("test", types);
+    }
+
+    Method getMethodWithParamInvalidatedArgs1() throws NoSuchMethodException {
+      Class<?> baz = Baz.class;
+      Class[] types = new Class[2];
+      types[0] = Integer.class;
+      types[1] = double.class;
+      mClassArray = types; // invalidate
+      return baz.getDeclaredMethod("test", types);
+    }
+
+    Method getMethodWithParamInvalidatedArgs2() throws NoSuchMethodException {
+      Class<?> baz = Baz.class;
+      Class[] types = new Class[2];
+      types[0] = Integer.class;
+      types[1] = double.class;
+      sClassArray = types; // invalidate
+      return baz.getDeclaredMethod("test", types);
+    }
+
+    Method getMethodWithParamInvalidatedArgs3() throws NoSuchMethodException {
+      Class<?> baz = Baz.class;
+      Class[] types = new Class[2];
+      types[0] = Integer.class;
+      types[1] = double.class;
+      updateClassArray(types); // invalidate
+      return baz.getDeclaredMethod("test", types);
+    }
+
+    Method getMethodWithParamInvalidatedArgs4() throws NoSuchMethodException {
+      Class<?> baz = Baz.class;
+      Class[] types = new Class[2];
+      types[0] = Integer.class;
+      types[1] = double.class;
+      Class[][] someArray = new Class[1][];
+      someArray[0] = types; // invalidate
+      return baz.getDeclaredMethod("test", types);
     }
   }
 
