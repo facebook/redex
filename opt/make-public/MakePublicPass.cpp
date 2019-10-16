f90d66902c45db658c159ee3a85aff493c3bb9b8
Sushan Jiang <suree404@fb.com>
2019-10-16 07:19:20 -0700

    Add a UnpackagePrivate pass
    Summary: Move unpackage private from RenameClassnamePass into libredex, and create a pass that just do that.
    
    Differential Revision: D17935069
    
    fbshipit-source-id: cdb8d3fd28bdcb3ae3f63cdeb13d1182211bbcb5

---
+++ opt/make-public/MakePublicPass.cpp
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include "MakePublicPass.h"
+
+#include "DexUtil.h"
+#include "UnpackagePrivate.h"
+
+void MakePublicPass::run_pass(DexStoresVector& stores,
+                              ConfigFiles& conf,
+                              PassManager& mgr) {
+  auto scope = build_class_scope(stores);
+  unpackage_private(scope);
+}
+
+static MakePublicPass s_pass;
