f46a3ddef33f9f734e3d1be845e6722e57d4d53b
Sumeet Arora <sumeetsingh@fb.com>
2019-10-18 15:43:46 -0700

    Revert D17849917: [redex][easy] Remove old StaticReloPass
    Differential Revision:
    D17849917
    
    Original commit changeset: 9e19ac320621
    
    fbshipit-source-id: 7f3c3de731b5d3b3424cab2e0ebb949ebe4e75c4

---
+++ opt/staticrelo/StaticRelo.h
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include "PassManager.h"
+#include "DexClass.h"
+
+class StaticReloPass : public Pass {
+ public:
+  StaticReloPass() : Pass("StaticReloPass") {}
+
+  void bind_config() override {
+    bind("dont_optimize_annos", {}, m_dont_optimize_annos);
+  }
+
+  void run_pass(DexStoresVector&, ConfigFiles&, PassManager&) override;
+
+ private:
+  std::vector<std::string> m_dont_optimize_annos;
+};
