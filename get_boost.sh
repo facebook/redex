f606f4c68d9406d9409d6f0a3a5afd7bb8fe5f0f
Andres Suarez <asuarez@fb.com>
2019-10-22 17:15:55 -0700

    Yearless license header
    Reviewed By: ssj933
    
    Differential Revision: D18061022
    
    fbshipit-source-id: 5f678a2029df7f211e6c6334fc19db9aea82f612

--- get_boost.sh
+++ get_boost.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 set -e
 
 wget https://dl.bintray.com/boostorg/release/1.64.0/source/boost_1_64_0.tar.bz2
--- tools/oatmeal/test/update-test-data.sh
+++ tools/oatmeal/test/update-test-data.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -e
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
 
 # Runs the regtest, overwriting previous output, and prompts user with diffs
 # showing the changes.
