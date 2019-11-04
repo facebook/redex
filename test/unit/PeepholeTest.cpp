861268b124fd2f79a3b537a63dfad10b118e9446
Andreas Gampe <agampe@fb.com>
2019-11-04 10:17:57 -0800

    Fix up PeepholeTest
    Summary: Refactor prior unified test into separate test cases. Refactor infrastructure to be shared.
    
    Reviewed By: int3
    
    Differential Revision: D18275934
    
    fbshipit-source-id: cc34e220096674858aacb150fbd9efaabeb97a55

--- test/unit/PeepholeTest.cpp
+++ test/unit/PeepholeTest.cpp
@@ -7,6 +7,9 @@
 
 #include <gtest/gtest.h>
 
+#include <atomic>
+#include <sstream>
+
 #include "Creators.h"
 #include "DexAsm.h"
 #include "DexUtil.h"
@@ -17,119 +20,15 @@
 #include "RedexTest.h"
 #include "ScopeHelper.h"
 
-class PeepholeTestB : public RedexTest {};
-
-TEST_F(PeepholeTestB, StringBuilderInit) {
-  ClassCreator creator(DexType::make_type("LFoo;"));
-  creator.set_super(known_types::java_lang_Object());
-  auto method_1 = DexMethod::make_method("LFoo;.b:()V")
-                      ->make_concrete(ACC_PUBLIC | ACC_STATIC, false);
-  auto original_code_1 = R"(
-     (
-      (new-instance "Ljava/lang/StringBuilder;")
-      (move-result-pseudo-object v0)
-      (invoke-direct (v0) "Ljava/lang/StringBuilder;.<init>:()V")
-      (const-string "foo")
-      (move-result-pseudo-object v1)
-      (invoke-virtual (v0 v1) "Ljava/lang/StringBuilder;.append:(Ljava/lang/String;)Ljava/lang/StringBuilder;")
-      (move-result-object v0)
-      (return-void)
-     )
-    )";
-  method_1->set_code(assembler::ircode_from_string(original_code_1));
-  creator.add_method(method_1);
-
-  auto method_2 = DexMethod::make_method("LFoo;.c:()V")
-                      ->make_concrete(ACC_PUBLIC | ACC_STATIC, false);
-  auto original_code_2 = R"(
-    (
-     (new-instance "Ljava/lang/StringBuilder;")
-     (move-result-pseudo-object v0)
-     (invoke-direct (v0) "Ljava/lang/StringBuilder;.<init>:()V")
-     (const-string "foo")
-     (move-result-pseudo-object v1)
-     (invoke-virtual (v0 v1) "Ljava/lang/StringBuilder;.append:(Ljava/lang/String;)Ljava/lang/StringBuilder;")
-     (return-void)
-    )
-   )";
-  method_2->set_code(assembler::ircode_from_string(original_code_2));
-  creator.add_method(method_2);
-
-  auto method_3 = DexMethod::make_method("LFoo;.d:()V")
-                      ->make_concrete(ACC_PUBLIC | ACC_STATIC, false);
-  auto original_code_3 = R"(
-    (
-     (new-instance "Ljava/lang/StringBuilder;")
-     (move-result-pseudo-object v0)
-     (invoke-direct (v0) "Ljava/lang/StringBuilder;.<init>:()V")
-     (const-string "foo")
-     (move-result-pseudo-object v1)
-     (invoke-virtual (v0 v1) "Ljava/lang/StringBuilder;.append:(Ljava/lang/String;)Ljava/lang/StringBuilder;")
-     (move-result-object v2)
-     (return-void)
-    )
-   )";
-  method_3->set_code(assembler::ircode_from_string(original_code_3));
-  creator.add_method(method_3);
-
-  PeepholePass peephole_pass;
-  PassManager manager({&peephole_pass});
-  ConfigFiles config(Json::nullValue);
-  DexStore store("classes");
-  store.add_classes({creator.create()});
-  std::vector<DexStore> stores;
-  stores.emplace_back(std::move(store));
-  manager.run_passes(stores, config);
-  TRACE(PEEPHOLE, 1, "\n\n\n\nfinished\n\n\n");
-  auto expected_code_1 = assembler::ircode_from_string(R"(
-       (
-        (new-instance "Ljava/lang/StringBuilder;")
-        (move-result-pseudo-object v0)
-        (const-string "foo")
-        (move-result-pseudo-object v1)
-        (invoke-direct (v0 v1) "Ljava/lang/StringBuilder;.<init>:(Ljava/lang/String;)V")
-        (return-void)
-       )
-     )");
-  EXPECT_EQ(assembler::to_s_expr(method_1->get_code()),
-            assembler::to_s_expr(expected_code_1.get()));
-  auto expected_code_2 = assembler::ircode_from_string(R"(
-        (
-         (new-instance "Ljava/lang/StringBuilder;")
-         (move-result-pseudo-object v0)
-         (const-string "foo")
-         (move-result-pseudo-object v1)
-         (invoke-direct (v0 v1) "Ljava/lang/StringBuilder;.<init>:(Ljava/lang/String;)V")
-         (return-void)
-        )
-      )");
-
-  EXPECT_EQ(assembler::to_s_expr(method_2->get_code()),
-            assembler::to_s_expr(expected_code_2.get()));
-
-  auto expected_code_3 = assembler::ircode_from_string(R"(
-         (
-          (new-instance "Ljava/lang/StringBuilder;")
-          (move-result-pseudo-object v0)
-          (const-string "foo")
-          (move-result-pseudo-object v1)
-          (invoke-direct (v0 v1) "Ljava/lang/StringBuilder;.<init>:(Ljava/lang/String;)V")
-          (move-object v2 v0)
-          (return-void)
-         )
-       )");
-
-  EXPECT_EQ(assembler::to_s_expr(method_3->get_code()),
-            assembler::to_s_expr(expected_code_3.get()));
-}
-
-class PeepholeTestNPE : public RedexTest {
+class PeepholeTest : public RedexTest {
  public:
   ::sparta::s_expr run_peephole_pass(const std::string& code) {
-    ClassCreator creator(DexType::make_type("LFoo;"));
+    auto class_name = next_class();
+    ClassCreator creator(DexType::make_type(class_name.c_str()));
     creator.set_super(known_types::java_lang_Object());
-    auto method = DexMethod::make_method("LFoo;.b:()V")
-                      ->make_concrete(ACC_PUBLIC | ACC_STATIC, false);
+    auto signature = class_name + ".foo:()V";
+    auto method = DexMethod::make_method(signature)->make_concrete(
+        ACC_PUBLIC | ACC_STATIC, false);
     method->set_code(assembler::ircode_from_string(code));
     creator.add_method(method);
 
@@ -148,9 +47,117 @@ class PeepholeTestNPE : public RedexTest {
   ::sparta::s_expr get_s_expr(const std::string& code) {
     return assembler::to_s_expr(assembler::ircode_from_string(code).get());
   }
+
+  ::testing::AssertionResult run_test(const std::string& input,
+                                      const std::string& expected) {
+    auto peepholed_s_expr = run_peephole_pass(input);
+    auto expected_s_expr = get_s_expr(expected);
+    if (peepholed_s_expr == expected_s_expr) {
+      return ::testing::AssertionSuccess();
+    }
+    return ::testing::AssertionFailure()
+           << input << "\nevaluates to\n"
+           << peepholed_s_expr.str() << "\ninstead of\n"
+           << expected_s_expr.str();
+  }
+
+ private:
+  // Generate unique class names, as tests may be run in parallel.
+  static std::string next_class() {
+    size_t cur = count_.fetch_add(1u);
+    std::ostringstream oss;
+    oss << "LFoo" << cur << ";";
+    return oss.str();
+  }
+
+  static std::atomic<size_t> count_;
 };
 
-TEST_F(PeepholeTestNPE, ThrowNPEEmpty) {
+std::atomic<size_t> PeepholeTest::count_{0};
+
+class PeepholeStringBuilderTest : public PeepholeTest {};
+
+TEST_F(PeepholeStringBuilderTest, ReduceEmptyInitMoveResultSame) {
+  auto original_code = R"(
+     (
+      (new-instance "Ljava/lang/StringBuilder;")
+      (move-result-pseudo-object v0)
+      (invoke-direct (v0) "Ljava/lang/StringBuilder;.<init>:()V")
+      (const-string "foo")
+      (move-result-pseudo-object v1)
+      (invoke-virtual (v0 v1) "Ljava/lang/StringBuilder;.append:(Ljava/lang/String;)Ljava/lang/StringBuilder;")
+      (move-result-object v0)
+      (return-void)
+     )
+    )";
+  auto expected_code = R"(
+     (
+      (new-instance "Ljava/lang/StringBuilder;")
+      (move-result-pseudo-object v0)
+      (const-string "foo")
+      (move-result-pseudo-object v1)
+      (invoke-direct (v0 v1) "Ljava/lang/StringBuilder;.<init>:(Ljava/lang/String;)V")
+      (return-void)
+     )
+    )";
+  ASSERT_TRUE(run_test(original_code, expected_code));
+}
+
+TEST_F(PeepholeStringBuilderTest, ReduceEmptyInitNoMoveResult) {
+  auto original_code = R"(
+     (
+      (new-instance "Ljava/lang/StringBuilder;")
+      (move-result-pseudo-object v0)
+      (invoke-direct (v0) "Ljava/lang/StringBuilder;.<init>:()V")
+      (const-string "foo")
+      (move-result-pseudo-object v1)
+      (invoke-virtual (v0 v1) "Ljava/lang/StringBuilder;.append:(Ljava/lang/String;)Ljava/lang/StringBuilder;")
+      (return-void)
+     )
+    )";
+  auto expected_code = R"(
+     (
+      (new-instance "Ljava/lang/StringBuilder;")
+      (move-result-pseudo-object v0)
+      (const-string "foo")
+      (move-result-pseudo-object v1)
+      (invoke-direct (v0 v1) "Ljava/lang/StringBuilder;.<init>:(Ljava/lang/String;)V")
+      (return-void)
+     )
+    )";
+  ASSERT_TRUE(run_test(original_code, expected_code));
+}
+
+TEST_F(PeepholeStringBuilderTest, ReduceEmptyInitMoveResultOther) {
+  auto original_code = R"(
+     (
+      (new-instance "Ljava/lang/StringBuilder;")
+      (move-result-pseudo-object v0)
+      (invoke-direct (v0) "Ljava/lang/StringBuilder;.<init>:()V")
+      (const-string "foo")
+      (move-result-pseudo-object v1)
+      (invoke-virtual (v0 v1) "Ljava/lang/StringBuilder;.append:(Ljava/lang/String;)Ljava/lang/StringBuilder;")
+      (move-result-object v2)
+      (return-void)
+     )
+    )";
+  auto expected_code = R"(
+     (
+      (new-instance "Ljava/lang/StringBuilder;")
+      (move-result-pseudo-object v0)
+      (const-string "foo")
+      (move-result-pseudo-object v1)
+      (invoke-direct (v0 v1) "Ljava/lang/StringBuilder;.<init>:(Ljava/lang/String;)V")
+      (move-object v2 v0)
+      (return-void)
+     )
+    )";
+  ASSERT_TRUE(run_test(original_code, expected_code));
+}
+
+class PeepholeNPETest : public PeepholeTest {};
+
+TEST_F(PeepholeNPETest, ThrowNPEEmpty) {
   auto original_code = R"(
      (
       (new-instance "Ljava/lang/NullPointerException;")
@@ -159,18 +166,16 @@ TEST_F(PeepholeTestNPE, ThrowNPEEmpty) {
       (throw v0)
      )
     )";
-  auto peepholed_s_expr = run_peephole_pass(original_code);
   auto expected_code = R"(
      (
       (const v0 0)
       (throw v0)
      )
     )";
-  auto expected_s_expr = get_s_expr(expected_code);
-  ASSERT_EQ(peepholed_s_expr, expected_s_expr);
+  ASSERT_TRUE(run_test(original_code, expected_code));
 }
 
-TEST_F(PeepholeTestNPE, ThrowNPENotEmpty) {
+TEST_F(PeepholeNPETest, ThrowNPENotEmpty) {
   auto original_code = R"(
      (
       (const-string "Test")
@@ -181,12 +186,10 @@ TEST_F(PeepholeTestNPE, ThrowNPENotEmpty) {
       (throw v0)
      )
     )";
-  auto peepholed_s_expr = run_peephole_pass(original_code);
-  auto expected_s_expr = get_s_expr(original_code);
-  ASSERT_EQ(peepholed_s_expr, expected_s_expr);
+  ASSERT_TRUE(run_test(original_code, original_code));
 }
 
-TEST_F(PeepholeTestNPE, ThrowNonNPEVerifiable) {
+TEST_F(PeepholeNPETest, ThrowNonNPEVerifiable) {
   auto original_code = R"(
      (
       (new-instance "Ljava/lang/IllegalArgumentException;")
@@ -197,10 +200,10 @@ TEST_F(PeepholeTestNPE, ThrowNonNPEVerifiable) {
     )";
   auto peepholed_s_expr = run_peephole_pass(original_code);
   auto expected_s_expr = get_s_expr(original_code);
-  ASSERT_EQ(peepholed_s_expr, expected_s_expr) << peepholed_s_expr.str();
+  ASSERT_TRUE(run_test(original_code, original_code));
 }
 
-TEST_F(PeepholeTestNPE, ThrowNonNPENotVerifiable) {
+TEST_F(PeepholeNPETest, ThrowNonNPENotVerifiable) {
   auto original_code = R"(
      (
       (new-instance "Ljava/lang/IllegalArgumentException;")
@@ -211,10 +214,10 @@ TEST_F(PeepholeTestNPE, ThrowNonNPENotVerifiable) {
     )";
   auto peepholed_s_expr = run_peephole_pass(original_code);
   auto expected_s_expr = get_s_expr(original_code);
-  ASSERT_EQ(peepholed_s_expr, expected_s_expr);
+  ASSERT_TRUE(run_test(original_code, original_code));
 }
 
-TEST_F(PeepholeTestNPE, ThrowNPEBasicBlock) {
+TEST_F(PeepholeNPETest, ThrowNPEBasicBlock) {
   auto original_code = R"(
      (
       (const v1 0)
@@ -233,7 +236,5 @@ TEST_F(PeepholeTestNPE, ThrowNPEBasicBlock) {
       (throw v0)
      )
     )";
-  auto peepholed_s_expr = run_peephole_pass(original_code);
-  auto expected_s_expr = get_s_expr(original_code);
-  ASSERT_EQ(peepholed_s_expr, expected_s_expr);
+  ASSERT_TRUE(run_test(original_code, original_code));
 }
