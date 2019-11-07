94c2b285f0ae89026623b6c2fe71a4e5b4e28d7e
Ashok Menon <ashokmenon@fb.com>
2019-11-07 11:50:46 -0800

    Introducing CFGMutation
    Summary:
    A class to batch modifications to the ControlFlowGraph.  This allows changes to
    be gathered whilst iterating over the instruction stream where modifying during
    iteration could have caused an iterator invalidation.
    
    Reviewed By: justinjhendrick
    
    Differential Revision: D18276520
    
    fbshipit-source-id: b6e3af9389d6f78120a92e5f6bc3822a224e7f29

---
+++ libredex/CFGMutation.cpp
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include "CFGMutation.h"
+
+namespace cfg {
+
+void CFGMutation::flush() {
+  auto ii = InstructionIterable(m_cfg);
+  for (auto it = ii.begin(); !it.is_end();) {
+    auto c = m_changes.find(it->insn);
+    if (c == m_changes.end()) {
+      // No change anchored at this instruction.
+      ++it;
+      continue;
+    }
+
+    auto& change = c->second;
+    change.apply(m_cfg, it);
+
+    // The anchor can be encountered again.  Erase the change to avoid it being
+    // applied again.
+    m_changes.erase(c);
+  }
+
+  // The effect of one change can erase the anchor for another.  The changes
+  // left behind are the ones whose anchors were removed. They will never be
+  // applied so clear them.
+  m_changes.clear();
+}
+
+void CFGMutation::ChangeSet::apply(ControlFlowGraph& cfg,
+                                   InstructionIterator& it) {
+  // Save in case of iterator invalidation.
+  Block* b = it.block();
+
+  bool invalidated = false;
+  switch (m_where) {
+  case CFGMutation::Insert::Before:
+    invalidated = cfg.insert_before(it, m_instructions);
+    break;
+  case CFGMutation::Insert::After:
+    invalidated = cfg.insert_after(it, m_instructions);
+    break;
+  case CFGMutation::Insert::Replacing: {
+    // The target will be invalidated by the replacement, increment the iterator
+    // in preparation.
+    auto target = it++;
+
+    if (target->insn->has_move_result_any() &&
+        cfg.move_result_of(target) == it) {
+      // The iterator is now sitting over the target's move-result, which is
+      // also going to be invalidated, increment again.
+      ++it;
+    }
+
+    invalidated = cfg.replace_insns(target, m_instructions);
+    break;
+  }
+  }
+
+  if (invalidated) {
+    // move iterator to the end of the last iterator's block to avoid missing
+    // any instructions.
+    it = b->to_cfg_instruction_iterator(b->end());
+  }
+}
+
+} // namespace cfg
---
+++ libredex/CFGMutation.h
@@ -0,0 +1,109 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include "ControlFlow.h"
+#include "IRInstruction.h"
+
+#include <unordered_map>
+#include <vector>
+
+namespace cfg {
+
+/// Gathers requests to insert \c IRInstructions into a \c ControlFlowGraph that
+/// can be flushed out in batches.  This offers an alternative to modifying the
+/// IR in a CFG whilst iterating over its instructions which is not supported in
+/// general as a modification to the IR could invalidate the iterator.
+class CFGMutation {
+ public:
+  enum class Insert { Before, After, Replacing };
+
+  /// Create a new mutation to apply to \p cfg.
+  CFGMutation(ControlFlowGraph& cfg);
+
+  /// Any changes remaining in the buffer will be flushed when the mutation is
+  /// destroyed.
+  ~CFGMutation();
+
+  /// CFGMutation is not copyable
+  CFGMutation(const CFGMutation&) = delete;
+  CFGMutation& operator=(const CFGMutation&) = delete;
+
+  /// CFGMutation is moveable
+  CFGMutation(CFGMutation&&) = default;
+  CFGMutation& operator=(CFGMutation&&) = default;
+
+  /// Add a new change to this mutation.
+  ///
+  /// \p where indicates where to add the  \p instructions, relative to the
+  ///     anchor.  \c Before means preserve the anchor instruction and add the
+  ///     instructions before it.  \c After means preserve the anchor
+  ///     instruction and add the instructions after it.  \c Replacing behaves
+  ///     like either \c Before or \c After followed by removing the anchor
+  ///     instruction.
+  /// \p anchor is the instruction that the change is made relative to.  If at
+  ///     the time the change is applied, the anchor does not exist, the change
+  ///     will be ignored.
+  /// \p instructions are the instructions that are inserted as part of the
+  ///     change.  This can be an empty list.
+  ///
+  /// \pre The anchor iterator must be dereferenceable (i.e. not \c end() ).
+  /// \pre This mutation must not have any other change associated with \p
+  ///     anchor.
+  void add_change(Insert where,
+                  const cfg::InstructionIterator& anchor,
+                  std::vector<IRInstruction*> instructions);
+
+  /// Apply all the changes that have been added since the last flush (or since
+  /// the mutation was created).  Changes are applied in the order they are
+  /// added to the mutation.
+  void flush();
+
+ private:
+  /// A memento of a change we wish to make to the CFG.
+  class ChangeSet {
+   public:
+    ChangeSet(Insert where, std::vector<IRInstruction*> instructions);
+
+    /// Apply this change on the control flow graph \p cfg, using \p it as the
+    /// anchoring instruction. Moves \p it if the change invalidates the anchor.
+    ///
+    ///  - The iterator is guaranteed not to be moved past the first instruction
+    ///    after the anchor's initial position.
+    ///  - Note the iterator may not be moved at all, even if the change is
+    ///    applied.
+    void apply(ControlFlowGraph& cfg, InstructionIterator& it);
+
+   private:
+    Insert m_where;
+    std::vector<IRInstruction*> m_instructions;
+  };
+
+  cfg::ControlFlowGraph& m_cfg;
+  std::unordered_map<IRInstruction*, ChangeSet> m_changes;
+};
+
+inline CFGMutation::CFGMutation(cfg::ControlFlowGraph& cfg) : m_cfg(cfg) {}
+
+inline CFGMutation::~CFGMutation() { flush(); }
+
+inline void CFGMutation::add_change(Insert where,
+                                    const cfg::InstructionIterator& anchor,
+                                    std::vector<IRInstruction*> instructions) {
+  always_assert(!anchor.is_end());
+  auto p = m_changes.emplace(anchor->insn,
+                             ChangeSet(where, std::move(instructions)));
+  always_assert_log(
+      p.second, "Conflicting change for anchor: %s.", SHOW(anchor->insn));
+}
+
+inline CFGMutation::ChangeSet::ChangeSet(
+    Insert where, std::vector<IRInstruction*> instructions)
+    : m_where(where), m_instructions(std::move(instructions)) {}
+
+} // namespace cfg
