82b8cb6f0fb7e4454444384d58f83722102e21d3
Nikolai Tillmann <nikolait@fb.com>
2019-11-06 23:01:47 -0800

    Bump boost to 1.66
    Reviewed By: int3
    
    Differential Revision: D18307897
    
    fbshipit-source-id: 433a6a4a4da53c074d615f793df8b84d86f6dc15

--- get_boost.sh
+++ get_boost.sh
@@ -6,8 +6,8 @@
 
 set -e
 
-wget https://dl.bintray.com/boostorg/release/1.64.0/source/boost_1_64_0.tar.bz2
-tar --bzip2 -xf boost_1_64_0.tar.bz2
-cd boost_1_64_0
+wget https://dl.bintray.com/boostorg/release/1.66.0/source/boost_1_66_0.tar.bz2
+tar --bzip2 -xf boost_1_66_0.tar.bz2
+cd boost_1_66_0
 ./bootstrap.sh --with-libraries=filesystem,iostreams,program_options,regex,system,thread
 ./b2 -d0 install
