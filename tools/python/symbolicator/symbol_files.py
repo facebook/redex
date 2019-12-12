e0dc3e9e76f979164bd146346a0423371ddd6ba4
Yuxuan Chen <ych@fb.com>
2019-12-11 16:30:09 -0800

    autoformat python scripts
    Summary: More autoformat, for python this time
    
    Reviewed By: agampe
    
    Differential Revision: D18928191
    
    fbshipit-source-id: e1a5f0f7a7361750382ca55a993f78ddde73e41c

--- tools/python/symbolicator/symbol_files.py
+++ tools/python/symbolicator/symbol_files.py
@@ -3,15 +3,14 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
+from __future__ import absolute_import, division, print_function, unicode_literals
+
 import json
 import logging
 import os
 import re
 import subprocess
+
 
 ##############################################################################
 # Util functions
