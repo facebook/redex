1054e38b09dc27dd0f124d23887389eabd8cd730
Michał Gregorczyk <michalgr@fb.com>
2020-05-12 19:28:01 -0700

    pick location of apksigner from PATH if it's not found in the sdk dir
    Summary:
    I don't have apksigned available under $ANDROID_SDK/build-tools/23.0.1/apksigner
    and as a result `//fbandroid/native/redex:fb-inject-debug` crashes. With this
    change I can run the tool with apksigner on PATH, see the test plan.
    
    Reviewed By: justinjhendrick
    
    Differential Revision: D21529509
    
    fbshipit-source-id: 61cdcaf6596199c173f1436b6787104494bbf253

--- pyredex/utils.py
+++ pyredex/utils.py
@@ -64,10 +64,17 @@ def find_android_build_tools():
     return join(build_tools, version)
 
 
+def find_apksigner():
+    candidate = join(find_android_build_tools(), "apksigner")
+    if os.path.exists(candidate):
+        return candidate
+    return shutil.which("apksigner")
+
+
 def sign_apk(keystore, keypass, keyalias, apk):
     subprocess.check_call(
         [
-            join(find_android_build_tools(), "apksigner"),
+            find_apksigner(),
             "sign",
             "--v1-signing-enabled",
             "--v2-signing-enabled",
