0232370dcc810a457968c1542b9e8a8a337049b2
Andreas Gampe <agampe@fb.com>
2020-06-05 12:41:41 -0700

    Support `-S` and `-J` in `redex-opt`
    Summary: As title. Simplifies passing parameters to passes.
    
    Reviewed By: amnn
    
    Differential Revision: D21894974
    
    fbshipit-source-id: 9d17f6938b95725ed25584d4f9ecfebc25b3a800

--- tools/redex-opt/main.cpp
+++ tools/redex-opt/main.cpp
@@ -23,6 +23,8 @@ struct Arguments {
   std::vector<std::string> pass_names;
   RedexOptions redex_options;
   std::string config_file;
+  std::vector<std::string> s_args;
+  std::vector<std::string> j_args;
 };
 
 Arguments parse_args(int argc, char* argv[]) {
@@ -41,6 +43,29 @@ Arguments parse_args(int argc, char* argv[]) {
                      po::value<std::string>(),
                      "A JSON-formatted config file to replace the one from "
                      "{input-ir}/entry.json");
+  desc.add_options()(",S",
+                     po::value<std::vector<std::string>>(), // Accumulation
+                     "-Skey=string\n"
+                     "  \tAdd a string value to the global config, overwriting "
+                     "the existing value if any\n"
+                     "    \te.g. -Smy_param_name=foo\n"
+                     "-Spass_name.key=string\n"
+                     "  \tAdd a string value to a pass"
+                     "config, overwriting the existing value if any\n"
+                     "    \te.g. -SMyPass.config=\"foo bar\"");
+  desc.add_options()(
+      ",J",
+      po::value<std::vector<std::string>>(), // Accumulation
+      "-Jkey=<json value>\n"
+      "  \tAdd a json value to the global config, overwriting the existing "
+      "value if any\n"
+      "    \te.g. -Jmy_param_name={\"foo\": true}\n"
+      "-JSomePassName.key=<json value>\n"
+      "  \tAdd a json value to a pass config, overwriting the existing value "
+      "if any\n"
+      "    \te.g. -JMyPass.config=[1, 2, 3]\n"
+      "Note: Be careful to properly escape JSON parameters, e.g., strings must "
+      "be quoted.");
 
   po::variables_map vm;
   po::store(po::parse_command_line(argc, argv, desc), vm);
@@ -79,7 +104,53 @@ Arguments parse_args(int argc, char* argv[]) {
     args.config_file = vm["config"].as<std::string>();
   }
 
+  if (vm.count("-S")) {
+    args.s_args = vm["-S"].as<std::vector<std::string>>();
+  }
+  if (vm.count("-J")) {
+    args.j_args = vm["-J"].as<std::vector<std::string>>();
+  }
+
   return args;
+}
+
+Json::Value parse_json_value(const std::string& value_string) {
+  std::istringstream temp_stream(value_string);
+  Json::Value temp_json;
+  temp_stream >> temp_json;
+  return temp_json;
+}
+
+bool add_value_to_config(Json::Value& config,
+                         const std::string& key_value,
+                         bool is_json) {
+  const size_t equals_idx = key_value.find('=');
+  if (equals_idx == std::string::npos) {
+    return false;
+  }
+
+  const size_t dot_idx = key_value.find('.');
+  if (dot_idx != std::string::npos && dot_idx < equals_idx) {
+    // Pass-specific config value specified with -Dpassname.key=value
+    std::string pass = key_value.substr(0, dot_idx);
+    std::string key = key_value.substr(dot_idx + 1, equals_idx - dot_idx - 1);
+    std::string value_string = key_value.substr(equals_idx + 1);
+    if (is_json) {
+      config[pass][key] = parse_json_value(value_string);
+    } else {
+      config[pass][key] = value_string;
+    }
+  } else {
+    // Global config value specified with -Dkey=value
+    std::string key = key_value.substr(0, equals_idx);
+    std::string value_string = key_value.substr(equals_idx + 1);
+    if (is_json) {
+      config[key] = parse_json_value(value_string);
+    } else {
+      config[key] = value_string;
+    }
+  }
+  return true;
 }
 
 /**
@@ -109,6 +180,18 @@ Json::Value process_entry_data(const Json::Value& entry_data,
   // apk_dir
   if (entry_data.isMember("apk_dir")) {
     config_data["apk_dir"] = entry_data["apk_dir"].asString();
+  }
+
+  // Include -S and -J params.
+  for (auto& key_value : args.s_args) {
+    if (!add_value_to_config(config_data, key_value, false)) {
+      std::cerr << "warning: cannot parse -S" << key_value << std::endl;
+    }
+  }
+  for (auto& key_value : args.j_args) {
+    if (!add_value_to_config(config_data, key_value, true)) {
+      std::cerr << "warning: cannot parse -S" << key_value << std::endl;
+    }
   }
 
   return config_data;
