c58876fa3fdf3c3288454c4ee0ee1a247537cb25
Andreas Gampe <agampe@fb.com>
2020-08-10 16:13:28 -0700

    Use `sign_apk` in `add_assets.py`
    Summary: Do not run custom signing. Instead, use the `pyredex` helper. This will transition to `apksigner`, and better ability to find the tooling.
    
    Reviewed By: thezhangwei
    
    Differential Revision: D23038941
    
    fbshipit-source-id: 99d5573cbee2e45a5f893e35e4ce84dd463d6444

--- test/debug_info/add_assets.py
+++ test/debug_info/add_assets.py
@@ -6,9 +6,9 @@
 
 import argparse
 import os
-import subprocess
-import sys
 import zipfile
+
+from pyredex.utils import sign_apk
 
 
 parser = argparse.ArgumentParser()
@@ -23,19 +23,4 @@ with zipfile.ZipFile(args.apk, "a") as zf:
     for asset in args.assets:
         zf.write(asset, os.path.join("assets", os.path.basename(asset)))
 
-subprocess.check_call(
-    [
-        "jarsigner",
-        "-sigalg",
-        "SHA1withRSA",
-        "-digestalg",
-        "SHA1",
-        "-keystore",
-        args.keystore,
-        "-storepass",
-        args.keypass,
-        args.apk,
-        args.keyalias,
-    ],
-    stdout=sys.stderr,
-)
+sign_apk(args.keystore, args.keypass, args.keyalias, args.apk)
