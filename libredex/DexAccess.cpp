18f35cf8fb277fd86f38bee374816a5ff8f80955
Feng Liu <fengliu@fb.com>
2020-08-27 18:04:55 -0700

    MakePublic should not publicize final methods if children exist
    Summary:
    The pass can mark final invisible methods be public and cause error "java.lang.LinkageError: Method xx overrides final method in class yyy".
    This diff does not change final invisible methods and it's "parents" methods be public to avoid the error. If other passes make access violation but could not be fixed by this pass, the access checker should catch the illegal access errors and we should fix them.
    
    Reviewed By: emma0303
    
    Differential Revision: D23363984 (https://github.com/facebook/redex/commit/eff39101c2739cfa8b9fde28205d4b1f1986a0df)
    
    fbshipit-source-id: bac2a4bf596ce6ad992d3fa65f9c6c31cc27402a

--- libredex/DexAccess.cpp
+++ libredex/DexAccess.cpp
@@ -7,18 +7,60 @@
 
 #include "DexAccess.h"
 #include "DexClass.h"
+#include "MethodOverrideGraph.h"
 #include "Walkers.h"
 
-void loosen_access_modifier(DexClass* clazz) {
+namespace {
+void overriden_should_not_be_public(
+    const DexMethod* method,
+    const method_override_graph::Graph* graph,
+    std::unordered_set<const DexMethod*>* should_not_mark) {
+  if (method->is_external()) {
+    return;
+  }
+  should_not_mark->insert(method);
+  const auto& node = graph->get_node(method);
+  for (const auto* overriden : node.parents) {
+    overriden_should_not_be_public(overriden, graph, should_not_mark);
+  }
+}
+/**
+ * XXX: Why not simply mark the virtual methods public? For a virtual method,
+ * there can be an invisible final virtual method with the same signature in the
+ * parents, they do not have overriding relationship. Change the latter method
+ * be visible to it is wrong.
+ */
+void loosen_access_modifier_for_vmethods(const Scope& scope) {
+  auto graph = method_override_graph::build_graph(scope);
+  const auto& nodes = graph->nodes();
+  std::unordered_set<const DexMethod*> should_not_mark;
+  for (const auto& pair : nodes) {
+    const auto* method = pair.first;
+    // If a final method has children, it can only be package-private and we can
+    // not change it to be public.
+    if (is_final(method) && !pair.second.children.empty()) {
+      overriden_should_not_be_public(method, graph.get(), &should_not_mark);
+      always_assert_log(!is_public(method) && !is_protected(method),
+                        "%s is visible final but it has children");
+    }
+  }
+  walk::parallel::classes(scope, [&should_not_mark](DexClass* cls) {
+    for (auto* method : cls->get_vmethods()) {
+      if (!should_not_mark.count(method)) {
+        set_public(method);
+      }
+    }
+  });
+}
+} // namespace
+
+void loosen_access_modifier_except_vmethods(DexClass* clazz) {
   set_public(clazz);
   for (auto field : clazz->get_ifields()) {
     set_public(field);
   }
   for (auto field : clazz->get_sfields()) {
     set_public(field);
-  }
-  for (auto method : clazz->get_vmethods()) {
-    set_public(method);
   }
   // Direct methods should have one of the modifiers, ACC_STATIC, ACC_PRIVATE
   // or ACC_CONSTRUCTOR.
@@ -31,8 +73,10 @@ void loosen_access_modifier(DexClass* clazz) {
 }
 
 void loosen_access_modifier(const Scope& scope) {
-  walk::parallel::classes(
-      scope, [](DexClass* clazz) { loosen_access_modifier(clazz); });
+  walk::parallel::classes(scope, [](DexClass* clazz) {
+    loosen_access_modifier_except_vmethods(clazz);
+  });
+  loosen_access_modifier_for_vmethods(scope);
 
   DexType* dalvikinner = DexType::get_type("Ldalvik/annotation/InnerClass;");
   if (!dalvikinner) {
---
+++ test/unit/LoosenAccessModifierTest.cpp
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <gtest/gtest.h>
+
+#include "Creators.h"
+#include "IRAssembler.h"
+#include "MethodOverrideGraph.h"
+#include "RedexTest.h"
+
+class LoosenAccessModifierTest : public RedexTest {};
+
+#define create_virtual_method(ident, scope, parent_type, access, name)      \
+  auto ident = assembler::method_from_string("(method (" access ") \"" name \
+                                             "\"((return-void)))");         \
+  {                                                                         \
+    ClassCreator cc(ident->get_class());                                    \
+    cc.set_super(parent_type);                                              \
+    cc.add_method(ident);                                                   \
+    scope.push_back(cc.create());                                           \
+  }
+
+/**
+ * a.A.bar() <- final a.A1.bar() -\- b.A11.bar()
+ * b.A11.bar() does not override a.A1.bar() because of the visibility, so the
+ * first two methods should not be public.
+ */
+TEST_F(LoosenAccessModifierTest, virtual_methods) {
+  Scope scope;
+  auto object = type::java_lang_Object();
+  create_virtual_method(parent, scope, object, "", "La/A;.bar:()V");
+  create_virtual_method(child, scope, parent->get_class(), "final",
+                        "La/A1;.bar:()V");
+  create_virtual_method(grand_child, scope, child->get_class(), "",
+                        "Lb/A11;.bar:()V");
+
+  EXPECT_FALSE(is_public(parent));
+  EXPECT_FALSE(is_public(child));
+  EXPECT_FALSE(is_public(grand_child));
+
+  loosen_access_modifier(scope);
+
+  EXPECT_FALSE(is_public(parent));
+  EXPECT_FALSE(is_public(child));
+  EXPECT_TRUE(is_public(grand_child));
+}
+
+#undef create_virtual_method
