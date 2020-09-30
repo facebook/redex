a6fde39e2e8964b1ab94d03fc67843bb1d4b0cae
Wei Zhang (Devinfra) <zwei@fb.com>
2020-09-29 20:10:07 -0700

    Disable remove_redundant_null_checks
    Summary:
    https://fb.workplace.com/groups/snaptuoncall/permalink/4424288457619735/
    
    Bytecode before redex promo, there's a null checking.
    https://www.internalfb.com/intern/paste/P143952869/?lines=278-279
    
    After redex promo, null-checking is removed.
    https://www.internalfb.com/intern/paste/P143953259/?lines=280
    
    Not quite clear why there needs a null-checking.
    https://www.internalfb.com/intern/diffusion/FBS/browsefile/master/fbandroid/apps/fblite/xMob-android/src/com/facebook/lite/components/ComponentsRendererView.java?commit=f1a9221376fa0a70843afbd7fea15b9014f22374&lines=168
    
    Reviewed By: wsanville
    
    Differential Revision: D23998307
    
    fbshipit-source-id: 88a40e7062a28302a300f693854ae3d142984be3

--- service/type-analysis/TypeAnalysisTransform.h
+++ service/type-analysis/TypeAnalysisTransform.h
@@ -23,7 +23,7 @@ class Transform final {
  public:
   using NullAssertionSet = std::unordered_set<DexMethodRef*>;
   struct Config {
-    bool remove_redundant_null_checks{true};
+    bool remove_redundant_null_checks{false};
     bool remove_kotlin_null_check_assertions{false};
     bool remove_redundant_type_checks{true};
     Config() {}
