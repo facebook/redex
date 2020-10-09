291a8143941e6b0f0b63624867b5bba8c7d84e47
Michał Gregorczyk <michalgr@fb.com>
2020-10-09 15:23:04 -0700

    more compact sleb128 encoding
    Summary:
    Signed leb is documented here: https://source.android.com/devices/tech/dalvik/dex-format#leb128
    The biggest number we can encode using N bytes is (64 << 7(N - 1)) - 1, which
    can be represented as 0xFF, 0xFF, ... (N - 1 times) ..., 0xFF, 0x3F.
    The smallest number we can encode using N bytes is -(64 << 7(N - 1)), which
    can be represented as 0x80, 0x80, ... (N - 1 times) ..., 0x80, 0x40.
    
    Currently redex chooses to encode -(64 << 7N) numbers using one more byte:
    0x80, 0x80, ... (N times) ..., 0x80, 0xC0, 0x7F. Reasonable parsers should
    handle that, but perhaps we can be a little bit more compact.
    
    Reviewed By: minjang
    
    Differential Revision: D24212260
    
    fbshipit-source-id: 400851400ed78203d16a52f12b34432a0fe4ffd7

---
+++ test/unit/LebTest.cpp
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <gtest/gtest.h>
+
+#include <array>
+
+#include "DexEncoding.h"
+#include "RedexTest.h"
+
+class LebTest : public RedexTest {};
+
+TEST_F(LebTest, Sleb128) {
+  auto check = [](int32_t num, std::vector<uint8_t> bytes) {
+    std::array<uint8_t, 5> storage = {0};
+    size_t length = write_sleb128(storage.begin(), num) - storage.begin();
+
+    EXPECT_EQ(length, bytes.size());
+    for (unsigned i = 0; i < length; ++i) {
+      EXPECT_EQ(storage[i], bytes[i]);
+    }
+
+    for (unsigned i = length; i < storage.size(); ++i) {
+      EXPECT_EQ(storage[i], 0);
+    }
+
+    const uint8_t* storage_ptr = storage.begin();
+    EXPECT_EQ(read_sleb128(&storage_ptr), num);
+  };
+
+  check((64 << 14), {0x80, 0x80, 0xC0, 0x00});
+  check((64 << 14) - 1, {0xFF, 0xFF, 0x3F});
+  check((64 << 7), {0x80, 0xC0, 0x00});
+  check((64 << 7) - 1, {0xFF, 0x3F});
+  check(64, {0xC0, 0x00});
+  check(63, {0x3F});
+  check(1, {0x01});
+  check(0, {0x00});
+  check(-1, {0x7F});
+  check(-64, {0x40});
+  check(-65, {0xBF, 0x7F});
+  check(-(64 << 7), {0x80, 0x40});
+  check(-(64 << 7) - 1, {0xFF, 0xBF, 0x7F});
+  check(-(64 << 14), {0x80, 0x80, 0x40});
+  check(-(64 << 14) - 1, {0xFF, 0xFF, 0xBF, 0x7F});
+}
