ec25b7774c1d808ea5275f95624dc39b192d46fb
Andreas Gampe <agampe@fb.com>
2020-12-05 00:24:08 -0800

    Package API level files (#576)
    Summary:
    Pull Request resolved: https://github.com/facebook/redex/pull/576
    
    Generate a Python module for the API level files.
    
    As the files together have nontrivial size (about 50MB at the time of writing), compress the files with zip or lzma, depending on availability in the host Python. This makes the output slightly nonportable in case lzma is supported on the generating host but not the target.
    
    The generated file contains the compressed files as a base64 text blob and provides a minimal API to get the file content. For lzma compression, the file is about 900KB at time of writing - it is unclear how to increase the compression level with the Python API, as manual experiments show it should be compressible to about 700K. For zip compression, the file is around 8MB.
    
    Let `redex.py` scan the input arguments. In case no API levels argument is given at all, attempt to load the generated module and extract its files. In case the module cannot be loaded, only log a warning message. This makes the approach more robust and corresponds to the original behavior.
    
    Reviewed By: minjang, Feng23
    
    Differential Revision: D25314571
    
    fbshipit-source-id: cf2f140a26479db646642fd5ca5121eb31001732

--- bundle-redex.sh
+++ bundle-redex.sh
@@ -4,6 +4,12 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-tar czf redex.tar.gz redex-all redex.py pyredex/*.py
+if [ -f "generated_apilevels.py" ] ; then
+  GEN_APILEVELS_INPUT="generated_apilevels.py"
+else
+  GEN_APILEVELS_INPUT=
+fi
+
+tar czf redex.tar.gz redex-all redex.py pyredex/*.py $GEN_APILEVELS_INPUT
 cat selfextract.sh redex.tar.gz > redex
 chmod +x redex
