a1360b72c767b0d8be090d359f27b917e42bb303
Sushan Jiang <suree404@fb.com>
2020-12-18 09:45:17 -0800

    Add some test for post dominator
    Summary:
    Post dominator is dominator of reversed cfg.
    will patch to three folders when accepted
    
    Reviewed By: Feng23
    
    Differential Revision: D24870398
    
    fbshipit-source-id: b56e182f8bf125ece312025aaf348e0990b366b6

--- test/unit/DominatorsTest.cpp
+++ test/unit/DominatorsTest.cpp
@@ -9,6 +9,7 @@
 
 #include <gtest/gtest.h>
 
+#include "MonotonicFixpointIterator.h"
 #include "SimpleGraph.h"
 
 TEST(DominatorsTest, simple) {
@@ -97,5 +98,154 @@ TEST(GraphUtilTest, doubleLoop) {
     EXPECT_EQ(doms.get_idom(2), 1);
     EXPECT_EQ(doms.get_idom(4), 3);
     EXPECT_EQ(doms.get_idom(5), 1);
+  }
+}
+
+TEST(GraphUtilTest, postdominator) {
+  {
+    //                 +---------+
+    //                 v         |
+    //     +---+     +---+     +---+     +----+
+    //  +- | 0 | --> | 1 | --> | 2 | --> |100 |
+    //  |  +---+     +---+     +---+     +----+
+    //  |                                  ^
+    //  |    +---------+                   |
+    //  |    v         |                   |
+    //  |  +---+     +---+                 |
+    //  +> | 3 | --> | 4 | ----------------+
+    //     +---+     +---+
+    GraphInterface::Graph graph;
+    graph.add_edge(0, 1);
+    graph.add_edge(1, 2);
+    graph.add_edge(2, 1);
+    graph.add_edge(0, 3);
+    graph.add_edge(3, 4);
+    graph.add_edge(4, 3);
+    graph.add_edge(4, 100);
+    graph.add_edge(2, 100);
+    dominators::SimpleFastDominators<
+        sparta::BackwardsFixpointIterationAdaptor<GraphInterfaceWithExit>>
+        post_doms(graph);
+    EXPECT_EQ(post_doms.get_idom(0), 100);
+    EXPECT_EQ(post_doms.get_idom(1), 2);
+    EXPECT_EQ(post_doms.get_idom(3), 4);
+    EXPECT_EQ(post_doms.get_idom(2), 100);
+    EXPECT_EQ(post_doms.get_idom(4), 100);
+    EXPECT_EQ(post_doms.get_idom(100), 100);
+  }
+  {
+    //                 +---------+
+    //                 v         |
+    //     +---+     +---+     +---+     +-----+
+    //     | 0 | --> | 1 | --> | 2 | --> | 100 |
+    //     +---+     +---+     +---+     +-----+
+    //                |                    ^
+    //  +-------------+                    |
+    //  |    +---------+                   |
+    //  |    v         |                   |
+    //  |  +---+     +---+                 |
+    //  +> | 3 | --> | 4 | ----------------+
+    //     +---+     +---+
+    GraphInterface::Graph graph;
+    graph.add_edge(0, 1);
+    graph.add_edge(1, 2);
+    graph.add_edge(2, 1);
+    graph.add_edge(1, 3);
+    graph.add_edge(3, 4);
+    graph.add_edge(4, 3);
+    graph.add_edge(4, 100);
+    graph.add_edge(2, 100);
+    dominators::SimpleFastDominators<
+        sparta::BackwardsFixpointIterationAdaptor<GraphInterfaceWithExit>>
+        post_doms(graph);
+    EXPECT_EQ(post_doms.get_idom(0), 1);
+    EXPECT_EQ(post_doms.get_idom(1), 100);
+    EXPECT_EQ(post_doms.get_idom(3), 4);
+    EXPECT_EQ(post_doms.get_idom(2), 100);
+    EXPECT_EQ(post_doms.get_idom(4), 100);
+    EXPECT_EQ(post_doms.get_idom(100), 100);
+  }
+  {
+    //                 +---------+
+    //                 v         |
+    //     +---+     +---+     +---+     +---+     +---+            +-----+
+    //     | 0 | --> | 1 | --> | 2 | --> | 5 | --> | 6 |            | 100 |
+    //     +---+     +---+     +---+     +---+     +---+            +-----+
+    //                |                    ^       |   |   +---+     ^   ^
+    //  +-------------+                    |     +-+   +-->| 8 |     |   |
+    //  |    +---------+                   |     |         +---+-----+   |
+    //  |    v         |                   |     v                       |
+    //  |  +---+     +---+                 |    +---+                    |
+    //  +> | 3 | --> | 4 | ----------------+    | 7 |--------------------+
+    //     +---+     +---+                      +---+
+    GraphInterface::Graph graph;
+    graph.add_edge(0, 1);
+    graph.add_edge(1, 2);
+    graph.add_edge(2, 1);
+    graph.add_edge(1, 3);
+    graph.add_edge(3, 4);
+    graph.add_edge(4, 3);
+    graph.add_edge(4, 5);
+    graph.add_edge(2, 5);
+    graph.add_edge(5, 6);
+    graph.add_edge(6, 7);
+    graph.add_edge(6, 8);
+    graph.add_edge(8, 100);
+    graph.add_edge(7, 100);
+    dominators::SimpleFastDominators<
+        sparta::BackwardsFixpointIterationAdaptor<GraphInterfaceWithExit>>
+        post_doms(graph);
+    EXPECT_EQ(post_doms.get_idom(0), 1);
+    EXPECT_EQ(post_doms.get_idom(1), 5);
+    EXPECT_EQ(post_doms.get_idom(3), 4);
+    EXPECT_EQ(post_doms.get_idom(2), 5);
+    EXPECT_EQ(post_doms.get_idom(4), 5);
+    EXPECT_EQ(post_doms.get_idom(5), 6);
+    EXPECT_EQ(post_doms.get_idom(6), 100);
+    EXPECT_EQ(post_doms.get_idom(7), 100);
+    EXPECT_EQ(post_doms.get_idom(8), 100);
+    EXPECT_EQ(post_doms.get_idom(100), 100);
+  }
+  {
+    //                 +---------+
+    //                 v         |
+    //     +---+     +---+     +---+     +----+
+    //  +- | 0 | --> | 1 | --> | 2 | --> |100 |
+    //     +---+     +---+     +---+     +----+
+    GraphInterface::Graph graph;
+    graph.add_edge(0, 1);
+    graph.add_edge(1, 2);
+    graph.add_edge(2, 1);
+    graph.add_edge(2, 100);
+    dominators::SimpleFastDominators<
+        sparta::BackwardsFixpointIterationAdaptor<GraphInterfaceWithExit>>
+        post_doms(graph);
+    EXPECT_EQ(post_doms.get_idom(0), 1);
+    EXPECT_EQ(post_doms.get_idom(1), 2);
+    EXPECT_EQ(post_doms.get_idom(2), 100);
+    EXPECT_EQ(post_doms.get_idom(100), 100);
+  }
+  {
+    //                 +---------+
+    //                 v         |
+    //     +---+     +---+     +---+     +----+
+    //  +- | 0 | --> | 1 | --> | 2 | --> |100 |
+    //     +---+     +---+     +---+     +----+
+    //                 |                   ^
+    //                 |                   |
+    //                 +-------------------+
+    GraphInterface::Graph graph;
+    graph.add_edge(0, 1);
+    graph.add_edge(1, 2);
+    graph.add_edge(2, 1);
+    graph.add_edge(2, 100);
+    graph.add_edge(1, 100);
+    dominators::SimpleFastDominators<
+        sparta::BackwardsFixpointIterationAdaptor<GraphInterfaceWithExit>>
+        post_doms(graph);
+    EXPECT_EQ(post_doms.get_idom(0), 1);
+    EXPECT_EQ(post_doms.get_idom(1), 100);
+    EXPECT_EQ(post_doms.get_idom(2), 100);
+    EXPECT_EQ(post_doms.get_idom(100), 100);
   }
 }
