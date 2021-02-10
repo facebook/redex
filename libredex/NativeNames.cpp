3f884f32ae5516b781104278f46cfd346c739ec0
Yuxuan Chen (C2) <ych@fb.com>
2021-02-10 14:52:40 -0800

    generate corresponding native names for native methods
    Summary:
    These helpers aim to help finding corresponding implementation in the native code for any native method found in Java classes. There are 2 ways how these names can be constructed depending on whether the method is overloaded.
    
    One caveat: it doesn't yet support Unicode characters in java identifiers.
    
    Reviewed By: emma0303
    
    Differential Revision: D26352984
    
    fbshipit-source-id: a21d5679573f161f1dbc75902a7d9d0103899275

---
+++ libredex/NativeNames.cpp
@@ -0,0 +1,112 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include "NativeNames.h"
+
+#include <boost/utility/string_view.hpp>
+#include <cctype>
+#include <sstream>
+
+// Implements name generation for JNI methods according to the spec.
+// https://docs.oracle.com/javase/1.5.0/docs/guide/jni/spec/design.html
+
+namespace native_names {
+
+// By standard, Java allows alphanumerical characters, unicode characters, as
+// well as _ and $ to be used in the identifiers.
+// NOTE: This doesn't yet support Unicode characters, even though "$" is
+// represented using Unicode.
+void escape_char(std::ostringstream& out, char c) {
+  if (isalnum(c)) {
+    out << c;
+    return;
+  }
+
+  switch (c) {
+  case '_':
+    out << "_1";
+    break;
+  case '$':
+    out << "_00024";
+    break;
+  default:
+    always_assert_log(false, "No mapping for char %c!", c);
+  }
+}
+
+void escape_single_identifier(std::ostringstream& out,
+                              boost::string_view name) {
+  for (char c : name) {
+    escape_char(out, c);
+  }
+}
+
+void mangle_class_name(std::ostringstream& out, boost::string_view cls_name) {
+  always_assert(cls_name.starts_with('L'));
+  always_assert(cls_name.ends_with(';'));
+  cls_name.remove_prefix(1);
+  cls_name.remove_suffix(1);
+
+  for (char c : cls_name) {
+    if (c == '/') {
+      out << "_";
+    } else {
+      escape_char(out, c);
+    }
+  }
+}
+
+void mangle_type_name_in_signature(std::ostringstream& out,
+                                   boost::string_view type_name) {
+  for (char c : type_name) {
+    switch (c) {
+    case '/':
+      out << "_";
+      break;
+    case ';':
+      out << "_2";
+      break;
+    case '[':
+      out << "_3";
+      break;
+    default:
+      escape_char(out, c);
+    }
+  }
+}
+
+void get_native_short_name_for_method_impl(std::ostringstream& out,
+                                           DexMethodRef* method) {
+  out << "Java_";
+  mangle_class_name(out, method->get_class()->str());
+  out << "_";
+  escape_single_identifier(out, method->get_name()->str());
+}
+
+void get_native_long_name_for_method_impl(std::ostringstream& out,
+                                          DexMethodRef* method) {
+  get_native_short_name_for_method_impl(out, method);
+  out << "__";
+  DexTypeList* types = method->get_proto()->get_args();
+  for (DexType* type : types->get_type_list()) {
+    mangle_type_name_in_signature(out, type->get_name()->str());
+  }
+}
+
+std::string get_native_short_name_for_method(DexMethodRef* method) {
+  std::ostringstream out;
+  get_native_short_name_for_method_impl(out, method);
+  return out.str();
+}
+
+std::string get_native_long_name_for_method(DexMethodRef* method) {
+  std::ostringstream out;
+  get_native_long_name_for_method_impl(out, method);
+  return out.str();
+}
+
+} // namespace native_names
---
+++ test/unit/NativeNamesTest.cpp
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <gtest/gtest.h>
+
+#include "NativeNames.h"
+#include "RedexTest.h"
+
+using namespace native_names;
+
+struct NativeNamesTest : public RedexTest {};
+
+TEST_F(NativeNamesTest, testBasicMethodNames) {
+  auto m = DexMethod::make_method(
+      "Lcom/facebook/redex/SomeClass;.someMethod:(ILjava/lang/String;)Ljava/"
+      "lang/Object;");
+  EXPECT_EQ("Java_com_facebook_redex_SomeClass_someMethod",
+            get_native_short_name_for_method(m));
+  EXPECT_EQ(
+      "Java_com_facebook_redex_SomeClass_someMethod__ILjava_lang_String_2",
+      get_native_long_name_for_method(m));
+}
+
+TEST_F(NativeNamesTest, testEscaping) {
+  auto m = DexMethod::make_method(
+      "Lcom/facebook/redex/Some$Interesting_Class;.$ome_Method:(ILjava/lang/"
+      "Str_ing;)Ljava/lang/Ob$ect;");
+  EXPECT_EQ(
+      "Java_com_facebook_redex_Some_00024Interesting_1Class__00024ome_1Method",
+      get_native_short_name_for_method(m));
+  EXPECT_EQ(
+      "Java_com_facebook_redex_Some_00024Interesting_1Class__00024ome_1Method__"
+      "ILjava_lang_Str_1ing_2",
+      get_native_long_name_for_method(m));
+}
