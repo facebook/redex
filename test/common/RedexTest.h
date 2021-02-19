d4b9b0585805bf76e6b7aa660c4c0424f4ca8b82
Adrian Catana <adicatana@fb.com>
2021-02-19 14:23:45 -0800

    ABExperiments] promote D26017367 to staging and stable
    Summary: As in the title.
    
    Reviewed By: NTillmann
    
    Differential Revision: D26544820
    
    fbshipit-source-id: ccc0b2712c0fc9e616f1a391b03b4ae7d1e3f7ce

--- test/common/RedexTest.h
+++ test/common/RedexTest.h
@@ -34,8 +34,8 @@ struct RedexTest : public testing::Test {
   static void force_experiments_control_mode() {
     ab_test::ABExperimentContext::force_control_mode();
   }
-  static void force_experiments_preferred_mode() {
-    ab_test::ABExperimentContext::force_preferred_mode();
+  static void disable_ab_experiments() {
+    ab_test::ABExperimentContext::disable_ab_experiments();
   }
   static void force_experiments_test_mode() {
     ab_test::ABExperimentContext::force_test_mode();
