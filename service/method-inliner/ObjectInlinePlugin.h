31dbf2322f448dd1d218eb1cd7b1a0536b8771f6
Kugan Vivekanandarajah <kugan@fb.com>
2021-04-08 02:19:12 -0700

    Used New analysis for the pass
    Summary:
    * This patch allows object inliner plugin to inline constructor using Spartra analysis.
    * Builder of the object and the constructor may set fields in the component that are being read by the method being inlined.
    * Object inliner plugin will assign a unique register for the field and all the reads/writes will be from this.
    
    Reviewed By: amnn
    
    Differential Revision: D26868265
    
    fbshipit-source-id: 597d63830750efe4ebdcc7e9f8c7d27e9a0164b9

--- service/method-inliner/ObjectInlinePlugin.h
+++ service/method-inliner/ObjectInlinePlugin.h
@@ -8,15 +8,17 @@
 #pragma once
 
 #include "CFGInliner.h"
-#include "ClassInitCounter.h"
 #include "ControlFlow.h"
 
 #include <unordered_map>
 
 #include <boost/optional.hpp>
 
-using namespace cic;
 using namespace cfg;
+namespace object_inliner_plugin {
+
+using FieldSet = std::unordered_map<reg_t, std::unordered_set<IRInstruction*>>;
+using FieldSetMap = std::unordered_map<DexFieldRef*, FieldSet>;
 
 class ObjectInlinePlugin : public CFGInlinerPlugin {
 
@@ -25,9 +27,8 @@ class ObjectInlinePlugin : public CFGInlinerPlugin {
       const FieldSetMap& field_sets,
       const std::unordered_map<DexFieldRef*, DexFieldRef*>& field_swaps,
       const std::vector<reg_t>& srcs,
-      reg_t value_register,
+      boost::optional<reg_t> value_register,
       boost::optional<reg_t> caller_this,
-      reg_t callee_this,
       DexType* callee_type);
 
   /*
@@ -46,14 +47,18 @@ class ObjectInlinePlugin : public CFGInlinerPlugin {
   bool inline_after() override;
   bool remove_inline_site() override;
 
-  const FieldSetMap& get_unaccessed_sets() { return m_unaccessed_field_sets; }
+  void set_src_regs(std::vector<reg_t> srcs) { m_srcs = std::move(srcs); }
 
  private:
+  // Fields set by the builder. This is provided as an argument.
   FieldSetMap m_initial_field_sets;
+  // Used to record the register <-> field map. This is created as part of the
+  // field removal in object inlining. All field reads and write will be
+  // re-writen to use this.
   FieldSetMap m_set_field_sets;
-  FieldSetMap m_unaccessed_field_sets;
   const std::unordered_map<DexFieldRef*, DexFieldRef*> m_field_swaps;
   std::vector<reg_t> m_srcs;
-  reg_t m_value_reg;
+  boost::optional<reg_t> m_value_reg;
   boost::optional<reg_t> m_caller_this_reg;
 };
+} // namespace object_inliner_plugin
