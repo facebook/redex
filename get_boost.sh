87304c44e76e4a42686faadc8a6b4f12cf10fac5
Andreas Gampe <agampe@fb.com>
2021-05-04 13:53:56 -0700

    Update `get_boost.sh` for caching (#608)
    Summary:
    Pull Request resolved: https://github.com/facebook/redex/pull/608
    
    Download the `boost` sources into `boost_cache` to potentially reuse. Will help with CircleCI.
    
    Also refactor a bit to simplify version bumps in the future.
    
    Reviewed By: Feng23
    
    Differential Revision: D28167866
    
    fbshipit-source-id: 0cf7434e7f59c3ec12346a19e9ae87fb701da464

--- get_boost.sh
+++ get_boost.sh
@@ -4,10 +4,27 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+BOOST_VERSION="1.71.0"
+
+BOOST_VERSION_UNDERSCORE="${BOOST_VERSION//./_}"
+BOOST_FILE="boost_${BOOST_VERSION_UNDERSCORE}.tar.bz2"
+BOOST_TAR_URL="https://boostorg.jfrog.io/artifactory/main/release/${BOOST_VERSION}/source/${BOOST_FILE}"
+BOOST_CACHE_DIR="boost_cache"
+BOOST_TAR_LOCAL="${BOOST_CACHE_DIR}/${BOOST_FILE}"
+BOOST_DIR="boost_${BOOST_VERSION_UNDERSCORE}"
+
 set -e
 
-wget https://boostorg.jfrog.io/artifactory/main/release/1.71.0/source/boost_1_71_0.tar.bz2
-tar --bzip2 -xf boost_1_71_0.tar.bz2
-cd boost_1_71_0
+# Check for cached artifacts.
+if [ ! -d "$BOOST_CACHE_DIR" ] ; then
+  mkdir boost_cache
+fi
+if [ ! -f "$BOOST_TAR_LOCAL" ] ; then
+  wget "$BOOST_TAR_URL" -O "$BOOST_TAR_LOCAL"
+fi
+
+tar --bzip2 -xf "$BOOST_TAR_LOCAL"
+
+cd "$BOOST_DIR"
 ./bootstrap.sh --with-libraries=filesystem,iostreams,program_options,regex,system,thread
 ./b2 -d0 install
