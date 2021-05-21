f26026f0bcd456450cca9c376ee03005df90d62b
Yuxuan Chen (DevInfra) <ych@fb.com>
2021-05-21 16:15:19 -0700

    Change boost download script + Fix OSS CI
    Summary: Boost download path has changed. Didn't do the cached boost thing because we don't run this script in SPARTA's circle CI config.
    
    Reviewed By: minjang
    
    Differential Revision: D28612582
    
    fbshipit-source-id: c60fb5121fd392c8d5de268eaf7f0a724df3d9e9

--- sparta/get_boost.sh
+++ sparta/get_boost.sh
@@ -4,10 +4,17 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+BOOST_VERSION="1.71.0"
+
+BOOST_VERSION_UNDERSCORE="${BOOST_VERSION//./_}"
+BOOST_FILE="boost_${BOOST_VERSION_UNDERSCORE}.tar.bz2"
+BOOST_TAR_URL="https://boostorg.jfrog.io/artifactory/main/release/${BOOST_VERSION}/source/${BOOST_FILE}"
+BOOST_DIR="boost_${BOOST_VERSION_UNDERSCORE}"
+
 set -e
 
-wget https://dl.bintray.com/boostorg/release/1.71.0/source/boost_1_71_0.tar.bz2
-tar --bzip2 -xf boost_1_71_0.tar.bz2
-cd boost_1_71_0
+wget "$BOOST_TAR_URL" -O "$BOOST_FILE"
+tar --bzip2 -xf "$BOOST_FILE"
+cd "$BOOST_DIR"
 ./bootstrap.sh --with-libraries=thread
 ./b2 -d0 install
