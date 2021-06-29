4668f15a7ee60505636444511b8cdefd981c0172
Andreas Gampe <agampe@fb.com>
2021-06-28 21:36:26 -0700

    Promote "Factor out `bintools.py`"
    Summary: Promote {D29384964 (https://github.com/facebook/redex/commit/4b0cfb7d0f699997c4b155117048cf276f767c7d)}.
    
    Reviewed By: Feng23
    
    Differential Revision: D29434515
    
    fbshipit-source-id: 0318a8653dd95bd3d5d0a6f7496559387b7bb4ea

--- redex.py
+++ redex.py
@@ -27,6 +27,7 @@ import zipfile
 from os.path import abspath, dirname, getsize, isdir, isfile, join
 from pipes import quote
 
+import pyredex.bintools as bintools
 import pyredex.logger as logger
 from pyredex.logger import log
 from pyredex.unpacker import unpack_tar_xz
@@ -165,208 +166,6 @@ def get_stop_pass_idx(passes_list, pass_name_and_num):
     )
 
 
-_BACKTRACE_PATTERN = re.compile(r"^([^(]+)(?:\((.*)\))?\[(0x[0-9a-f]+)\]$")
-
-
-def maybe_addr2line(lines):
-    global _BACKTRACE_PATTERN
-
-    # Generate backtrace lines.
-    def find_matches():
-        for line in lines:
-            stripped_line = line.strip()
-            m = _BACKTRACE_PATTERN.fullmatch(stripped_line)
-            if m is not None:
-                yield m
-
-    # Check whether there is anything to do.
-    matches_gen = find_matches()
-    first_elem = next(matches_gen, None)
-    if first_elem is None:
-        return
-    matches_gen = itertools.chain([first_elem], matches_gen)
-
-    # Check whether addr2line is available
-    def has_addr2line():
-        try:
-            subprocess.check_call(
-                ["addr2line", "-v"],
-                stdout=subprocess.DEVNULL,
-                stderr=subprocess.DEVNULL,
-            )
-            return True
-        except (subprocess.CalledProcessError, FileNotFoundError):
-            return False
-
-    if not has_addr2line():
-        sys.stderr.write("Addr2line not found!\n")
-        return
-    sys.stderr.write("\n")
-
-    addr2line_base = ["addr2line", "-f", "-i", "-C", "-e"]
-
-    def symbolize(filename, offset):
-        # It's good enough not to use server mode.
-        try:
-            output = subprocess.check_output(addr2line_base + [filename, offset])
-            return output.decode(sys.stderr.encoding).splitlines()
-        except subprocess.CalledProcessError:
-            return ["<addr2line error>"]
-
-    for m in matches_gen:
-        sys.stderr.write("%s(%s)[%s]\n" % (m.group(1), m.group(2), m.group(3)))
-        decoded = symbolize(m.group(1), m.group(3))
-        odd_line = True
-        for line in decoded:
-            sys.stderr.write("%s%s\n" % ("  " * (1 if odd_line else 2), line.strip()))
-            odd_line = not odd_line
-
-    sys.stderr.write("\n")
-
-
-def find_abort_error(lines):
-    terminate_lines = []
-    for line in lines:
-        stripped_line = line.rstrip()
-
-        if stripped_line.startswith("terminate called"):
-            terminate_lines.append(stripped_line)
-            continue
-
-        if len(terminate_lines) > 0:
-            terminate_lines.append(stripped_line)
-
-            # Stop on ten lines.
-            if len(terminate_lines) >= 10:
-                break
-            continue
-
-    if not terminate_lines:
-        return None
-
-    if len(terminate_lines) >= 3:
-        # Try to find the first line matching a backtrace.
-        backtrace_idx = None
-        global _BACKTRACE_PATTERN
-        for i in range(2, len(terminate_lines)):
-            m = _BACKTRACE_PATTERN.fullmatch(terminate_lines[i])
-            if m is not None:
-                backtrace_idx = i
-                break
-
-        if backtrace_idx:
-            terminate_lines = terminate_lines[:backtrace_idx]
-        else:
-            # Probably not one of ours, or with a very detailed error, just
-            # print two lines.
-            terminate_lines = terminate_lines[0:2]
-
-    # Remove trailing newlines.
-    while terminate_lines:
-        if terminate_lines[-1] == "":
-            terminate_lines.pop()
-        else:
-            break
-
-    # Add space to offset.
-    return "\n".join(" " + line for line in terminate_lines)
-
-
-def run_and_stream_stderr(args, env, pass_fds):
-    if IS_WINDOWS:
-        # Windows does not support `pass_fds` parameter.
-        proc = subprocess.Popen(args, env=env, stderr=subprocess.PIPE)
-    else:
-        proc = subprocess.Popen(
-            args, env=env, pass_fds=pass_fds, stderr=subprocess.PIPE
-        )
-
-    def stream_and_return(line_handler=None):
-        err_out = []
-        # Copy and stash the output.
-        for line in proc.stderr:
-            try:
-                str_line = line.decode(sys.stdout.encoding)
-            except UnicodeDecodeError:
-                str_line = "<UnicodeDecodeError>\n"
-            if line_handler:
-                str_line = line_handler(str_line)
-            sys.stderr.write(str_line)
-            err_out.append(str_line)
-            if len(err_out) > 1000:
-                err_out = err_out[100:]
-
-        returncode = proc.wait()
-
-        return (returncode, err_out)
-
-    return (proc, stream_and_return)
-
-
-# Signal handlers.
-# A SIGINT handler gives the process some time to wait for redex to terminate
-# and symbolize a backtrace. The SIGINT handler uninstalls itself so that a
-# second SIGINT really kills redex.py and install a SIGALRM handler. The SIGALRM
-# handler either sends a SIGINT to redex, waits some more, or terminates
-# redex.py.
-class RedexState(enum.Enum):
-    UNSTARTED = 1
-    STARTED = 2
-    POSTPROCESSING = 3
-    FINISHED = 4
-
-
-class SigIntHandler:
-    def __init__(self):
-        self._old_handler = None
-        self._state = RedexState.UNSTARTED
-        self._proc = None
-
-    def install(self):
-        # On Linux, support ctrl-c.
-        # Note: must be on the main thread. Add checks. Portability is an issue.
-        if platform.system() != "Linux":
-            return
-
-        self._old_handler = signal.getsignal(signal.SIGINT)
-        signal.signal(signal.SIGINT, self._sigint_handler)
-
-    def uninstall(self):
-        if self._old_handler is not None:
-            signal.signal(signal.SIGINT, self._old_handler)
-
-    def set_state(self, new_state):
-        self._state = new_state
-
-    def set_proc(self, new_proc):
-        self._proc = new_proc
-
-    def _sigalrm_handler(self, _signum, _frame):
-        signal.signal(signal.SIGALRM, signal.SIG_DFL)
-        if self._state == RedexState.STARTED:
-            # Send SIGINT in case redex-all was not in the same process
-            # group and wait some more.
-            self._proc.send_signal(signal.SIGINT)
-            signal.alarm(3)
-            return
-        if self._state == RedexState.POSTPROCESSING:
-            # Maybe symbolization took a while. Give it some more time.
-            signal.alarm(3)
-            return
-        # Kill ourselves.
-        os.kill(os.getpid(), signal.SIGINT)
-
-    def _sigint_handler(self, _signum, _frame):
-        signal.signal(signal.SIGINT, self._old_handler)
-        if self._state == RedexState.UNSTARTED or self._state == RedexState.FINISHED:
-            os.kill(os.getpid(), signal.SIGINT)
-
-        # This is the first SIGINT, schedule some waiting period. redex-all is
-        # likely in the same process group and already got a SIGINT delivered.
-        signal.signal(signal.SIGALRM, self._sigalrm_handler)
-        signal.alarm(3)
-
-
 class ExceptionMessageFormatter:
     def format_rerun_message(self, gdb_script_name, lldb_script_name):
         return "You can re-run it under gdb by running {} or under lldb by running {}".format(
@@ -483,63 +282,55 @@ def run_redex_binary(state, exception_formatter, output_line_handler):
     add_extra_environment_args(env)
 
     def run():
-        sigint_handler = SigIntHandler()
-        sigint_handler.install()
-
-        try:
-            proc, handler = run_and_stream_stderr(
+        with bintools.SigIntHandler() as sigint_handler:
+            proc, handler = bintools.run_and_stream_stderr(
                 prefix + args, env, (logger.trace_fp.fileno(),)
             )
-            sigint_handler.set_proc(proc)
-            sigint_handler.set_state(RedexState.STARTED)
+            sigint_handler.set_started(proc)
 
             returncode, err_out = handler(output_line_handler)
 
-            sigint_handler.set_state(RedexState.POSTPROCESSING)
+            sigint_handler.set_postprocessing()
 
-            if returncode != 0:
-                # Check for crash traces.
-                maybe_addr2line(err_out)
+            if returncode == 0:
+                return
 
-                abort_error = None
-                if returncode == -6:  # SIGABRT
-                    abort_error = find_abort_error(err_out)
-                if abort_error is not None:
-                    abort_error = "\n" + abort_error
-                else:
-                    abort_error = ""
+            # Check for crash traces.
+            bintools.maybe_addr2line(err_out)
 
-                default_error_msg = "redex-all crashed with exit code {}!{}".format(
-                    returncode, abort_error
-                )
-                if IS_WINDOWS:
-                    raise RuntimeError(default_error_msg)
+            abort_error = None
+            if returncode == -6:  # SIGABRT
+                abort_error = bintools.find_abort_error(err_out)
+            abort_error = "\n" + abort_error if abort_error else ""
 
-                gdb_script_name = write_debugger_command(
-                    "gdb", state.args.debug_source_root, args
-                )
-                lldb_script_name = write_debugger_command(
-                    "lldb", state.args.debug_source_root, args
-                )
-                msg = exception_formatter.format_message(
-                    err_out, default_error_msg, gdb_script_name, lldb_script_name
-                )
-                raise RuntimeError(msg)
-            return True
-        except OSError as err:
-            if err.errno == errno.ETXTBSY:
-                return False
-            raise err
-        finally:
-            sigint_handler.set_state(RedexState.FINISHED)
-            sigint_handler.uninstall()
+            default_error_msg = "redex-all crashed with exit code {}!{}".format(
+                returncode, abort_error
+            )
+            if IS_WINDOWS:
+                raise RuntimeError(default_error_msg)
+
+            gdb_script_name = write_debugger_command(
+                "gdb", state.args.debug_source_root, args
+            )
+            lldb_script_name = write_debugger_command(
+                "lldb", state.args.debug_source_root, args
+            )
+            msg = exception_formatter.format_message(
+                err_out, default_error_msg, gdb_script_name, lldb_script_name
+            )
+            raise RuntimeError(msg)
 
     # Our CI system occasionally fails because it is trying to write the
     # redex-all binary when this tries to run.  This shouldn't happen, and
     # might be caused by a JVM bug.  Anyways, let's retry and hope it stops.
-    for _ in range(5):
-        if run():
+    for i in range(5):
+        try:
+            run()
             break
+        except OSError as err:
+            if err.errno == errno.ETXTBSY and i < 4:
+                continue
+            raise err
 
     log("Dex processing finished in {:.2f} seconds".format(timer() - start))
 
