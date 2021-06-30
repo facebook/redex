c933cfb565e1c1de1de2c1137648d5e837e6ce17
William Sanville <wsanville@fb.com>
2021-06-30 12:05:34 -0700

    redex_instr_test for bundle sample app reachability
    Reviewed By: ssj933
    
    Differential Revision: D29219839
    
    fbshipit-source-id: b0a15118383e962775a8ab0f4ad84d9476c05be4

---
+++ test/instr/BundleReachActivityTest.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.redextest;
+
+import android.app.Activity;
+import android.support.test.rule.ActivityTestRule;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Rule;
+import org.junit.Test;
+
+import com.fb.bundles.MainActivity;
+
+public class BundleReachActivityTest {
+
+  private static View findViewWithText(View parent, String toFind) {
+    List<View> list = new ArrayList<>();
+    list.add(parent);
+    while (!list.isEmpty()) {
+      View view = list.get(0);
+      if (view instanceof ViewGroup) {
+        ViewGroup viewGroup = (ViewGroup) view;
+        int childCount = viewGroup.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+          list.add(viewGroup.getChildAt(i));
+        }
+      }
+      if (view instanceof TextView) {
+        TextView textView = (TextView) view;
+        CharSequence text = textView.getText();
+        if (text != null && text.toString().contains(toFind)) {
+          return view;
+        }
+      }
+      list.remove(0);
+    }
+    throw new AssertionError("NOT FOUND: " + toFind);
+  }
+
+  @Rule
+  public ActivityTestRule<MainActivity> mActivityRule =
+    new ActivityTestRule<>(MainActivity.class);
+
+  @Test
+  public void testLaunchActivity() {
+    // NOTE: Intentionally not using any good APIs for finding views/verifying,
+    // just do something very rudimentary (in case things get obfuscated/changed
+    // heavily).
+    View decor = mActivityRule.getActivity().getWindow().getDecorView();
+    {
+      View result = findViewWithText(decor, "Oh my!");
+      android.util.Log.w("BNDL", "FOUND: " + result.toString());
+    }
+    {
+      View result = findViewWithText(decor, "That's cool");
+      android.util.Log.w("BNDL", "FOUND: " + result.toString());
+    }
+  }
+}
