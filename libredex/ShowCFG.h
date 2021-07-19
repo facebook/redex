79a905716103093639cca250c20bb8015bc04750
Andreas Gampe <agampe@fb.com>
2021-07-18 20:41:59 -0700

    Introduce `ShowCFG.h`
    Summary:
    Factor out `show` for a basic block and CFG. Allow to inject data into the stream.
    
    Add a specialized variant that works with a fixpoint iterator.
    
    Reviewed By: NTillmann
    
    Differential Revision: D29706478
    
    fbshipit-source-id: e5a898ae4014c10873adbaf994754a92ee5f36af

---
+++ libredex/ShowCFG.h
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include "ControlFlow.h"
+#include "IRList.h"
+#include "Show.h"
+
+template <typename Special>
+std::string show(const cfg::Block* block, Special& special) {
+  std::ostringstream ss;
+  for (const auto& mie : *block) {
+    special.mie_before(ss, mie);
+    ss << "   " << show(mie) << "\n";
+    special.mie_after(ss, mie);
+  }
+  return ss.str();
+}
+
+template <typename Special>
+std::string show(const cfg::ControlFlowGraph& cfg, Special& special) {
+  const auto& blocks = cfg.blocks();
+  std::ostringstream ss;
+  ss << "CFG:\n";
+  for (auto* b : blocks) {
+    ss << " Block B" << b->id() << ":";
+    if (b == cfg.entry_block()) {
+      ss << " entry";
+    }
+    ss << "\n";
+    special.start_block(ss, b);
+
+    ss << "   preds:";
+    for (const auto& p : b->preds()) {
+      ss << " (" << *p << " B" << p->src()->id() << ")";
+    }
+    ss << "\n";
+
+    ss << show(b, special);
+
+    ss << "   succs:";
+    for (auto& s : b->succs()) {
+      ss << " (" << *s << " B" << s->target()->id() << ")";
+    }
+    ss << "\n";
+    special.end_block(ss, b);
+  }
+  return ss.str();
+}
+
+// Special helper interjecting fixpoint iterator data.
+namespace show_impl {
+
+template <typename Environment, typename Iterator>
+struct IteratorSpecial {
+  Environment cur;
+  const Iterator& iter;
+
+  explicit IteratorSpecial(const Iterator& iter) : iter(iter) {}
+
+  void mie_before(std::ostream& os, const MethodItemEntry& mie) {
+    if (mie.type != MFLOW_OPCODE) {
+      return;
+    }
+    os << "state: " << cur << "\n";
+    iter.analyze_instruction(mie.insn, &cur);
+  }
+  void mie_after(std::ostream&, const MethodItemEntry&) {}
+
+  void start_block(std::ostream& os, cfg::Block* b) {
+    cur = iter.get_entry_state_at(b);
+    os << "entry state: " << cur << "\n";
+  }
+  void end_block(std::ostream& os, cfg::Block* b) {
+    auto exit_state = iter.get_exit_state_at(b);
+    os << "exit state: " << exit_state << "\n";
+  }
+};
+
+} // namespace show_impl
+
+template <typename Environment, typename Iterator>
+std::string show_analysis(const cfg::ControlFlowGraph& cfg,
+                          const Iterator& iter) {
+  show_impl::IteratorSpecial<Environment, Iterator> special(iter);
+  return show(cfg, special);
+}
