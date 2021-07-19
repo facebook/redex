949e162bac77415f7153f12b86b80f0c2924157b
Yinghui Hong <hyinghui@fb.com>
2021-07-18 21:52:24 -0700

    FastRegAllocPass unit test
    Summary: Add unit tests to check the behavior of the linear scan register allocation.
    
    Reviewed By: beicy
    
    Differential Revision: D29732773
    
    fbshipit-source-id: ad6254c493f3f8a69d946f3f99a868bb2ab4c5f7

--- test/unit/FastRegAllocTest.cpp
+++ test/unit/FastRegAllocTest.cpp
@@ -13,6 +13,8 @@ struct FastRegAllocTest : public RedexTest {};
 
 /*
  * Check function: allocate() in class: LinearScanAllocator
+ * Note: current expected_code is based on the behavior of the non-spill
+ * version.
  */
 TEST_F(FastRegAllocTest, RegAlloc) {
   auto method = assembler::method_from_string(R"(
@@ -51,6 +53,7 @@ TEST_F(FastRegAllocTest, RegAlloc) {
 }
 
 /*
+ * Check allocation behavior when there is dead code (vreg defined but no use).
  * If a vreg is defined but never used, we assume its live interval to be 1
  * insn.
  */
@@ -72,6 +75,43 @@ TEST_F(FastRegAllocTest, NoUseVReg) {
     (
       (const v0 1)
       (const v0 2)
+      (return v0)
+    )
+)");
+  EXPECT_CODE_EQ(method->get_code(), expected_code.get());
+}
+
+/*
+ * Check allocation behavior on control flow.
+ */
+TEST_F(FastRegAllocTest, ControlFlow) {
+  auto method = assembler::method_from_string(R"(
+    (method (public static) "LFoo;.bar:()Z"
+      (
+        (const v2 1)
+        (const v1 1)
+        (if-eqz v2 :branch)
+        (return v2)
+
+        (:branch)
+        (add-int v2 v2 v1)
+        (return v2)
+      )
+    )
+)");
+
+  fastregalloc::LinearScanAllocator allocator(method);
+  allocator.allocate();
+
+  auto expected_code = assembler::ircode_from_string(R"(
+    (
+      (const v0 1)
+      (const v1 1)
+      (if-eqz v0 :branch)
+      (return v0)
+
+      (:branch)
+      (add-int v0 v0 v1)
       (return v0)
     )
 )");
