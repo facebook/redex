92ffafa3063133dcc3ef0b2dfe9fcfff78f42c89
Feng Liu <fengliu@fb.com>
2021-08-13 20:56:07 -0700

    Output deterministic merging mapping
    Summary:
    Class mapping is deterministic after the diff.
    Method order is not, will look into it latter.
    
    Reviewed By: thezhangwei
    
    Differential Revision: D30171662
    
    fbshipit-source-id: 08a86d3a6f16f92ff4672bff01d05d0956588bb1

--- service/class-merging/ModelMerger.cpp
+++ service/class-merging/ModelMerger.cpp
@@ -454,10 +454,9 @@ void trim_method_debug_map(
   TRACE(CLMG, 5, "Method debug map trimmed %zu", trimmed_cnt);
 }
 
-void write_out_type_mapping(
-    const ConfigFiles& conf,
-    const std::unordered_map<const DexType*, DexType*>& mergeable_to_merger,
-    const TypeToMethodMap& method_dedup_map) {
+void write_out_type_mapping(const ConfigFiles& conf,
+                            const std::vector<const MergerType*>& mergers,
+                            const TypeToMethodMap& method_dedup_map) {
   std::string mapping_file = conf.metafile(TE_MAPPING_FILE_NAME);
   std::ofstream os(mapping_file, std::ios_base::app);
   if (!os.is_open()) {
@@ -465,15 +464,15 @@ void write_out_type_mapping(
   }
 
   std::ostringstream out;
-  for (const auto& pair : mergeable_to_merger) {
-    auto mergeable = pair.first;
-    auto merger = pair.second;
-    out << SHOW(mergeable) << " -> " << SHOW(merger) << std::endl;
+  for (auto merger : mergers) {
+    for (auto mergeable : merger->mergeables) {
+      out << SHOW(mergeable) << " -> " << SHOW(merger->type) << std::endl;
 
-    if (method_dedup_map.count(mergeable)) {
-      for (auto& symbol_map : method_dedup_map.at(mergeable)) {
-        out << "  " << symbol_map.first << " -> " << SHOW(symbol_map.second)
-            << std::endl;
+      if (method_dedup_map.count(mergeable)) {
+        for (auto& symbol_map : method_dedup_map.at(mergeable)) {
+          out << "  " << symbol_map.first << " -> " << SHOW(symbol_map.second)
+              << std::endl;
+        }
       }
     }
   }
@@ -627,7 +626,7 @@ std::vector<DexClass*> ModelMerger::merge_model(Scope& scope,
 
   // Write out mapping files
   auto method_dedup_map = mm.get_method_dedup_map();
-  write_out_type_mapping(conf, mergeable_to_merger, method_dedup_map);
+  write_out_type_mapping(conf, to_materialize, method_dedup_map);
   if (!to_materialize.empty()) {
     post_process(model, type_tags, mergeable_to_merger_ctor);
   }
