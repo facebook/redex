ad6fcda5ac1ba648124883529837036f50ee3b8b
Adrian Catana <adicatana@fb.com>
2021-11-05 07:41:52 -0700

    add Kotlin coroutines test for ClassMerging
    Summary:
    As in the title. This exposes the pattern we want to optimize in the next diff. Currently, there will be 6 constructors generated, that all look very similar: P461624188.
    
    This stack aims to weaken the argument types representing instance fields that are merged by class merging under new fields of type Object. This way, the existing merging logic of ClassMerging for constructors will be able to dedup more code.
    
    Reviewed By: thezhangwei
    
    Differential Revision: D30698621
    
    fbshipit-source-id: 1649db3b3a1838de74188703e1d83df2f85b9e0a

---
+++ test/instr/ClassMergingKotlinCoroutinesTestVerify.cpp
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include "Show.h"
+#include "verify/VerifyUtil.h"
+
+TEST_F(PostVerify, SinkCommonCtorInvocation) {
+  auto cls_coroutine_1 = find_class_named(
+      classes, "Lkotlin/sequences/SequencesKt__SequencesKt$flatMapIndexed$1;");
+  auto cls_coroutine_2 = find_class_named(
+      classes,
+      "Lkotlin/sequences/SequencesKt___SequencesKt$runningReduceIndexed$1;");
+
+  verify_type_erased(cls_coroutine_1);
+  verify_type_erased(cls_coroutine_2);
+}
