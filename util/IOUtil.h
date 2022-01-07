c4b181b0b28bcbf843212321a79352e1c8a2f4ec
William Sanville <wsanville@fb.com>
2022-01-06 20:52:15 -0800

    Move delete_files_relative
    Summary: Make this usable from any pass.
    
    Reviewed By: ssj933
    
    Differential Revision: D32011528
    
    fbshipit-source-id: 82478e7a6b649972e5c1a83ad9763a990a40e883

--- util/IOUtil.h
+++ util/IOUtil.h
@@ -5,9 +5,13 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+#include <boost/filesystem.hpp>
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <unordered_set>
+
+#include "Debug.h"
 
 inline void open_or_die(const std::string& filename, std::ofstream* os) {
   os->open(filename);
@@ -21,4 +25,34 @@ inline void write_string_to_file(const std::string& filename,
                                  const std::string& contents) {
   std::ofstream out(filename, std::ofstream::binary);
   out << contents;
+}
+
+// Given a set of relative files from the zip root, delete from the unpacked dir
+// asserting that all deletions were successful.
+inline size_t delete_files_relative(
+    const std::string& apk_dir,
+    const std::unordered_set<std::string>& relative_file_paths) {
+  size_t actually_deleted = 0;
+  for (const auto& f : relative_file_paths) {
+    std::string full_path = apk_dir + "/" + f;
+    if (exists(boost::filesystem::path(full_path))) {
+      auto remove_res = std::remove(full_path.c_str());
+      always_assert(remove_res == 0);
+      actually_deleted++;
+    }
+  }
+  return actually_deleted;
+}
+
+inline size_t delete_files_absolute(
+    const std::unordered_set<std::string>& absolute_file_paths) {
+  size_t actually_deleted = 0;
+  for (const auto& path : absolute_file_paths) {
+    if (exists(boost::filesystem::path(path))) {
+      auto remove_res = std::remove(path.c_str());
+      always_assert(remove_res == 0);
+      actually_deleted++;
+    }
+  }
+  return actually_deleted;
 }
