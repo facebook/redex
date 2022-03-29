fb3615f835545eae0465980a709bfd310f07d0cd
Andreas Gampe <agampe@fb.com>
2022-03-29 10:48:56 -0700

    Partially promote D34688140
    Summary: Promote {D34688140 (https://github.com/facebook/redex/commit/3bc1293a399775580d45209d1236812e4a735191)} to redex-staging and redex-stable.
    
    Reviewed By: Feng23
    
    Differential Revision: D35220480
    
    fbshipit-source-id: 3444794625497800605446e555399952ed235135

--- test/setup.sh
+++ test/setup.sh
@@ -9,10 +9,10 @@ set -e
 TEST_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 pushd "$TEST_DIR"
 
-# Note: Use at least 1.8 as we need googlemock, too.
+# Note: Use at least 1.10 as we need googlemock, too.
 
-test -e gtest-1.8.0.zip || {
-    curl https://codeload.github.com/google/googletest/zip/release-1.8.0 \
-         -o gtest-1.8.0.zip
-    unzip -o gtest-1.8.0.zip
+test -e gtest-1.10.0.zip || {
+    curl https://codeload.github.com/google/googletest/zip/release-1.10.0 \
+         -o gtest-1.10.0.zip
+    unzip -o gtest-1.10.0.zip
 }
