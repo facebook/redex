58e34ff04337675cf2afed22e2f9947d9e016fa0
Wei Zhang (Devinfra) <zwei@fb.com>
2022-06-16 18:01:33 -0700

    Properly track interface roots in AnonymousClassMerging
    Summary: Make the roots metrics more consistent between `AnonymousClassMerging` and `IntraDexClassMerging`
    
    Reviewed By: NTillmann
    
    Differential Revision: D37217733
    
    fbshipit-source-id: 27448abcac33cc928618dfd196e22e0f0886621a

--- opt/class-merging/ModelSpecGenerator.cpp
+++ opt/class-merging/ModelSpecGenerator.cpp
@@ -224,12 +224,13 @@ void discover_mergeable_anonymous_classes(
         // java.lang.Object.
         continue;
       }
-      mgr->incr_metric("cls_" + show(parent), pair.second.size());
       if (is_interface(type_class(parent))) {
         auto first_mergeable = type_class(pair.second[0]);
         merging_spec->roots.insert(first_mergeable->get_super_class());
+        mgr->incr_metric("intf_" + show(parent), pair.second.size());
       } else {
         merging_spec->roots.insert(parent);
+        mgr->incr_metric("cls_" + show(parent), pair.second.size());
       }
       merging_spec->merging_targets.insert(pair.second.begin(),
                                            pair.second.end());
