97cb49708a7dbeae3ed300ee905bb1566b3def10
Adrian Catana <adicatana@fb.com>
2022-06-30 05:02:06 -0700

    Remove unused profiled_methods_file option
    Summary: Not used anymore, let's remove it.
    
    Reviewed By: thezhangwei
    
    Differential Revision: D37522447
    
    fbshipit-source-id: cfa8d62351cade5bf3e8b7a7a8d333280a2b1ed3

--- libredex/GlobalConfig.cpp
+++ libredex/GlobalConfig.cpp
@@ -146,9 +146,6 @@ void GlobalConfig::bind_config() {
   bind("lower_with_cfg", {}, bool_param);
   bind("no_optimizations_annotations", {}, string_vector_param);
   bind("no_optimizations_blocklist", {}, string_vector_param);
-  // TODO: Remove unused profiled_methods_file option and all build system
-  // references
-  bind("profiled_methods_file", "", string_param);
   bind("proguard_map", "", string_param);
   bind("prune_unexported_components", {}, string_vector_param);
   bind("pure_methods", {}, string_vector_param);
