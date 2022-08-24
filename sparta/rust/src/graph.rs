3aee7ba1fd432d92dd8f0dbec30e64607233006b
Gang Zhao <zhaogang@fb.com>
2022-08-24 10:55:00 -0700

    Add ReversedGraph, add default implementation for SuccessorNodes.
    Summary:
    For some analyses, e.g., liveness analysis, we need to reverse the graph. Users can create reversed version of the actual graph (e.g., CFG) and implement Graph/SuccessorNodes trait again. Or we can add a const bool generic to Graph trait, so that users can implement Graph<true>. Graph<false> for the same actual graph type to distinguish the original and reversed flow. Both are tedious work. Instead, we provide a default wrapper over any type implements Graph, which will act as its reversed version. Graph/SuccessorNodes have been implemented for this wrapper type.
    
    We now also provide a default SuccessorNodes implementation for any type that implements Graph trait.
    
    Reviewed By: yuxuanchen1997
    
    Differential Revision: D38914160
    
    fbshipit-source-id: 10cef2c35d10687e168aa9c00f943262f84d9f3d

--- sparta/rust/src/graph.rs
+++ sparta/rust/src/graph.rs
@@ -5,7 +5,10 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+use std::borrow::Borrow;
+use std::fmt::Debug;
 use std::hash::Hash;
+use std::marker::PhantomData;
 
 use smallvec::SmallVec;
 
@@ -24,7 +27,7 @@ pub const DEFAULT_GRAPH_SUCCS_NUM: usize = 4;
 // S as an associated constant since it is part of the return type for
 // method predecessors and successors.
 pub trait Graph<const S: usize = DEFAULT_GRAPH_SUCCS_NUM> {
-    type NodeId: Copy + Hash + Eq;
+    type NodeId: Copy + Hash + Eq + Ord + Debug;
     type EdgeId: Copy;
 
     /// Entry node.
@@ -51,10 +54,105 @@ pub trait Graph<const S: usize = DEFAULT_GRAPH_SUCCS_NUM> {
 
 /// A limited view of the graph that allows retrieving successor nodes of a given node.
 ///
-/// The same type that implements Graph trait can implement this trait,
-/// or clients can create a new type to implement it.
+/// Clients can create a new type to implement it, or they can use the default
+/// implementation provided on types that implemented `Graph`.
 pub trait SuccessorNodes<const S: usize = DEFAULT_GRAPH_SUCCS_NUM> {
     type NodeId: Copy + Hash + Eq;
 
     fn get_succ_nodes(&self, n: Self::NodeId) -> SmallVec<[Self::NodeId; S]>;
+}
+
+/// Implement `SuccessorNodes` for any type implemented `Graph`.
+///
+/// This is a convenient facility for clients who don't want to
+/// create a new type to implement `SuccessorNodes`. Note that
+/// this may be less efficient than custom implementation of
+/// SuccessorNodes since it can only use available trait methods
+/// such as `successors`.
+impl<const S: usize, T: Graph<S>> SuccessorNodes<S> for T {
+    type NodeId = T::NodeId;
+
+    fn get_succ_nodes(&self, n: Self::NodeId) -> SmallVec<[Self::NodeId; S]> {
+        self.successors(n)
+            .into_iter()
+            .map(|edge_idx| self.target(edge_idx))
+            .collect()
+    }
+}
+
+/// A reversed graph used for backward analysis.
+pub struct ReversedGraph<G: Graph<S>, B: Borrow<G>, const S: usize = DEFAULT_GRAPH_SUCCS_NUM> {
+    graph: B,
+    phantom: PhantomData<*const G>,
+}
+
+// Type alias to ease specifying generic parameters.
+pub type ReversedRefGraph<'a, G, const S: usize = DEFAULT_GRAPH_SUCCS_NUM> =
+    ReversedGraph<G, &'a G, S>;
+pub type ReversedIntoGraph<G, const S: usize = DEFAULT_GRAPH_SUCCS_NUM> = ReversedGraph<G, G, S>;
+
+/// Graph type that implements this trait can create reversed graph.
+pub trait ReverseGraph<const S: usize = DEFAULT_GRAPH_SUCCS_NUM> {
+    type G: Graph<S>;
+
+    /// Reversed graph.
+    fn rev(&self) -> ReversedRefGraph<Self::G, S>;
+
+    /// Reversed graph that consumes the original graph.
+    fn into_rev(self) -> ReversedIntoGraph<Self::G, S>;
+}
+
+/// Default `ReverseGraph` implementation for any type that implements `Graph`
+///
+/// This can be chained multiple times, e.g., `graph.rev().rev()` becomes
+/// the same as `graph`. This behavior is the same as `std::iter::Iterator::rev()`.
+impl<const S: usize, T: Graph<S>> ReverseGraph<S> for T {
+    type G = T;
+
+    fn rev(&self) -> ReversedRefGraph<T, S> {
+        ReversedGraph {
+            graph: self,
+            phantom: PhantomData,
+        }
+    }
+
+    fn into_rev(self) -> ReversedIntoGraph<T, S> {
+        ReversedGraph {
+            graph: self,
+            phantom: PhantomData,
+        }
+    }
+}
+
+impl<G: Graph<S>, B: Borrow<G>, const S: usize> Graph<S> for ReversedGraph<G, B, S> {
+    type NodeId = G::NodeId;
+    type EdgeId = G::EdgeId;
+
+    fn entry(&self) -> Self::NodeId {
+        self.graph.borrow().exit()
+    }
+
+    fn exit(&self) -> Self::NodeId {
+        self.graph.borrow().entry()
+    }
+
+    fn predecessors(&self, n: Self::NodeId) -> SmallVec<[Self::EdgeId; S]> {
+        self.graph.borrow().successors(n)
+    }
+
+    fn successors(&self, n: Self::NodeId) -> SmallVec<[Self::EdgeId; S]> {
+        self.graph.borrow().predecessors(n)
+    }
+
+    fn source(&self, e: Self::EdgeId) -> Self::NodeId {
+        self.graph.borrow().target(e)
+    }
+
+    fn target(&self, e: Self::EdgeId) -> Self::NodeId {
+        self.graph.borrow().source(e)
+    }
+
+    fn size(&self) -> usize {
+        self.graph.borrow().size()
+    }
 }
--- sparta/rust/tests/graph_test.rs
+++ sparta/rust/tests/graph_test.rs
@@ -8,16 +8,22 @@
 mod common;
 
 use common::graph::SimpleGraph;
+use sparta::graph::ReverseGraph;
 use sparta::graph::SuccessorNodes;
 
-#[test]
-fn test_graph_successors() {
+fn create_graph() -> SimpleGraph {
     let mut g = SimpleGraph::new(1, 5);
     g.add_edge(1, 2);
     g.add_edge(1, 3);
     g.add_edge(3, 4);
     g.add_edge(2, 5);
     g.add_edge(4, 5);
+    g
+}
+
+#[test]
+fn test_graph_successors() {
+    let g = create_graph();
 
     let mut succ_nodes = g.get_succ_nodes(1);
     succ_nodes.sort();
@@ -26,4 +32,18 @@ fn test_graph_successors() {
     let mut succ_nodes = g.get_succ_nodes(4);
     succ_nodes.sort();
     assert_eq!(succ_nodes.into_vec(), vec![5]);
+}
+
+#[test]
+fn test_reversed_graph() {
+    let graph = create_graph();
+    let reversed_g = graph.into_rev();
+
+    let mut succ_nodes = reversed_g.get_succ_nodes(5);
+    succ_nodes.sort();
+    assert_eq!(succ_nodes.into_vec(), vec![2, 4]);
+
+    let mut succ_nodes = reversed_g.get_succ_nodes(1);
+    succ_nodes.sort();
+    assert_eq!(succ_nodes.into_vec(), vec![]);
 }
