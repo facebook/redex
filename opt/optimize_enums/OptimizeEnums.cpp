d00161f02fd897159975d2bba5f8fb00131178f7
Wei Zhang (Devinfra) <zwei@meta.com>
2022-11-10 16:54:07 -0800

    Temporarily revert to old switch map removal logic
    Summary: Switch off the new `OptimizeEnumsUnmap` logic to investigate the nondeterminism issue.
    
    Reviewed By: ssj933
    
    Differential Revision: D41200163
    
    fbshipit-source-id: c8f7ff038abbae799c95f3a84cacb6ede09328bb

--- opt/optimize_enums/OptimizeEnums.cpp
+++ opt/optimize_enums/OptimizeEnums.cpp
@@ -677,7 +677,7 @@ class OptimizeEnums {
 
       // TODO(ngorski): This is left in just to minimize diff size
       // for review, and is deleted later in the diff stack.
-      const bool kUseMatchFlow = true;
+      const bool kUseMatchFlow = false;
       if (kUseMatchFlow) {
         unmap.unmap_switchmaps(cfg);
       } else {
