06c5d99dfe8fa1501ea59e473c879ae2bd143f10
Man Wang <wangm@meta.com>
2022-11-14 11:35:16 -0800

    remove unused code
    Summary: as title
    
    Reviewed By: thezhangwei
    
    Differential Revision: D41129162
    
    fbshipit-source-id: c028e9a38c91b9f25597e55f8eb073d299a27c21

--- libredex/Transform.h
+++ libredex/Transform.h
@@ -23,15 +23,6 @@ void remap_registers(cfg::ControlFlowGraph&, const RegMap&);
 void remap_registers(IRInstruction* insn, const RegMap& reg_map);
 void remap_registers(MethodItemEntry& mei, const RegMap& reg_map);
 
-/*
- * Sets all the opcodes in unreachable blocks to MFLOW_FALLTHROUGH, and removes
- * all successor edges connecting them to the graph. Does not actually delete
- * the blocks themselves.
- *
- * Return the number of instructions removed.
- */
-size_t remove_unreachable_blocks(IRCode* code);
-
 // if pos is inside a try block, return the corresponding catch
 // if not, return null
 MethodItemEntry* find_active_catch(IRCode* code, IRList::iterator pos);
