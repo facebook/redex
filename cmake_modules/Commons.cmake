9ab385b415ab6b0bb22372d4ebdd649ac860942e
Andreas Gampe <agampe@meta.com>
2023-03-01 14:04:11 -0800

    Remove boost::system dependency (#767)
    Summary:
    Pull Request resolved: https://github.com/facebook/redex/pull/767
    
    This should not be needed and may unblock the Windows build.
    
    Reviewed By: wsanville
    
    Differential Revision: D43609182
    
    fbshipit-source-id: ac2da7c820e51017ad93396e2ad5d4c91ad493e0

--- cmake_modules/Commons.cmake
+++ cmake_modules/Commons.cmake
@@ -47,7 +47,7 @@ macro(add_dependent_packages_for_redex)
         set(Boost_USE_MULTITHREADED ON)
     endif()
 
-    find_package(Boost 1.71.0 REQUIRED COMPONENTS system regex filesystem program_options iostreams thread)
+    find_package(Boost 1.71.0 REQUIRED COMPONENTS regex filesystem program_options iostreams thread)
     print_dirs("${Boost_INCLUDE_DIRS}" "Boost_INCLUDE_DIRS")
     print_dirs("${Boost_LIBRARIES}" "Boost_LIBRARIES")
 
