bbb2527120396b3be1add62938852fce3bb2fe22
Nikolai Tillmann <nikolait@meta.com>
2023-03-08 15:31:01 -0800

    Add option to emit (encoded) unrenamable set of types moved to longtail module
    Differential Revision: D43888092
    
    fbshipit-source-id: 0b9a17f3b0735a0454f5fe111aa1ac7133229f34

---
+++ libredex/StringTreeSet.cpp
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include "StringTreeSet.h"
+
+#include "Debug.h"
+#include <unordered_map>
+
+void StringTreeSet::insert(const std::string& s, size_t start) {
+  if (start == s.size()) {
+    m_terminal = true;
+    return;
+  }
+  always_assert(start < s.size());
+  m_map[s.at(start)].insert(s, start + 1);
+}
+
+void StringTreeSet::encode(std::ostringstream& oss) const {
+  if (!m_terminal && m_map.size() == 1) {
+    auto&& [c, rest] = *m_map.begin();
+    always_assert(c >= 32);
+    oss.put(c);
+    rest.encode(oss);
+    return;
+  }
+  always_assert(m_terminal || !m_map.empty());
+  always_assert(m_map.size() < 64);
+  size_t terminal_and_size = m_terminal | (m_map.size() << 1);
+  always_assert(terminal_and_size > 0);
+  always_assert(terminal_and_size < 128);
+  if (terminal_and_size >= 31) {
+    oss.put(31);
+  }
+  oss.put(terminal_and_size);
+  bool first{true};
+  std::unordered_map<char, std::ostringstream::pos_type> offsets;
+  for (auto&& [c, nested] : m_map) {
+    oss.put(c);
+    if (first) {
+      first = false;
+    } else {
+      offsets.emplace(c, oss.tellp());
+      oss.put(0);
+      oss.put(0);
+      oss.put(0);
+    }
+  }
+  first = true;
+  for (auto&& [c, rest] : m_map) {
+    if (first) {
+      first = false;
+    } else {
+      auto pos = oss.tellp();
+      always_assert(pos < 127 * 127 * 127);
+      oss.seekp(offsets.at(c));
+      oss.put((pos % 127) + 1);
+      oss.put(((pos / 127) % 127) + 1);
+      oss.put((pos / (127 * 127)) + 1);
+      oss.seekp(pos);
+    }
+    rest.encode(oss);
+  }
+}
---
+++ libredex/StringTreeSet.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <iostream>
+#include <map>
+#include <string>
+
+// The StringTreeSet provides a compact encoding for a set of strings that tend
+// to share prefices.
+class StringTreeSet {
+ public:
+  void insert(const std::string& s, size_t start = 0);
+
+  void encode(std::ostringstream& oss) const;
+
+ private:
+  std::map<char, StringTreeSet> m_map;
+  bool m_terminal{false};
+};
