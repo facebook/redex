fe58956b12beb4eff13c610f5d4db412388a9119
Adrian Catana <adicatana@meta.com>
2023-03-27 06:57:01 -0700

    Introducing BPC to Redex
    Summary: The diff introduces a compression-aware function reordering using Balanced Graph Partitioning (BPC) to Redex. Instead of heuristically placing "similar" methods close to each other, the new method directly reduces for compressed size of the app by creating the most "LZ-friendly" function layout.
    
    Reviewed By: NTillmann
    
    Differential Revision: D43995649
    
    fbshipit-source-id: 788f20f3a85aabd5573caceef8af983596cfb0d4

---
+++ libredex/BalancedPartitioning.h
@@ -0,0 +1,221 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <random>
+#include <string>
+#include <vector>
+
+class Document;
+class KmerSignature;
+
+/**
+ * Recursive balanced graph partitioning algorithm.
+ *
+ * The algorithm is used to find an ordering of Documents while optimizing
+ * a specified objective. The algorithm uses recursive bisection; it starts
+ * with a collection of unordered documents and tries to split them into
+ * two sets (buckets) of equal cardinality. Each bisection step is comprised of
+ * iterations that greedily swap the documents between the two buckets while
+ * there is an improvement of the objective. Once the process converges, the
+ * problem is divided into two sub-problems of half the size, which are
+ * recursively applied for the two buckets. The final ordering of the documents
+ * is obtained by concatenating the two (recursively computed) orderings.
+ *
+ * In order to speed up the computation, we limit the depth of the recursive
+ * tree by a specified constant (SplitDepth) and apply at most a constant
+ * number of greedy iterations per split (IterationsPerSplit). The worst-case
+ * time complexity of the implementation is bounded by O(M*log^2 N), where
+ * N is the number of documents and M is the number of document-kmer edges;
+ * (assuming that any collection of D documents contains O(D) k-mers). Notice
+ * that the two different recursive sub-problems are independent and thus can
+ * be efficiently processed in parallel.
+ */
+class BalancedPartitioning {
+  using SignaturesType = std::vector<KmerSignature>;
+
+ private:
+  BalancedPartitioning(const BalancedPartitioning&) = delete;
+  BalancedPartitioning& operator=(const BalancedPartitioning&) = delete;
+
+ public:
+  explicit BalancedPartitioning(std::vector<Document*>& documents);
+
+  /// Run recursive graph partitioning that optimizes a given objective.
+  void run() const;
+
+ private:
+  /// Run a recursive bisection of a given list of documents where
+  ///  - 'rec_depth' is the current depth of recursion
+  ///  - 'root_bucket' is the initial bucket of the dataVertices
+  ///  - the assigned buckets are the range [offset, offset + num_documents)
+  void bisect(const std::vector<Document*>::iterator& document_begin,
+              const std::vector<Document*>::iterator& document_end,
+              uint32_t rec_depth,
+              uint32_t root_bucket,
+              uint32_t offset) const;
+
+  /// Run bisection iterations.
+  /// Returns true iff a progress has been made.
+  void run_iterations(const std::vector<Document*>::iterator& document_begin,
+                      const std::vector<Document*>::iterator& document_end,
+                      uint32_t left_bucket,
+                      uint32_t right_bucket,
+                      std::mt19937& rng) const;
+
+  /// Run a bisection iteration to improve the optimization goal.
+  /// Returns the total number of moved documents.
+  uint32_t run_iteration(const std::vector<Document*>::iterator& document_begin,
+                         const std::vector<Document*>::iterator& document_end,
+                         uint32_t left_bucket,
+                         uint32_t right_bucket,
+                         SignaturesType& Signatures,
+                         std::mt19937& rng) const;
+
+  /// Try to move a document from one bucket to another.
+  /// Return true iff the document is moved.
+  bool move_data_vertex(Document* document,
+                        uint32_t left_bucket,
+                        uint32_t right_bucket,
+                        SignaturesType& Signatures,
+                        std::mt19937& rng) const;
+
+  /// Update document adjacency lists.
+  /// The method returns the maximum kmer index adjacent to the given documents.
+  uint32_t update_documents(
+      const std::vector<Document*>::iterator& document_begin,
+      const std::vector<Document*>::iterator& document_end) const;
+
+  /// Initialize k-mer signatures.
+  void initialize_signatures(
+      SignaturesType& Signatures,
+      const std::vector<Document*>::iterator& document_begin,
+      const std::vector<Document*>::iterator& document_end,
+      uint32_t left_bucket) const;
+
+  /// Split all the documents into 2 buckets, start_bucket and start_bucket + 1.
+  /// The method is used for an initial assignment before a bisection step.
+  void split(const std::vector<Document*>::iterator& document_begin,
+             const std::vector<Document*>::iterator& document_end,
+             uint32_t start_bucket) const;
+
+  /// Order the list of documents by assigning buckets in the range
+  /// [start_bucket + document_begin, start_bucket + document_end).
+  /// The method is used for assigning buckets when the number of
+  /// documents is small (to truncate the bisection tree).
+  void order(const std::vector<Document*>::iterator& document_begin,
+             const std::vector<Document*>::iterator& document_end,
+             uint32_t start_bucket) const;
+
+  /// Initialize k-mer signature before a bisection iteration.
+  void prepare_signature(KmerSignature& signature) const;
+
+  /// An average optimization goal for a given k-mer signature:
+  /// - to represent an integer k, one needs log_2(k) bits;
+  /// - to represent n integers in the range [0..U) (using the diff encoding),
+  ///   one needs log_2(U/n) per number, since an average diff is U/n.
+  /// Hence, n integers in the range [0..U) require (2 + log(U/n))*n bits, where
+  /// two additional bits is a constant overhead.
+  ///
+  /// Compute the move gain for uniform log-gap cost:
+  /// cost = x * log(U / (x+1)) + y * log(U / (y+1)) =
+  ///      = x * log(U) + y * log(U) - (x * log(x+1) + y * log(y+1)) =
+  ///      = U * log(U) - (x * log(x+1) + y * log(y+1))
+  /// The first term is constant; the second is 'log_cost'.
+  double move_gain(const Document* doc,
+                   bool from_left_to_right,
+                   const SignaturesType& signatures) const;
+
+  /// The cost of the uniform log-gap cost, assuming a k-mer has X
+  /// documents in the left bucket and Y documents in the right one.
+  double log_cost(uint32_t x, uint32_t y) const;
+
+  /// Input documents that shall be reordered by the algorithm.
+  std::vector<Document*>& documents;
+
+  /// Precomputed values of log2(x). Table size is small enough to fit in cache.
+  static constexpr uint32_t LOG_CACHE_SIZE = 16384;
+  double LOG2_CACHE[LOG_CACHE_SIZE];
+
+  /// Algorithm parameters; default values are tuned on real-world binaries.
+  ///
+  /// The depth of the recursive bisection.
+  uint32_t SPLIT_DEPTH = 18;
+  /// The maximum number of bp iterations per split.
+  uint32_t ITERATIONS_PER_SPLIT = 40;
+  /// The probability for a vertex to skip a move from its current bucket to
+  /// another bucket; it often helps to escape from a local optima.
+  static constexpr double SKIP_PROBABILITY = 0.1;
+};
+
+/**
+ * A document with edges to k-mers. After merging duplicates, the object
+ * may represent a group of duplicate (or very similar) documents whose ids
+ * are stored in the corresponding field.
+ */
+class Document {
+ public:
+  Document(const Document&) = delete;
+  Document(Document&&) = default;
+  Document& operator=(const Document&) = delete;
+  Document& operator=(Document&&) = default;
+
+  explicit Document() {}
+
+  void init(uint32_t doc_id) { ids.push_back(doc_id); }
+
+  void add(uint32_t adjacent_kmer) { edges.push_back(adjacent_kmer); }
+
+  void assign(const std::vector<uint32_t>& adjacent_kmers) {
+    edges.assign(adjacent_kmers.begin(), adjacent_kmers.end());
+  }
+
+  const std::vector<uint32_t>& adjacent_kmers() const { return edges; }
+
+  void shrink_to_fit() { edges.shrink_to_fit(); }
+
+  size_t size() const { return edges.size(); }
+
+  /// Document bucket assigned by balanced partitioning.
+  uint32_t bucket = uint32_t(-1);
+
+  /// Hash code of the document based on its content.
+  uint64_t hash = 0;
+
+ private:
+  /// Document ids of all (duplicate) documents corresponding to the instance.
+  std::vector<uint32_t> ids;
+  /// Adjacent k-mers of the document.
+  std::vector<uint32_t> edges;
+};
+
+/**
+ * Signature of a Kmer utilized in a bisection step, that is, the number of
+ * incident documents in the two buckets.
+ */
+class KmerSignature {
+ public:
+  KmerSignature(const KmerSignature&) = delete;
+  KmerSignature(KmerSignature&&) = default;
+  KmerSignature& operator=(const KmerSignature&) = delete;
+  KmerSignature& operator=(KmerSignature&&) = default;
+
+  explicit KmerSignature(uint32_t left_count = 0, uint32_t right_count = 0)
+      : left_count(left_count), right_count(right_count) {}
+
+  /// The number of documents in the left bucket.
+  uint32_t left_count;
+  /// The number of documents in the right bucket.
+  uint32_t right_count;
+  /// Cached cost of moving a document from left to right bucket.
+  double cached_cost_lr = 0;
+  /// Cached cost of moving a document from right to left bucket.
+  double cached_cost_rl = 0;
+  /// Whether the cached costs must be recomputed.
+  bool cache_is_invalid = true;
+};
---
+++ libredex/MethodSimilarityCompressionConsciousOrderer.cpp
@@ -0,0 +1,268 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include "MethodSimilarityCompressionConsciousOrderer.h"
+
+#include <unordered_map>
+
+#include "BalancedPartitioning.h"
+#include "Debug.h"
+#include "Show.h"
+
+namespace {
+constexpr size_t METHOD_MAX_OUTPUT_SIZE = 512 * 1024;
+
+/// Murmur-inspired hashing.
+constexpr uint64_t hash_128_to_64(const uint64_t upper,
+                                  const uint64_t lower) noexcept {
+  const uint64_t k_mul = 0x9ddfea08eb382d69ULL;
+  uint64_t A = (lower ^ upper) * k_mul;
+  A ^= (A >> 47);
+  uint64_t B = (upper ^ A) * k_mul;
+  B ^= (B >> 47);
+  B *= k_mul;
+  return B;
+}
+
+/// A wrapper of a DexMethod with a hash value and corresponding k-mers.
+struct BinaryFunction {
+  explicit BinaryFunction(DexMethod* m) : method(m) {}
+
+  // The method corresponding to the function.
+  DexMethod* method;
+  // The list of k-mers used for compression (instruction stable hashes).
+  std::vector<uint64_t> kmers;
+  // Corresponding document.
+  Document* doc{nullptr};
+  // Generated hash value.
+  uint64_t hash = 0;
+};
+
+/// Get the function's hash code based on its content. It is used to identify
+/// and merge duplicates.
+uint64_t compute_hash_code(
+    BinaryFunction& func,
+    const std::unordered_map<uint64_t, uint32_t>& kmer_frequency) {
+  const uint32_t k_min_kmer_frequency = 5;
+  uint64_t hash = 0;
+  for (uint64_t kmer : func.kmers) {
+    always_assert_log(kmer_frequency.find(kmer)->second > 0,
+                      "Incorrect kmer frequency");
+    // Ignore rare k-mers so as we merge near-duplicate functions
+    if (kmer_frequency.find(kmer)->second <= k_min_kmer_frequency) continue;
+
+    hash = hash_128_to_64(hash, kmer);
+  }
+  return hash;
+}
+
+/// Initialize data structures for the reordering algorithm.
+void init_bipartite_graph(std::vector<BinaryFunction>& functions,
+                          std::vector<Document>& documents) {
+  // Compute k-mer frequency
+  std::unordered_map<uint64_t, uint32_t> kmer_frequency;
+  for (uint32_t f = 0; f < functions.size(); f++) {
+    BinaryFunction& func = functions[f];
+    for (uint64_t kmer : func.kmers) {
+      kmer_frequency[kmer]++;
+    }
+  }
+
+  // Filter out unique and too frequent k-mers that do not affect compression;
+  // for the remaining k-mers, assign an index from the range [0, |Kmers|)
+  std::unordered_map<uint64_t, uint32_t> kmer_index;
+  for (auto it : kmer_frequency) {
+    uint64_t kmer = it.first;
+    uint32_t freq = it.second;
+    if (freq <= 1) continue;
+    if (freq * 2 >= functions.size()) continue;
+
+    uint32_t new_idx = kmer_index.size();
+    kmer_index[kmer] = new_idx;
+  }
+
+  // Computing function hashes and record the first one having a specific hash
+  // value (in order to merge duplicates)
+  std::unordered_map<uint64_t, uint32_t> first_func_with_hash;
+  for (uint32_t f = 0; f < functions.size(); f++) {
+    BinaryFunction& func = functions[f];
+    func.hash = compute_hash_code(func, kmer_frequency);
+    if (first_func_with_hash.find(func.hash) == first_func_with_hash.end()) {
+      first_func_with_hash[func.hash] = f;
+    }
+  }
+
+  // Initialize all documents corresponding to unique functions
+  uint32_t num_docs = 0;
+  documents.resize(first_func_with_hash.size());
+  for (uint32_t f = 0; f < functions.size(); f++) {
+    uint64_t hash = functions[f].hash;
+    always_assert_log(first_func_with_hash.find(hash) !=
+                          first_func_with_hash.end(),
+                      "Function with hash %lu not found", hash);
+    if (first_func_with_hash[hash] == f) {
+      // Found a new function instance
+      Document& doc = documents.at(num_docs);
+      doc.init(f);
+      functions[f].doc = &doc;
+      for (uint64_t kmer : functions[f].kmers) {
+        auto it = kmer_index.find(kmer);
+        if (it != kmer_index.end()) {
+          doc.add(it->second);
+        }
+      }
+      doc.shrink_to_fit();
+      num_docs++;
+    } else {
+      // A function with this hash already exists
+      BinaryFunction& first_func = functions[first_func_with_hash[hash]];
+      always_assert_log(first_func.doc != nullptr,
+                        "Incorrect document for method %s",
+                        SHOW(first_func.method));
+      Document* doc = first_func.doc;
+      doc->init(f);
+      functions[f].doc = doc;
+    }
+  }
+}
+
+/// Apply compression-conscious reordering function reordering using
+/// Balanced Graph Partitioning for a given set of functions.
+void apply_bpc(std::vector<BinaryFunction>& functions) {
+  // Creating and initializing a bipartite graph in which one part is a given
+  // set of documents (functions) and another part is the corresponding k-mers
+  std::vector<Document> documents;
+  init_bipartite_graph(functions, documents);
+
+  // Copying the list of documents as it will be modified during the execution
+  std::vector<Document*> documents_ptr;
+  documents_ptr.reserve(documents.size());
+  for (Document& doc : documents) {
+    documents_ptr.push_back(&doc);
+  }
+
+  // Run the reordering algorithm
+  BalancedPartitioning alg(documents_ptr);
+  alg.run();
+
+  // Verify that every document gets a correct bucket
+  for (Document& doc : documents) {
+    always_assert(0 <= doc.bucket && doc.bucket < functions.size());
+  }
+
+  // Sort functions by the resulting buckets
+  std::stable_sort(
+      functions.begin(), functions.end(),
+      [&](const BinaryFunction& left, const BinaryFunction& right) {
+        return left.doc->bucket < right.doc->bucket;
+      });
+}
+
+/// Generate k-mers (uint64_t hashes) from a given method content.
+std::vector<uint64_t> create_kmers(const std::vector<uint8_t>& content) {
+  always_assert_log(!content.empty(), "Constructing kmers for empty code.");
+  std::vector<uint64_t> kmers;
+
+  // Compute k-mers from the given content by iterating over the data. Shorter
+  // (overlapping) k-mers force method with similar instructions to stay
+  // together, while longer (non-overlapping) k-mers bring together methods
+  // containing identical sequences (e.g., basic blocks) of instructions.
+  const size_t k_window_1 = 5;
+  const size_t k_window_2 = 10;
+  for (size_t i = 0; i + k_window_1 <= content.size(); i++) {
+    // Collect overlapping k-mers of a smaller size
+    uint64_t hash = 0;
+    for (size_t j = 0; j < k_window_1; j++) {
+      hash = hash_128_to_64(hash, static_cast<uint64_t>(content[i + j]));
+    }
+    kmers.push_back(hash);
+    // Collect non-overlapping k-mers of a larger size
+    if (i % k_window_2 == 0 && i + k_window_2 <= content.size()) {
+      hash = 0;
+      for (size_t j = 0; j < k_window_2; j++) {
+        hash = hash_128_to_64(hash, static_cast<uint64_t>(content[i + j]));
+      }
+      kmers.push_back(hash);
+    }
+  }
+
+  // Sort the k-mers and get rid of duplicates
+  std::sort(kmers.begin(), kmers.end());
+  kmers.erase(std::unique(kmers.begin(), kmers.end()), kmers.end());
+
+  return kmers;
+}
+
+}; // namespace
+
+std::vector<uint8_t>
+MethodSimilarityCompressionConsciousOrderer::get_encoded_method_content(
+    DexMethod* meth,
+    std::unique_ptr<DexOutputIdx>& dodx,
+    std::unique_ptr<uint8_t[]>& output) {
+  // Get the code
+  DexCode* code = meth->get_dex_code();
+  always_assert_log(code != nullptr, "Empty code for method %s", SHOW(meth));
+
+  // Clean up
+  memset(output.get(), 0, METHOD_MAX_OUTPUT_SIZE);
+
+  // Encode
+  size_t size = code->encode(dodx.get(), (uint32_t*)(output.get()));
+  always_assert_log(size <= METHOD_MAX_OUTPUT_SIZE,
+                    "Encoded code size limit exceeded %lu versus %lu", size,
+                    METHOD_MAX_OUTPUT_SIZE);
+
+  // Collect the results
+  std::vector<uint8_t> content;
+  for (size_t i = 0; i < size; i++) {
+    uint8_t out_item = *(reinterpret_cast<uint8_t*>(output.get() + i));
+    content.push_back(out_item);
+  }
+
+  return content;
+}
+
+void MethodSimilarityCompressionConsciousOrderer::order(
+    std::vector<DexMethod*>& methods, GatheredTypes* m_gtypes) {
+  Timer t("Reordering " + std::to_string(methods.size()) +
+          " methods by similarity using BPC");
+  if (methods.empty()) return;
+
+  // We assume no method takes more than 512KB
+  auto output = std::make_unique<uint8_t[]>(METHOD_MAX_OUTPUT_SIZE);
+  auto dodx = std::make_unique<DexOutputIdx>(*m_gtypes->get_dodx(output.get()));
+
+  // Collect binary functions in the original order
+  std::vector<BinaryFunction> functions;
+  functions.reserve(methods.size());
+  std::vector<DexMethod*> empty_methods;
+  for (DexMethod* method : methods) {
+    auto* code = method->get_dex_code();
+    if (code == nullptr) {
+      empty_methods.push_back(method);
+      continue;
+    }
+    functions.emplace_back(method);
+    auto& func = functions.back();
+    auto content = get_encoded_method_content(method, dodx, output);
+    func.kmers = create_kmers(content);
+  }
+
+  // Apply the reordering
+  if (!functions.empty()) {
+    apply_bpc(functions);
+  }
+  methods.clear();
+
+  // Record the reordered methods
+  for (BinaryFunction& func : functions) {
+    methods.push_back(func.method);
+  }
+  // Record the result for the empty methods
+  methods.insert(methods.end(), empty_methods.begin(), empty_methods.end());
+}
