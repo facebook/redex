4565ed164f03d28baa660474865ab327dd0be0ec
William Sanville <wsanville@meta.com>
2023-05-02 16:46:14 -0700

    Promote D44443651 to staging, stable
    Reviewed By: ssj933
    
    Differential Revision: D45486463
    
    fbshipit-source-id: 0faacbd20f6bfc6d79bb40707c76951d383fd955

--- libredex/StringTreeSet.cpp
+++ libredex/StringTreeSet.cpp
@@ -8,18 +8,24 @@
 #include "StringTreeSet.h"
 
 #include "Debug.h"
+#include <cmath>
 #include <unordered_map>
 
-void StringTreeSet::insert(const std::string& s, size_t start) {
+template <typename ValueType>
+void StringTreeMap<ValueType>::insert(const std::string& s,
+                                      ValueType value,
+                                      size_t start) {
   if (start == s.size()) {
     m_terminal = true;
+    m_value = value;
     return;
   }
   always_assert(start < s.size());
-  m_map[s.at(start)].insert(s, start + 1);
+  m_map[s.at(start)].insert(s, value, start + 1);
 }
 
-void StringTreeSet::encode(std::ostringstream& oss) const {
+template <typename ValueType>
+void StringTreeMap<ValueType>::encode(std::ostringstream& oss) const {
   if (!m_terminal && m_map.size() == 1) {
     auto&& [c, rest] = *m_map.begin();
     always_assert(c >= 32);
@@ -28,14 +34,38 @@ void StringTreeSet::encode(std::ostringstream& oss) const {
     return;
   }
   always_assert(m_terminal || !m_map.empty());
-  always_assert(m_map.size() < 64);
-  size_t terminal_and_size = m_terminal | (m_map.size() << 1);
-  always_assert(terminal_and_size > 0);
-  always_assert(terminal_and_size < 128);
-  if (terminal_and_size >= 31) {
-    oss.put(31);
+  // Write <= 31 to pack in the following:
+  // A B C D E
+  // A = non terminal?
+  // B = payload is zero?
+  // CDE = how many payload chars come next, each char will have 0x40 set and
+  // use lowest 6 bits to denote the value.
+  constexpr uint8_t BITS_PER_PAYLOAD_UNIT = 6;
+  constexpr uint8_t FLAG_PAYLOAD_UNIT = 1 << BITS_PER_PAYLOAD_UNIT;
+  constexpr uint8_t PAYLOAD_MASK = FLAG_PAYLOAD_UNIT - 1;
+  constexpr uint8_t FLAG_NONTERMINAL = 1 << 4;
+  constexpr uint8_t FLAG_NO_PAYLOAD = 1 << 3;
+  constexpr uint8_t PAYLOAD_UNITS_MASK = FLAG_NO_PAYLOAD - 1;
+  size_t num_payload_chars =
+      std::ceil((double)(sizeof(ValueType) * 8) / BITS_PER_PAYLOAD_UNIT);
+  always_assert(num_payload_chars < FLAG_NO_PAYLOAD);
+  char header = m_terminal
+                    ? (m_value == 0 ? FLAG_NO_PAYLOAD : num_payload_chars)
+                    : FLAG_NONTERMINAL;
+  oss.put(header);
+  // Each payload char will be nonzero to make string encoding work (which will
+  // need to get shifted and assembled to read proper value).
+  if (m_terminal && m_value != 0) {
+    uint64_t value_to_write = m_value & 0xFFFFFFFF;
+    for (size_t i = 0; i < num_payload_chars; i++) {
+      oss.put(FLAG_PAYLOAD_UNIT | (value_to_write & PAYLOAD_MASK));
+      value_to_write = value_to_write >> BITS_PER_PAYLOAD_UNIT;
+    }
   }
-  oss.put(terminal_and_size);
+  // Followed by the size of this tree's map + 1.
+  size_t map_size = m_map.size() + 1;
+  always_assert(map_size < 128);
+  oss.put(map_size);
   bool first{true};
   std::unordered_map<char, std::ostringstream::pos_type> offsets;
   for (auto&& [c, nested] : m_map) {
@@ -66,13 +96,36 @@ void StringTreeSet::encode(std::ostringstream& oss) const {
   }
 }
 
-std::string StringTreeSet::encode_string_tree_set(
-    const std::vector<std::string>& strings) {
-  StringTreeSet sts;
-  for (const auto& s : strings) {
-    sts.insert(s);
+template <typename ValueType>
+std::string StringTreeMap<ValueType>::encode_string_tree_map(
+    const std::map<std::string, ValueType>& strings) {
+  StringTreeMap<ValueType> stm;
+  for (const auto& [s, v] : strings) {
+    stm.insert(s, v);
   }
   std::ostringstream oss;
-  sts.encode(oss);
+  stm.encode(oss);
+  return oss.str();
+}
+
+template class StringTreeMap<int16_t>;
+template class StringTreeMap<int32_t>;
+
+void StringTreeSet::encode(std::ostringstream& oss) const {
+  StringTreeMap<int16_t> stm;
+  for (const auto& s : m_set) {
+    stm.insert(s, 0);
+  }
+  return stm.encode(oss);
+}
+
+std::string StringTreeSet::encode_string_tree_set(
+    const std::vector<std::string>& strings) {
+  StringTreeMap<int16_t> stm;
+  for (const auto& s : strings) {
+    stm.insert(s, 0);
+  }
+  std::ostringstream oss;
+  stm.encode(oss);
   return oss.str();
 }
