00ee9f9885044490a54fe6f16d77373a290b4051
Daniel Hill <drhill@meta.com>
2023-05-10 09:04:52 -0700

    Make add_assets explicity provide apksigner tool path
    Summary:
    add_assets.py calls into the apksigner tool under the redex scripts without explicitly providing the path.
    
    We should provide this explicitly.
    
    Reviewed By: IanChilds
    
    Differential Revision: D45706080
    
    fbshipit-source-id: 86e587cd7b1165e84966f41ccd66372acc79b45d

--- test/debug_info/add_assets.py
+++ test/debug_info/add_assets.py
@@ -8,7 +8,7 @@ import argparse
 import os
 import zipfile
 
-from pyredex.utils import sign_apk
+from pyredex.utils import add_tool_override, sign_apk
 
 
 parser = argparse.ArgumentParser()
@@ -17,10 +17,12 @@ parser.add_argument("assets", help="Files to add to assets/", nargs="+")
 parser.add_argument("--keystore")
 parser.add_argument("--keypass")
 parser.add_argument("--keyalias")
+parser.add_argument("--apksigner_path", help="Path to apksigner tool.")
 args = parser.parse_args()
 
 with zipfile.ZipFile(args.apk, "a") as zf:
     for asset in args.assets:
         zf.write(asset, os.path.join("assets", os.path.basename(asset)))
 
+add_tool_override("apksigner", args.apksigner_path)
 sign_apk(args.keystore, args.keypass, args.keyalias, args.apk)
