1fd69b02b56ac3397ba14e8c4e26598d8cb68dda
Guojin He <guojinhe@meta.com>
2023-06-28 14:34:00 -0700

    Introducing ArtProfileWriterPass-split 2 promote to redex-stable
    Summary: similar to previous diff on the stack, but this one promotes it into redex-stable
    
    Reviewed By: agampe
    
    Differential Revision: D47096556
    
    fbshipit-source-id: e79e1dc93c314bf93f60cafd061400fcc8d89370

---
+++ opt/art-profile-writer/ArtProfileWriterPass.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include "Pass.h"
+
+class ArtProfileWriterPass : public Pass {
+ public:
+  ArtProfileWriterPass() : Pass("ArtProfileWriterPass") {}
+
+  redex_properties::PropertyInteractions get_property_interactions()
+      const override {
+    using namespace redex_properties::interactions;
+    using namespace redex_properties::names;
+    return {
+        {DexLimitsObeyed, Preserves},
+        {HasSourceBlocks, Preserves},
+        {NoInitClassInstructions, Preserves},
+        {NoSpuriousGetClassCalls, Preserves},
+        {RenameClass, Preserves},
+    };
+  }
+
+  void bind_config() override;
+  void run_pass(DexStoresVector&, ConfigFiles&, PassManager&) override;
+
+ private:
+  struct PerfConfig {
+    float appear100_threshold;
+    float call_count_threshold;
+    std::vector<std::string> interactions{"ColdStart"};
+
+    PerfConfig()
+        : appear100_threshold(101.0), call_count_threshold(0) {} // Default: off
+    PerfConfig(float a, float c)
+        : appear100_threshold(a), call_count_threshold(c) {}
+  };
+
+  PerfConfig m_perf_config;
+};
