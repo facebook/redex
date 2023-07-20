795db2570f0c2afaa23b8efdb9aacefc1616914e
Michał Gregorczyk <michalgr@meta.com>
2023-07-19 17:00:15 -0700

    Introduce AddSecondaryDexPass
    Reviewed By: NTillmann, agampe
    
    Differential Revision: D47503107
    
    fbshipit-source-id: 95b7ea475fd210ea3301f3ad548a12b68d972e8b

---
+++ opt/add-secondary-dex/AddSecondaryDexPass.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include "Pass.h"
+
+class AddSecondaryDexPass : public Pass {
+ public:
+  AddSecondaryDexPass() : Pass("AddSecondaryDexPass") {}
+
+  redex_properties::PropertyInteractions get_property_interactions()
+      const override {
+    using namespace redex_properties::interactions;
+    using namespace redex_properties::names;
+    return {
+        {DexLimitsObeyed, Preserves},
+        {HasSourceBlocks, Preserves},
+        {NoInitClassInstructions, Preserves},
+        {NoSpuriousGetClassCalls, Preserves},
+        {RenameClass, Preserves},
+    };
+  }
+
+  void run_pass(DexStoresVector&, ConfigFiles&, PassManager&) override;
+};
