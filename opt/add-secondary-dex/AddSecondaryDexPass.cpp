bdce81dcc58d79996af8cd24f6d55a1a9d652983
Michał Gregorczyk <michalgr@meta.com>
2023-07-19 17:00:15 -0700

    Promote D47503107 to Redex staging and stable
    Summary:
    redex-stable does not have D46408315 yet, so I replaced calls to `create_canary`
    with `interdex::create_canary`.
    
    Reviewed By: NTillmann
    
    Differential Revision: D47548016
    
    fbshipit-source-id: 30a6613f4b6ff748b73a4fd6d5ccb7086f4d1f7d

---
+++ opt/add-secondary-dex/AddSecondaryDexPass.cpp
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include "AddSecondaryDexPass.h"
+#include "InterDexPass.h"
+
+void AddSecondaryDexPass::run_pass(DexStoresVector& stores,
+                                   ConfigFiles&,
+                                   PassManager&) {
+  redex_assert(!stores.empty());
+  auto& root_dexen = stores[0].get_dexen();
+  root_dexen.emplace_back(1, interdex::create_canary(root_dexen.size()));
+}
+
+static AddSecondaryDexPass s_pass;
