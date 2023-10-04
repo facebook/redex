c577eab1c83e105b69ea32d12398eb923a3148fe
Nikolai Tillmann <nikolait@meta.com>
2023-10-04 09:58:18 -0700

    respect no_optimizations
    Reviewed By: ssj933
    
    Differential Revision: D49667802
    
    fbshipit-source-id: 92f4c039da58cb54d85da0b6865102c4e5b9bd1a

--- opt/stringbuilder-outliner/StringBuilderOutliner.cpp
+++ opt/stringbuilder-outliner/StringBuilderOutliner.cpp
@@ -456,14 +456,18 @@ void StringBuilderOutlinerPass::run_pass(DexStoresVector& stores,
   Outliner outliner(m_config);
   // 1) Determine which methods have candidates for outlining.
   walk::parallel::code(scope, [&](const DexMethod* method, IRCode& code) {
-    outliner.analyze(code);
+    if (!method->rstate.no_optimizations()) {
+      outliner.analyze(code);
+    }
   });
   // 2) Determine which candidates occur frequently enough to be worth
   // outlining. Build the corresponding outline helper functions.
   outliner.create_outline_helpers(&stores);
   // 3) Actually do the outlining.
   walk::parallel::code(scope, [&](const DexMethod* method, IRCode& code) {
-    outliner.transform(&code);
+    if (!method->rstate.no_optimizations()) {
+      outliner.transform(&code);
+    }
   });
 
   mgr.incr_metric("stringbuilders_removed",
