4f1daf5ad14c2a5f8fc39336e94564d22d4e457e
Maxime Arthaud <arthaud@meta.com>
2023-10-06 06:06:39 -0700

    Implement partition and environment abstract domains on top of PatriciaTreeHashMap
    Summary:
    The previous diff introduced `PatriciaTreeHashMap`, a generalized hash map on top of patricia trees.
    This diff implements partition and environment abstract domains, similar to `PatriciaTreeMapAbstractPartition` and `PatriciaTreeMapAbstractEnvironment`.
    
    Reviewed By: arnaudvenet
    
    Differential Revision: D49697554
    
    fbshipit-source-id: 27dedb945ee1668311a0701393242a2958f9d170

---
+++ sparta/include/sparta/PatriciaTreeHashMapAbstractPartition.h
@@ -0,0 +1,255 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <cstddef>
+#include <functional>
+#include <initializer_list>
+#include <ostream>
+#include <utility>
+
+#include <sparta/AbstractDomain.h>
+#include <sparta/PatriciaTreeHashMap.h>
+
+namespace sparta {
+
+/*
+ * An abstract partition based on `PatriciaTreeHashMap`.
+ *
+ * See `PatriciaTreeMapAbstractPartition` for more information.
+ */
+template <typename Label, typename Domain>
+class PatriciaTreeHashMapAbstractPartition final
+    : public AbstractDomain<
+          PatriciaTreeHashMapAbstractPartition<Label, Domain>> {
+ public:
+  struct ValueInterface {
+    using type = Domain;
+
+    static type default_value() { return type::bottom(); }
+
+    static bool is_default_value(const type& x) { return x.is_bottom(); }
+
+    static bool equals(const type& x, const type& y) { return x.equals(y); }
+
+    static bool leq(const type& x, const type& y) { return x.leq(y); }
+
+    constexpr static AbstractValueKind default_value_kind =
+        AbstractValueKind::Bottom;
+  };
+
+  using MapType = PatriciaTreeHashMap<Label, Domain, ValueInterface>;
+
+  /*
+   * The default constructor produces the Bottom value.
+   */
+  PatriciaTreeHashMapAbstractPartition() = default;
+
+  explicit PatriciaTreeHashMapAbstractPartition(
+      std::initializer_list<std::pair<Label, Domain>> l) {
+    for (const auto& p : l) {
+      set(p.first, p.second);
+    }
+  }
+
+  /*
+   * Number of bindings not set to Bottom. This operation is not defined if the
+   * PatriciaTreeHashMapAbstractPartition is set to Top.
+   */
+  size_t size() const {
+    RUNTIME_CHECK(!is_top(), undefined_operation());
+    return m_map.size();
+  }
+
+  /*
+   * Get the bindings that are not set to Bottom. This operation is not defined
+   * if the PatriciaTreeHashMapAbstractPartition is set to Top.
+   */
+  const MapType& bindings() const {
+    RUNTIME_CHECK(!is_top(), undefined_operation());
+    return m_map;
+  }
+
+  const Domain& get(const Label& label) const {
+    if (is_top()) {
+      static const Domain top = Domain::top();
+      return top;
+    }
+    return m_map.at(label);
+  }
+
+  /*
+   * This is a no-op if the partition is set to Top.
+   */
+  PatriciaTreeHashMapAbstractPartition& set(const Label& label,
+                                            const Domain& value) {
+    return set_internal(label, value);
+  }
+
+  /*
+   * This is a no-op if the partition is set to Top.
+   */
+  PatriciaTreeHashMapAbstractPartition& set(const Label& label,
+                                            Domain&& value) {
+    return set_internal(label, std::move(value));
+  }
+
+  /*
+   * This is a no-op if the partition is set to Top.
+   */
+
+  template <typename Operation> // void(Domain*)
+  PatriciaTreeHashMapAbstractPartition& update(const Label& label,
+                                               Operation&& operation) {
+    if (is_top()) {
+      return *this;
+    }
+    m_map.update(std::forward<Operation>(operation), label);
+    return *this;
+  }
+
+  template <typename Operation> // void(Domain*)
+  bool map(Operation&& f) {
+    if (is_top()) {
+      return false;
+    }
+    return m_map.map(std::forward<Operation>(f));
+  }
+
+  bool is_top() const { return m_is_top; }
+
+  bool is_bottom() const { return !m_is_top && m_map.empty(); }
+
+  void set_to_bottom() {
+    m_map.clear();
+    m_is_top = false;
+  }
+
+  void set_to_top() {
+    m_map.clear();
+    m_is_top = true;
+  }
+
+  bool leq(const PatriciaTreeHashMapAbstractPartition& other) const {
+    if (is_top()) {
+      return other.is_top();
+    }
+    if (other.is_top()) {
+      return true;
+    }
+    return m_map.leq(other.m_map);
+  }
+
+  bool equals(const PatriciaTreeHashMapAbstractPartition& other) const {
+    if (m_is_top != other.m_is_top) {
+      return false;
+    }
+    return m_map.equals(other.m_map);
+  }
+
+  void join_with(const PatriciaTreeHashMapAbstractPartition& other) {
+    join_like_operation(
+        other, [](Domain* x, const Domain& y) { return x->join_with(y); });
+  }
+
+  void widen_with(const PatriciaTreeHashMapAbstractPartition& other) {
+    join_like_operation(
+        other, [](Domain* x, const Domain& y) { return x->widen_with(y); });
+  }
+
+  void meet_with(const PatriciaTreeHashMapAbstractPartition& other) {
+    meet_like_operation(
+        other, [](Domain* x, const Domain& y) { return x->meet_with(y); });
+  }
+
+  void narrow_with(const PatriciaTreeHashMapAbstractPartition& other) {
+    meet_like_operation(
+        other, [](Domain* x, const Domain& y) { return x->narrow_with(y); });
+  }
+
+  template <typename Operation> // Domain(Domain*, const Domain&)
+  void join_like_operation(const PatriciaTreeHashMapAbstractPartition& other,
+                           Operation&& operation) {
+    if (is_top()) {
+      return;
+    }
+    if (other.is_top()) {
+      set_to_top();
+      return;
+    }
+    m_map.union_with(std::forward<Operation>(operation), other.m_map);
+  }
+
+  template <typename Operation> // void(Domain*, const Domain&)
+  void meet_like_operation(const PatriciaTreeHashMapAbstractPartition& other,
+                           Operation&& operation) {
+    if (is_top()) {
+      *this = other;
+      return;
+    }
+    if (other.is_top()) {
+      return;
+    }
+    m_map.intersection_with(std::forward<Operation>(operation), other.m_map);
+  }
+
+  template <typename Operation> // void(Domain*, const Domain&)
+  void difference_like_operation(
+      const PatriciaTreeHashMapAbstractPartition& other,
+      Operation&& operation) {
+    if (other.is_top()) {
+      set_to_bottom();
+    } else if (is_top()) {
+      return;
+    } else {
+      m_map.difference_with(std::forward<Operation>(operation), other.m_map);
+    }
+  }
+
+  static PatriciaTreeHashMapAbstractPartition bottom() {
+    return PatriciaTreeHashMapAbstractPartition();
+  }
+
+  static PatriciaTreeHashMapAbstractPartition top() {
+    auto part = PatriciaTreeHashMapAbstractPartition();
+    part.m_is_top = true;
+    return part;
+  }
+
+ private:
+  template <typename D>
+  PatriciaTreeHashMapAbstractPartition& set_internal(const Label& label,
+                                                     D&& value) {
+    if (is_top()) {
+      return *this;
+    }
+    m_map.insert_or_assign(label, std::forward<D>(value));
+    return *this;
+  }
+
+  MapType m_map;
+  bool m_is_top{false};
+};
+
+} // namespace sparta
+
+template <typename Label, typename Domain>
+inline std::ostream& operator<<(
+    std::ostream& o,
+    const typename sparta::PatriciaTreeHashMapAbstractPartition<Label, Domain>&
+        partition) {
+  if (partition.is_bottom()) {
+    o << "_|_";
+  } else if (partition.is_top()) {
+    o << "T";
+  } else {
+    o << "[#" << partition.size() << "]";
+    o << partition.bindings();
+  }
+  return o;
+}
---
+++ sparta/test/PatriciaTreeHashMapAbstractPartitionTest.cpp
@@ -0,0 +1,256 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <sparta/HashedSetAbstractDomain.h>
+#include <sparta/PatriciaTreeHashMapAbstractPartition.h>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include <sstream>
+#include <string>
+
+#include "AbstractDomainPropertyTest.h"
+
+using namespace sparta;
+
+using Domain = HashedSetAbstractDomain<std::string>;
+
+using Partition = PatriciaTreeHashMapAbstractPartition<uint32_t, Domain>;
+
+INSTANTIATE_TYPED_TEST_CASE_P(PatriciaTreeHashMapAbstractPartition,
+                              AbstractDomainPropertyTest,
+                              Partition);
+
+template <>
+std::vector<Partition>
+AbstractDomainPropertyTest<Partition>::non_extremal_values() {
+  Partition p1({{1, Domain({"a", "b"})},
+                {2, Domain("c")},
+                {3, Domain({"d", "e", "f"})},
+                {4, Domain({"a", "f"})}});
+  Partition p2({{0, Domain({"c", "f"})},
+                {2, Domain({"c", "d"})},
+                {3, Domain({"d", "e", "g", "h"})}});
+  return {p1, p2};
+}
+
+TEST(PatriciaTreeHashMapAbstractPartitionTest, basicPartialOrders) {
+  {
+    Partition p1;
+    EXPECT_TRUE(p1.leq(p1));
+  }
+
+  {
+    Partition p1;
+    Partition p2;
+    EXPECT_TRUE(p1.leq(p2));
+    EXPECT_TRUE(p2.leq(p1));
+  }
+
+  {
+    Partition p1({{1, Domain({"a"})}});
+    Partition p2({{1, Domain({"a"})}});
+    EXPECT_TRUE(p1.leq(p2));
+    EXPECT_TRUE(p2.leq(p1));
+  }
+
+  {
+    Partition p1({{2, Domain({"a"})}, {3, Domain({"a"})}});
+    Partition p2({{2, Domain({"a"})}, {3, Domain({"a"})}});
+    EXPECT_TRUE(p1.leq(p2));
+    EXPECT_TRUE(p2.leq(p1));
+  }
+
+  {
+    Partition p1;
+    Partition p2({{1, Domain({"a"})}});
+    Partition p3({{2, Domain({"a"})}, {3, Domain({"a"})}});
+    EXPECT_TRUE(p1.leq(p2));
+    EXPECT_FALSE(p2.leq(p1));
+    EXPECT_TRUE(p1.leq(p3));
+    EXPECT_FALSE(p3.leq(p1));
+  }
+
+  {
+    Partition p1({{1, Domain({"a"})}});
+    Partition p2({{1, Domain({"a"})}, {2, Domain({"a"})}});
+    Partition p3({{2, Domain({"a"})}, {3, Domain({"a"})}});
+    EXPECT_TRUE(p1.leq(p2));
+    EXPECT_FALSE(p2.leq(p1));
+    EXPECT_FALSE(p1.leq(p3));
+    EXPECT_FALSE(p3.leq(p1));
+  }
+
+  {
+    Partition p1;
+    p1.set_to_bottom();
+    p1.set(1, Domain({"a"}));
+    p1.set(2, Domain({"a"}));
+    Partition p2;
+    p2.set_to_bottom();
+    p2.set(1, Domain({"a"}));
+    EXPECT_FALSE(p1.leq(p2));
+    EXPECT_TRUE(p2.leq(p1));
+  }
+
+  {
+    Partition p1({{1, Domain({"a"})}, {3, Domain({"a"})}});
+    Partition p2({{1, Domain({"a"})}, {2, Domain({"a"})}, {3, Domain({"a"})}});
+    EXPECT_TRUE(p1.leq(p2));
+    EXPECT_FALSE(p2.leq(p1));
+  }
+
+  {
+    Partition p1({{1, Domain({"a"})}, {3, Domain({"b"})}});
+    Partition p2({{1, Domain({"a"})}, {2, Domain({"a"})}, {3, Domain({"a"})}});
+    EXPECT_FALSE(p1.leq(p2));
+    EXPECT_FALSE(p2.leq(p1));
+  }
+
+  {
+    Partition p1({{1, Domain({"a"})}, {3, Domain({"b"})}});
+    Partition p2(
+        {{1, Domain({"a", "b"})}, {2, Domain({"a"})}, {3, Domain({"a", "b"})}});
+    EXPECT_TRUE(p1.leq(p2));
+    EXPECT_FALSE(p2.leq(p1));
+  }
+}
+
+TEST(PatriciaTreeHashMapAbstractPartitionTest, latticeOperations) {
+  Partition p1({{1, Domain({"a", "b"})},
+                {2, Domain("c")},
+                {3, Domain({"d", "e", "f"})},
+                {4, Domain({"a", "f"})}});
+  Partition p2({{0, Domain({"c", "f"})},
+                {2, Domain({"c", "d"})},
+                {3, Domain({"d", "e", "g", "h"})}});
+  EXPECT_EQ(4, p1.size());
+  EXPECT_EQ(3, p2.size());
+
+  EXPECT_FALSE(p1.leq(p2));
+  EXPECT_FALSE(p2.leq(p1));
+
+  EXPECT_FALSE(p1.equals(p2));
+  EXPECT_TRUE(Partition::bottom().equals(Partition()));
+
+  Partition join = p1.join(p2);
+  EXPECT_EQ(5, join.size());
+  EXPECT_EQ(join.get(0).elements(), p2.get(0).elements());
+  EXPECT_EQ(join.get(1).elements(), p1.get(1).elements());
+  EXPECT_THAT(join.get(2).elements(),
+              ::testing::UnorderedElementsAre("c", "d"));
+  EXPECT_THAT(join.get(3).elements(),
+              ::testing::UnorderedElementsAre("d", "e", "f", "g", "h"));
+  EXPECT_EQ(join.get(4).elements(), p1.get(4).elements());
+  EXPECT_TRUE(join.equals(p1.widening(p2)));
+
+  Partition meet = p1.meet(p2);
+  EXPECT_EQ(2, meet.size());
+  EXPECT_THAT(meet.get(2).elements(), ::testing::ElementsAre("c"));
+  EXPECT_THAT(meet.get(3).elements(),
+              ::testing::UnorderedElementsAre("d", "e"));
+  EXPECT_EQ(meet, p1.narrowing(p2));
+}
+
+TEST(PatriciaTreeHashMapAbstractPartitionTest, destructiveOperations) {
+  Partition p1({{1, Domain({"a", "b"})}});
+  Partition p2({{2, Domain({"c", "d"})}, {3, Domain({"g", "h"})}});
+
+  p1.set(2, Domain({"c", "f"})).set(4, Domain({"e", "f", "g"}));
+  EXPECT_EQ(3, p1.size());
+  EXPECT_THAT(p1.get(1).elements(), ::testing::UnorderedElementsAre("a", "b"));
+  EXPECT_THAT(p1.get(2).elements(), ::testing::UnorderedElementsAre("c", "f"));
+  EXPECT_THAT(p1.get(4).elements(),
+              ::testing::UnorderedElementsAre("e", "f", "g"));
+
+  Partition join = p1;
+  join.join_with(p2);
+  EXPECT_EQ(4, join.size());
+  EXPECT_EQ(join.get(1).elements(), p1.get(1).elements());
+  EXPECT_EQ(join.get(4).elements(), p1.get(4).elements());
+  EXPECT_THAT(join.get(2).elements(),
+              ::testing::UnorderedElementsAre("c", "d", "f"));
+  EXPECT_EQ(join.get(3).elements(), p2.get(3).elements());
+
+  Partition widening = p1;
+  widening.widen_with(p2);
+  EXPECT_TRUE(widening.equals(join));
+
+  Partition meet = p1;
+  meet.meet_with(p2);
+  EXPECT_EQ(1, meet.size());
+  EXPECT_THAT(meet.get(2).elements(), ::testing::ElementsAre("c"));
+
+  Partition narrowing = p1;
+  narrowing.narrow_with(p2);
+  EXPECT_TRUE(narrowing.equals(meet));
+
+  auto add_e = [](Domain* s) { s->add("e"); };
+  p1.update(1, add_e).update(2, add_e);
+  EXPECT_EQ(3, p1.size());
+  EXPECT_THAT(p1.get(1).elements(),
+              ::testing::UnorderedElementsAre("a", "b", "e"));
+  EXPECT_THAT(p1.get(2).elements(),
+              ::testing::UnorderedElementsAre("c", "e", "f"));
+  EXPECT_THAT(p1.get(4).elements(),
+              ::testing::UnorderedElementsAre("e", "f", "g"));
+
+  Partition p3 = p2;
+  EXPECT_EQ(2, p3.size());
+  p3.update(1, add_e).update(2, add_e);
+  EXPECT_EQ(2, p3.size());
+  EXPECT_THAT(p3.get(2).elements(),
+              ::testing::UnorderedElementsAre("c", "d", "e"));
+  EXPECT_THAT(p3.get(3).elements(), ::testing::UnorderedElementsAre("g", "h"));
+
+  auto make_bottom = [](Domain* s) { s->set_to_bottom(); };
+  Partition p4 = p2;
+  p4.update(2, make_bottom);
+  EXPECT_FALSE(p4.is_bottom());
+  EXPECT_EQ(1, p4.size());
+
+  auto refine_de = [](Domain* s) { s->meet_with(Domain({"d", "e"})); };
+  EXPECT_EQ(2, p2.size());
+  p2.update(1, refine_de).update(2, refine_de);
+  EXPECT_EQ(2, p2.size());
+  EXPECT_TRUE(p2.get(1).is_bottom());
+  EXPECT_THAT(p2.get(2).elements(), ::testing::ElementsAre("d"));
+  EXPECT_THAT(p2.get(3).elements(), ::testing::UnorderedElementsAre("g", "h"));
+
+  Partition p5({{0, Domain({"c", "d"})},
+                {2, Domain::bottom()},
+                {3, Domain({"a", "f", "g"})}});
+
+  EXPECT_EQ(2, p5.size());
+  p5.set(0, Domain::bottom());
+  p5.set(3, Domain::bottom());
+  EXPECT_TRUE(p5.is_bottom());
+  EXPECT_EQ(Partition::bottom(), p5);
+  EXPECT_TRUE(p5.get(4).is_bottom());
+
+  Partition p6{Partition::top()};
+  EXPECT_TRUE(p6.get(0).is_top());
+
+  // All operations on Top are no-ops.
+  p6.set(1, Domain::bottom());
+  EXPECT_TRUE(p6.get(1).is_top());
+  EXPECT_TRUE(p6.is_top());
+
+  p6.update(1, make_bottom);
+  EXPECT_TRUE(p6.get(1).is_top());
+  EXPECT_TRUE(p6.is_top());
+}
+
+TEST(PatriciaTreeHashMapAbstractPartitionTest, map) {
+  Partition p1({{1, Domain({"a", "b"})}});
+  bool any_changes = p1.map([](Domain*) {});
+  EXPECT_FALSE(any_changes);
+
+  any_changes = p1.map([](Domain* d) { d->set_to_bottom(); });
+  EXPECT_TRUE(any_changes);
+  EXPECT_TRUE(p1.is_bottom());
+}
