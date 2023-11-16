395da1c66fd3ab66d999b52320538b97d8d6e282
Adrian Catana <adicatana@meta.com>
2023-11-15 18:23:20 -0800

    Coldstart method ordering
    Summary: Add the logic to read an orderfile of methods that will be placed at the beginning of the code items section.
    
    Reviewed By: NTillmann
    
    Differential Revision: D51071073
    
    fbshipit-source-id: ed661d303a5d1d9211906fe5274b46e9175a101f

--- test/integ/DexOutputTest.cpp
+++ test/integ/DexOutputTest.cpp
@@ -28,6 +28,7 @@ class DexOutputTest : public RedexIntegrationTest {
 
   std::vector<DexMethod*> get_ordered_methods(DexOutput& dout) {
     std::vector<DexMethod*> code_items;
+    code_items.reserve(dout.m_code_item_emits.size());
     for (auto& ci : dout.m_code_item_emits) {
       code_items.push_back(ci.method);
     }
@@ -260,6 +261,125 @@ TEST_F(DexOutputTest, TEST_COMPRESSION_ORDERER_PERF_SENSITIVE) {
       "LDexOutputTest;.DgetSixpublic:()I",
       "LDexOutputTest;.AjustReturnFive:()I",
       "LDexOutputTest;.EjustReturnFive:()I",
+      "LDexOutputTest;.BjustCallSixpublic:()I",
+      "LDexOutputTest;.GjustCallSixpublic:()I"};
+
+  EXPECT_TRUE(std::equal(method_names.begin(), method_names.end(),
+                         expected_order.begin()));
+}
+
+TEST_F(DexOutputTest, TEST_COLDSTART_ORDER) {
+  std::vector<SortMode> sort_modes{SortMode::METHOD_COLDSTART_ORDER,
+                                   SortMode::METHOD_SIMILARITY};
+  std::string profile_path = std::getenv("coldstart_methods_file");
+
+  Json::Value cfg;
+  std::istringstream temp_json("{\"coldstart_methods_file\":\"" + profile_path +
+                               "\"}");
+  temp_json >> cfg;
+
+  ConfigFiles config_files(cfg);
+  config_files.parse_global_config();
+  std::unique_ptr<PositionMapper> pos_mapper(PositionMapper::make(""));
+
+  (*classes)[1]->set_perf_sensitive(PerfSensitiveGroup::BETAMAP_ORDERED);
+  (*classes)[2]->set_perf_sensitive(PerfSensitiveGroup::BETAMAP_ORDERED);
+
+  DexMethod::make_method("LDexOutputTest2$Class;.someRandomMethodNotInDex:(I)I")
+      ->make_concrete(ACC_PUBLIC, false);
+
+  auto scope = build_class_scope(stores);
+
+  // Lower the code
+  walk::parallel::methods<>(
+      scope, [](DexMethod* m) { instruction_lowering::lower(m, true); });
+
+  auto gtypes = std::make_shared<GatheredTypes>(&*classes);
+  DexOutput dout("", &*classes, std::move(gtypes), nullptr, true, 0, nullptr, 0,
+                 DebugInfoKind::NoCustomSymbolication, nullptr, config_files,
+                 pos_mapper.get(), nullptr, nullptr, DexOutputConfig(), 25);
+
+  dout.prepare(SortMode::DEFAULT, sort_modes, config_files, "dex\n039");
+  auto code_items = get_ordered_methods(dout);
+
+  uint32_t i = 0;
+  std::vector<std::string> method_names(code_items.size());
+  for (auto* m : code_items) {
+    method_names[i++] = show(m);
+  }
+
+  std::vector<std::string> expected_order = {
+      "LDexOutputTest$PerfSensitiveClass;.<init>:(LDexOutputTest;)V",
+      "LDexOutputTest$SecondPerfSensitiveClass;.<init>:(LDexOutputTest;)V",
+      "LDexOutputTest$PerfSensitiveClass;.EjustReturnFive:()I",
+      "LDexOutputTest$SecondPerfSensitiveClass;.EjustReturnFive:()I",
+      "LDexOutputTest$SecondPerfSensitiveClass;.FsomeLogic:(I)I",
+      "LDexOutputTest$PerfSensitiveClass;.FsomeLogic:(I)I",
+      "LDexOutputTest$NonPerfSensitiveClass;.<init>:(LDexOutputTest;)V",
+      "LDexOutputTest;.<init>:()V",
+      "LDexOutputTest$NonPerfSensitiveClass;.EjustReturnFive:()I",
+      "LDexOutputTest;.AjustReturnFive:()I",
+      "LDexOutputTest;.EjustReturnFive:()I",
+      "LDexOutputTest;.DgetSixpublic:()I",
+      "LDexOutputTest$NonPerfSensitiveClass;.FsomeLogic:(I)I",
+      "LDexOutputTest;.CsomeLogic:(I)I",
+      "LDexOutputTest;.FsomeLogic:(I)I",
+      "LDexOutputTest;.HsomeLogic:(I)I",
+      "LDexOutputTest;.BjustCallSixpublic:()I",
+      "LDexOutputTest;.GjustCallSixpublic:()I"};
+
+  EXPECT_TRUE(std::equal(method_names.begin(), method_names.end(),
+                         expected_order.begin()));
+}
+
+TEST_F(DexOutputTest, TEST_COLDSTART_ORDER_EMPTY_FILE) {
+  std::vector<SortMode> sort_modes{SortMode::METHOD_COLDSTART_ORDER,
+                                   SortMode::METHOD_SIMILARITY};
+  Json::Value cfg;
+  ConfigFiles config_files(cfg);
+  config_files.parse_global_config();
+  std::unique_ptr<PositionMapper> pos_mapper(PositionMapper::make(""));
+
+  (*classes)[1]->set_perf_sensitive(PerfSensitiveGroup::BETAMAP_ORDERED);
+  (*classes)[2]->set_perf_sensitive(PerfSensitiveGroup::BETAMAP_ORDERED);
+
+  auto scope = build_class_scope(stores);
+
+  // Lower the code
+  walk::parallel::methods<>(
+      scope, [](DexMethod* m) { instruction_lowering::lower(m, true); });
+
+  auto gtypes = std::make_shared<GatheredTypes>(&*classes);
+  DexOutput dout("", &*classes, std::move(gtypes), nullptr, true, 0, nullptr, 0,
+                 DebugInfoKind::NoCustomSymbolication, nullptr, config_files,
+                 pos_mapper.get(), nullptr, nullptr, DexOutputConfig(), 25);
+
+  dout.prepare(SortMode::DEFAULT, sort_modes, config_files, "dex\n039");
+  auto code_items = get_ordered_methods(dout);
+
+  uint32_t i = 0;
+  std::vector<std::string> method_names(code_items.size());
+  for (auto* m : code_items) {
+    method_names[i++] = show(m);
+  }
+
+  std::vector<std::string> expected_order = {
+      "LDexOutputTest$NonPerfSensitiveClass;.<init>:(LDexOutputTest;)V",
+      "LDexOutputTest$PerfSensitiveClass;.<init>:(LDexOutputTest;)V",
+      "LDexOutputTest$SecondPerfSensitiveClass;.<init>:(LDexOutputTest;)V",
+      "LDexOutputTest;.<init>:()V",
+      "LDexOutputTest$NonPerfSensitiveClass;.EjustReturnFive:()I",
+      "LDexOutputTest$PerfSensitiveClass;.EjustReturnFive:()I",
+      "LDexOutputTest$SecondPerfSensitiveClass;.EjustReturnFive:()I",
+      "LDexOutputTest;.AjustReturnFive:()I",
+      "LDexOutputTest;.EjustReturnFive:()I",
+      "LDexOutputTest;.DgetSixpublic:()I",
+      "LDexOutputTest$NonPerfSensitiveClass;.FsomeLogic:(I)I",
+      "LDexOutputTest$PerfSensitiveClass;.FsomeLogic:(I)I",
+      "LDexOutputTest$SecondPerfSensitiveClass;.FsomeLogic:(I)I",
+      "LDexOutputTest;.CsomeLogic:(I)I",
+      "LDexOutputTest;.FsomeLogic:(I)I",
+      "LDexOutputTest;.HsomeLogic:(I)I",
       "LDexOutputTest;.BjustCallSixpublic:()I",
       "LDexOutputTest;.GjustCallSixpublic:()I"};
 
