b39fdf52076c4f5f754ebb996f5edf8407c9a443
Bryan Tan <bryantan@technius.net>
2023-12-19 00:41:18 -0800

    Improvements to make SPARTA easier to use as a dependency
    Summary:
    This PR includes some improvements to the CMake configuration that makes SPARTA easier to use as a dependency (or at least I've found these useful for a project I'm working on). Specifically:
    
    * Generate a `sparta-config.cmake` file that is created in both the build folder and for the install target. This enables the library to be used with `find_package`.
    * (Breaking change) Install the `sparta_target.cmake` to `$LIBDIR/cmake/sparta` instead of `$PREFIX/cmake` to support \*nix systems as well as `find_package`. While this is a breaking change, it's unlikely that anyone used the installed file due to the bugs observed in https://github.com/facebook/SPARTA/issues/23.
    * (Breaking change) Use `sparta::` as the target namespace when exporting the `sparta` target. Projects that previously included the `sparta_target.cmake` will need to be updated to use  `sparta::sparta` instead of `sparta` when specifying it as a dependency.
    * Update the README with additional tips on how to use SPARTA in CMake projects.
    
    This PR is stacked on top of https://github.com/facebook/SPARTA/issues/23, and I'll rebase after it is merged.
    
    X-link: https://github.com/facebook/SPARTA/pull/24
    
    Reviewed By: arnaudvenet
    
    Differential Revision: D52238117
    
    Pulled By: arthaud
    
    fbshipit-source-id: abfad1312d5357a5adc64335d7a51e80935cf111

--- sparta/cmake_modules/Commons.cmake
+++ sparta/cmake_modules/Commons.cmake
@@ -44,14 +44,14 @@ macro(add_dependent_packages_for_sparta)
   configure_file(cmake_modules/gtest.cmake.in googletest-download/CMakeLists.txt)
   execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
     RESULT_VARIABLE result
-    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/googletest-download
+    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/googletest-download
   )
   if(result)
     message(FATAL_ERROR "CMake step for googletest failed: ${result}")
   endif()
   execute_process(COMMAND ${CMAKE_COMMAND} --build .
     RESULT_VARIABLE result
-    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/googletest-download
+    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/googletest-download
   )
   if(result)
     message(FATAL_ERROR "Build step for googletest failed: ${result}")
@@ -64,8 +64,8 @@ macro(add_dependent_packages_for_sparta)
   # Add googletest directly to our build. This defines
   # the gtest and gtest_main targets.
   add_subdirectory(
-    ${CMAKE_BINARY_DIR}/googletest-src
-    ${CMAKE_BINARY_DIR}/googletest-build
+    ${CMAKE_CURRENT_BINARY_DIR}/googletest-src
+    ${CMAKE_CURRENT_BINARY_DIR}/googletest-build
     EXCLUDE_FROM_ALL
   )
 
