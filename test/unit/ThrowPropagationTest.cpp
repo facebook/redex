a39958c87fbc93e46d029cdce94742cb0968d3c3
Nikolai Tillmann <nikolait@meta.com>
2024-01-24 14:50:35 -0800

    Retiring ThrowPropagationPass
    Summary:
    This pass is obsolete; it already was disabled, and is now fully subsumed by RMU.
    RMU still uses its guts, found in ThrowPropagationImpl. This remains, and the existing unit tests are adapted to only reference the -Impl, but not the obsolete -Pass.
    
    Reviewed By: thezhangwei
    
    Differential Revision: D52895792
    
    fbshipit-source-id: 204a3d865d35ae9cff469e80f3f7512fd4997600

--- test/unit/ThrowPropagationTest.cpp
+++ test/unit/ThrowPropagationTest.cpp
@@ -13,7 +13,8 @@
 #include "IRCode.h"
 #include "Purity.h"
 #include "RedexTest.h"
-#include "ThrowPropagationPass.h"
+#include "Show.h"
+#include "ThrowPropagationImpl.h"
 #include "VirtualScope.h"
 #include "Walkers.h"
 
@@ -26,19 +27,111 @@ class ThrowPropagationTest : public RedexTest {
   }
 };
 
+bool exclude_method(DexMethod* method) {
+  return method->get_code() == nullptr || is_abstract(method) ||
+         method->is_external() || is_native(method) ||
+         method->rstate.no_optimizations();
+}
+
+bool is_no_return_method(DexMethod* method) {
+  if (exclude_method(method)) {
+    return false;
+  }
+  bool can_return{false};
+  editable_cfg_adapter::iterate_with_iterator(
+      method->get_code(), [&can_return](const IRList::iterator& it) {
+        if (opcode::is_a_return(it->insn->opcode())) {
+          can_return = true;
+          return editable_cfg_adapter::LOOP_BREAK;
+        }
+        return editable_cfg_adapter::LOOP_CONTINUE;
+      });
+  return !can_return;
+}
+
+ConcurrentSet<DexMethod*> get_no_return_methods(const Scope& scope) {
+  ConcurrentSet<DexMethod*> concurrent_no_return_methods;
+  walk::parallel::methods(scope, [&](DexMethod* method) {
+    if (is_no_return_method(method)) {
+      concurrent_no_return_methods.insert(method);
+    }
+  });
+  return concurrent_no_return_methods;
+}
+
+void run_throw_propagation(const ConcurrentSet<DexMethod*>& no_return_methods,
+                           const method_override_graph::Graph& graph,
+                           IRCode* code) {
+  auto& cfg = code->cfg();
+  std::vector<DexMethod*> return_methods;
+  auto is_no_return_invoke = [&](IRInstruction* insn) {
+    if (!opcode::is_an_invoke(insn->opcode()) ||
+        insn->opcode() == OPCODE_INVOKE_SUPER) {
+      return false;
+    }
+    auto method_ref = insn->get_method();
+    DexMethod* method = resolve_method(method_ref, opcode_to_search(insn));
+    if (method == nullptr) {
+      return false;
+    }
+    if (insn->opcode() == OPCODE_INVOKE_INTERFACE &&
+        is_annotation(type_class(method->get_class()))) {
+      return false;
+    }
+    return_methods.clear();
+    auto check_for_no_return = [&](DexMethod* other_method) {
+      if (exclude_method(other_method)) {
+        return false;
+      }
+      if (!no_return_methods.count_unsafe(other_method)) {
+        return_methods.push_back(other_method);
+      }
+      return true;
+    };
+    if (!process_base_and_overriding_methods(
+            &graph, method, /* methods_to_ignore */ nullptr,
+            /* ignore_methods_with_assumenosideeffects */ false,
+            check_for_no_return)) {
+      return false;
+    }
+    return return_methods.empty();
+  };
+
+  throw_propagation_impl::ThrowPropagator impl(cfg);
+  size_t throws_inserted{0};
+  for (auto block : cfg.blocks()) {
+    auto ii = InstructionIterable(block);
+    for (auto it = ii.begin(); it != ii.end(); it++) {
+      auto insn = it->insn;
+      if (!is_no_return_invoke(insn)) {
+        continue;
+      }
+
+      if (impl.try_apply(block->to_cfg_instruction_iterator(it))) {
+        throws_inserted++;
+      }
+
+      // Stop processing more instructions in this block
+      break;
+    }
+  }
+
+  if (throws_inserted > 0) {
+    cfg.remove_unreachable_blocks();
+    cfg.recompute_registers_size();
+  }
+}
+
 void test(const Scope& scope,
           const std::string& code_str,
           const std::string& expected_str) {
   auto code = assembler::ircode_from_string(code_str);
   auto expected = assembler::ircode_from_string(expected_str);
 
-  ThrowPropagationPass::Config config;
-  auto no_return_methods =
-      ThrowPropagationPass::get_no_return_methods(config, scope);
+  auto no_return_methods = get_no_return_methods(scope);
   auto override_graph = method_override_graph::build_graph(scope);
   code->build_cfg();
-  ThrowPropagationPass::run(
-      config, no_return_methods, *override_graph, code.get());
+  run_throw_propagation(no_return_methods, *override_graph, code.get());
   code->clear_cfg();
 
   EXPECT_CODE_EQ(code.get(), expected.get());
