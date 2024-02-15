3ed7fd1d96e51b1e426930f0eed199b73886f413
Andreas Gampe <agampe@meta.com>
2024-02-15 06:08:50 -0800

    Allow builddir != srcdir
    Summary:
    As title. Allow usage like
    ```
    mkdir build && cd build
    ../configure
    make
    ```
    
    Differential Revision: D53794041

--- bundle-redex.sh
+++ bundle-redex.sh
@@ -4,12 +4,14 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+SRC_DIR="${SRC_DIR:-.}"
+
 if [ -f "generated_apilevels.py" ] ; then
   GEN_APILEVELS_INPUT="generated_apilevels.py"
 else
   GEN_APILEVELS_INPUT=
 fi
 
-tar czf redex.tar.gz redex-all redex.py pyredex/*.py $GEN_APILEVELS_INPUT
-cat selfextract.sh redex.tar.gz > redex
+tar czf redex.tar.gz redex-all "${SRC_DIR}/redex.py" "${SRC_DIR}/pyredex/*.py" $GEN_APILEVELS_INPUT
+cat "${SRC_DIR}/selfextract.sh" redex.tar.gz > redex
 chmod +x redex
