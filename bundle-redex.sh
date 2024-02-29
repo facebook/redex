44aa6eea77b67136bc6dda62df6f72b1b2710e23
Andreas Gampe <agampe@meta.com>
2024-02-29 13:29:46 -0800

    Fix bundle-redex.sh in Makefile
    Summary:
    Local errors were silently hidden - add `set -e`.
    
    Fix the glob. Fix how the Makefile is pushing the `src_dir`.
    
    Reviewed By: beicy
    
    Differential Revision: D54360801
    
    fbshipit-source-id: 70be5df65dc47eaeca2b425f1131d226401f178d

--- bundle-redex.sh
+++ bundle-redex.sh
@@ -4,6 +4,8 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+set -e
+
 SRC_DIR="${SRC_DIR:-.}"
 
 if [ -f "generated_apilevels.py" ] ; then
@@ -12,6 +14,6 @@ else
   GEN_APILEVELS_INPUT=
 fi
 
-tar czf redex.tar.gz redex-all "${SRC_DIR}/redex.py" "${SRC_DIR}/pyredex/*.py" $GEN_APILEVELS_INPUT
+tar czf redex.tar.gz redex-all "${SRC_DIR}/redex.py" "${SRC_DIR}"/pyredex/*.py $GEN_APILEVELS_INPUT
 cat "${SRC_DIR}/selfextract.sh" redex.tar.gz > redex
 chmod +x redex
