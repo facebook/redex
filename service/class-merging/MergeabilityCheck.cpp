277b552bbb5a3aed877dddb4c461a86cc4f5c759
Wei Zhang (Devinfra) <zwei@meta.com>
2024-04-17 14:56:25 -0700

    Promo D55762070 to stable
    Summary: The change is already on staging. We need this to promo a later diff D55993834 on to stable.
    
    Reviewed By: ssj933
    
    Differential Revision: D56213344
    
    fbshipit-source-id: 20f1695132caa18df89dfdae53247277ed2164fd

--- service/class-merging/MergeabilityCheck.cpp
+++ service/class-merging/MergeabilityCheck.cpp
@@ -74,6 +74,7 @@ TypeSet MergeabilityChecker::exclude_unsupported_bytecode_refs_for(
   bool has_type_tag = m_spec.has_type_tag();
   std::vector<std::pair<IRInstruction*, const DexType*>>
       const_classes_to_verify;
+  std::unordered_set<const IRInstruction*> const_classes_to_verify_set;
   auto& cfg = code->cfg();
   for (const auto& mie : InstructionIterable(cfg)) {
     auto insn = mie.insn;
@@ -147,6 +148,7 @@ TypeSet MergeabilityChecker::exclude_unsupported_bytecode_refs_for(
       } else {
         // To verify the usages
         const_classes_to_verify.emplace_back(insn, type);
+        const_classes_to_verify_set.insert(insn);
       }
     }
   }
@@ -155,7 +157,9 @@ TypeSet MergeabilityChecker::exclude_unsupported_bytecode_refs_for(
     return non_mergeables;
   }
 
-  live_range::MoveAwareChains chains(cfg);
+  live_range::MoveAwareChains chains(
+      cfg, /* ignore_unreachable */ false,
+      [&](auto* insn) { return const_classes_to_verify_set.count(insn); });
   live_range::DefUseChains du_chains = chains.get_def_use_chains();
 
   for (const auto& pair : const_classes_to_verify) {
