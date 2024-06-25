2d5b17fe47c85d9e6f2643ffa660e0db51b14a33
William Sanville <wsanville@meta.com>
2024-06-25 14:53:51 -0700

    Make a PreVerify test case that loads resources.arsc
    Summary: Hook up the .arsc file into the buck deps, show how to read it from a test case.
    
    Reviewed By: NTillmann
    
    Differential Revision: D59015988
    
    fbshipit-source-id: 450b1ff6c23ffc8e569d4da48170149de3def7ba

--- test/instr/ResourcesInliningPassVerify.cpp
+++ test/instr/ResourcesInliningPassVerify.cpp
@@ -5,8 +5,36 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
 #include "verify/VerifyUtil.h"
+
+#include "ApkResources.h"
+
+TEST_F(PreVerify, ResourcesInliningPassTest) {
+  const auto& resource_arsc_file = resources.at("resources.arsc");
+  auto res_table = ResourcesArscFile(resource_arsc_file);
+  EXPECT_THAT(res_table.sorted_res_ids,
+              testing::ElementsAre(
+                  /* bool */
+                  0x7f010000,
+                  /* color */
+                  0x7f020000,
+                  0x7f020001,
+                  0x7f020002,
+                  0x7f020003,
+                  /* dimen */
+                  0x7f030000,
+                  0x7f030001,
+                  /* integer */
+                  0x7f040000,
+                  /* layout */
+                  0x7f050000,
+                  0x7f050001,
+                  0x7f050002,
+                  /* string */
+                  0x7f060000));
+}
 
 TEST_F(PostVerify, ResourcesInliningPassTest) {}
