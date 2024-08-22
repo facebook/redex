896f45ead99c64e972ae698b0ca7c79267fe4cd6
Andreas Gampe <agampe@meta.com>
2024-08-21 18:48:15 -0700

    Revert D61608258 "Revert D61502882: [redex][*] Promote "full-rename-map json""
    Summary:
    Original commit changeset: aa68b399f9c2
    
    Original Phabricator Diff: D61502882
    
    Reviewed By: wsanville
    
    Differential Revision: D61614119
    
    fbshipit-source-id: cd91bbeaa0e1f7e643bf5779a4bbef30d5f43dab

--- redex.py
+++ redex.py
@@ -1395,6 +1395,16 @@ def get_compression_list() -> typing.List[CompressionEntry]:
             None,
             CompressionLevel.DEFAULT,  # Bit larger.
         ),
+        CompressionEntry(
+            "Redex Full Rename Map (JSON)",
+            lambda args: not _is_preserve_input_dexes(args),
+            True,
+            ["redex-full-rename-map.json"],
+            [],
+            "redex-full-rename-map.json.zstd",
+            None,
+            CompressionLevel.DEFAULT,  # Bit larger.
+        ),
     ]
 
 
