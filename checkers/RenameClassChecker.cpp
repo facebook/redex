2513e141f1f8636e7c247ac9eae346ab693f1d9c
Nikolai Tillmann <nikolait@meta.com>
2024-10-22 09:59:37 -0700

    fix RenameClassChecker
    Summary: We recently relaxed the class ordering requirements. This updates the checker.
    
    Reviewed By: wsanville
    
    Differential Revision: D64635331
    
    fbshipit-source-id: bef1e08d51e0b029c88f091a336cd4ef71366ab7

--- checkers/RenameClassChecker.cpp
+++ checkers/RenameClassChecker.cpp
@@ -7,6 +7,7 @@
 
 #include "RenameClassChecker.h"
 
+#include "ConfigFiles.h"
 #include "Debug.h"
 #include "DexClass.h"
 #include "Show.h"
@@ -18,25 +19,69 @@ using facebook::Locator;
 namespace redex_properties {
 
 void RenameClassChecker::run_checker(DexStoresVector& stores,
-                                     ConfigFiles& /* conf */,
+                                     ConfigFiles& conf,
                                      PassManager& /* mgr */,
                                      bool established) {
   if (!established) {
     return;
   }
-  const auto& scope = build_class_scope(stores);
+
+  const JsonWrapper& json_config = conf.get_json_config();
+  bool use_locator_strings = json_config.get("emit_locator_strings", false);
+
   uint32_t sequence_nr = 0;
-  walk::classes(scope, [&](const DexClass* cls) {
-    const char* cls_name = cls->get_name()->c_str();
-    uint32_t global_cls_nr = Locator::decodeGlobalClassIndex(cls_name);
-    if (global_cls_nr != Locator::invalid_global_class_index) {
-      always_assert_log(
-          sequence_nr == global_cls_nr,
-          "[%s] invalid class number, expected %u, got %u, class %s!\n",
-          get_name(get_property()), sequence_nr, global_cls_nr, cls_name);
+  int64_t max_sequence_nr_in_prev_stores = -1;
+  for (auto& store : stores) {
+    auto& dexen = store.get_dexen();
+    int64_t max_sequence_nr_in_store = -1;
+    for (auto& dex : dexen) {
+      bool first_in_dex = true;
+      std::optional<uint32_t> last_sequence_nr_in_dex;
+      for (auto* cls : dex) {
+        const char* cls_name = cls->get_name()->c_str();
+        uint32_t global_cls_nr = Locator::decodeGlobalClassIndex(cls_name);
+        if (global_cls_nr != Locator::invalid_global_class_index) {
+          // All sequence numbers within a store must be larger then sequence
+          // numbers in previous stores.
+          if (first_in_dex) {
+            always_assert_log(
+                max_sequence_nr_in_prev_stores < (int64_t)global_cls_nr,
+                "[%s] invalid class number, expected a number larger than %u, "
+                "got %u, class %s!\n",
+                get_name(get_property()),
+                (uint32_t)max_sequence_nr_in_prev_stores, global_cls_nr,
+                cls_name);
+            first_in_dex = false;
+          }
+
+          // No matter what, sequence numbers within a dex must be increasing
+          if (last_sequence_nr_in_dex) {
+            always_assert_log(*last_sequence_nr_in_dex < global_cls_nr,
+                              "[%s] invalid class number, expected a number "
+                              "larger than %u, got %u, class %s!\n",
+                              get_name(get_property()),
+                              *last_sequence_nr_in_dex, global_cls_nr,
+                              cls_name);
+          }
+          last_sequence_nr_in_dex = global_cls_nr;
+
+          if (use_locator_strings) {
+            // If we are using locator strings, we require strict global
+            // ordering.
+            always_assert_log(
+                sequence_nr == global_cls_nr,
+                "[%s] invalid class number, expected %u, got %u, class %s!\n",
+                get_name(get_property()), sequence_nr, global_cls_nr, cls_name);
+          }
+          max_sequence_nr_in_store =
+              std::max(max_sequence_nr_in_store, (int64_t)global_cls_nr);
+        }
+        ++sequence_nr;
+      }
     }
-    ++sequence_nr;
-  });
+    max_sequence_nr_in_prev_stores =
+        std::max(max_sequence_nr_in_prev_stores, max_sequence_nr_in_store);
+  }
 }
 
 } // namespace redex_properties
