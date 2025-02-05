1d2fed36882740601422c5092b3d73d9dbf7d48f
Nikolai Tillmann <nikolait@meta.com>
2025-02-04 17:20:42 -0800

    cleanup
    Summary:
    Almost 8 years ago, D5049668 changed some test code to reflect that an experiment optimziation got disabled.
    This left behind some non-sensical comments and test code. This is getting cleaned up, and we add more checking.
    
    Reviewed By: kobywchan
    
    Differential Revision: D68907660
    
    fbshipit-source-id: 2e8a4ae2b01755321b226e9844d1db5785312d02

--- test/integ/ConstantPropagationTest.cpp
+++ test/integ/ConstantPropagationTest.cpp
@@ -24,62 +24,36 @@ This test takes as input the Dex bytecode for the class generated
 from the Java source file:
    <redex root>/test/integ/ConstantPropagation.java
 which is specified in Buck tests via an environment variable in the
-BUCK file. Before optimization, the code for the propagate method is:
+BUCK file.
 
-dmethod: if_false
-dmethod: regs: 4, ins: 0, outs: 2
-const/4 v1
-if-eqz v1
-new-instance Lcom/facebook/redextest/MyBy2Or3; v0
-const/4 v3
-invoke-direct public com.facebook.redextest.MyBy2Or3.<init>(I)V v0, v3
-invoke-virtual public com.facebook.redextest.MyBy2Or3.Double()I v0
-move-result v2
-return v2
-const/16 v2
-goto
+Before optimization, if_false, if_true and if_unknown contain conditional
+branches. After the optimization, only if_unknown keeps its condiitonal branch.
 
-dmethod: if_true
-dmethod: regs: 2, ins: 0, outs: 0
-const/4 v0
-if-eqz v0
-const/16 v1
-return v1
-invoke-static public static com.facebook.redextest.Alpha.theAnswer()I
-move-result v1
-goto
-
-
-After optimization with ConstantPropagationPass, the code should be:
-
-dmethod: if_false
-dmethod: regs: 2, ins: 0, outs: 0
-const/16 v1
-return v1
-
-dmethod: if_true
-dmethod: regs: 2, ins: 0, outs: 0
-const/16 v1
-return v1
-
-This test mainly checks whether the constant propagation is fired. It does
-this by checking to make sure there are no OPCODE_IF_EQZ and OPCODE_NEW_INSTANCE
-instructions in the optimized method.
+This test mainly checks whether the constant propagation is fired.
 */
 
 // The ClassType enum is used to classify and filter classes in test result
 enum ClassType {
   MAINCLASS = 0,
-  REMOVEDCLASS = 1,
   OTHERCLASS = 2,
 };
 
 ClassType filter_test_classes(const DexString* cls_name) {
   if (strcmp(cls_name->c_str(), "Lcom/facebook/redextest/Propagation;") == 0)
     return MAINCLASS;
-  if (strcmp(cls_name->c_str(), "Lcom/facebook/redextest/Alpha;") == 0)
-    return REMOVEDCLASS;
   return OTHERCLASS;
+}
+
+bool has_conditional_branch(DexMethod* method) {
+  const auto& insns = InstructionIterable(method->get_code());
+  TRACE(CONSTP, 1, "%s", SHOW(insns));
+  for (auto& mie : insns) {
+    IROpcode op = mie.insn->opcode();
+    if (opcode::is_a_conditional_branch(op)) {
+      return true;
+    }
+  }
+  return false;
 }
 
 class ConstantPropagationTest : public RedexIntegrationTest {};
@@ -87,10 +61,11 @@ class ConstantPropagationTest : public RedexIntegrationTest {};
 TEST_F(ConstantPropagationTest, constantPropagation) {
   std::cout << "Loaded classes: " << classes->size() << std::endl;
 
+  size_t before_methods = 0;
   TRACE(CONSTP, 1, "Code before:");
   for (const auto& cls : *classes) {
     TRACE(CONSTP, 1, "Class %s", SHOW(cls));
-    if (filter_test_classes(cls->get_name()) < 2) {
+    if (filter_test_classes(cls->get_name()) == MAINCLASS) {
       TRACE(CONSTP, 1, "Class %s", SHOW(cls));
       for (const auto& dm : cls->get_dmethods()) {
         TRACE(CONSTP, 1, "dmethod: %s", dm->get_name()->c_str());
@@ -98,16 +73,19 @@ TEST_F(ConstantPropagationTest, constantPropagation) {
             strcmp(dm->get_name()->c_str(), "if_true") == 0 ||
             strcmp(dm->get_name()->c_str(), "if_unknown") == 0) {
           TRACE(CONSTP, 1, "%s", SHOW(InstructionIterable(dm->get_code())));
+          before_methods++;
         }
       }
     }
   }
+  EXPECT_EQ(before_methods, 3);
 
   Pass* constp = new ConstantPropagationPass();
   std::vector<Pass*> passes = {constp};
 
   run_passes(passes);
 
+  size_t after_methods = 0;
   TRACE(CONSTP, 1, "Code after:");
   for (const auto& cls : *classes) {
     TRACE(CONSTP, 1, "Class %s", SHOW(cls));
@@ -115,32 +93,17 @@ TEST_F(ConstantPropagationTest, constantPropagation) {
       for (const auto& dm : cls->get_dmethods()) {
         TRACE(CONSTP, 1, "dmethod: %s", dm->get_name()->c_str());
         if (strcmp(dm->get_name()->c_str(), "if_false") == 0) {
-          const auto& insns = InstructionIterable(dm->get_code());
-          TRACE(CONSTP, 1, "%s", SHOW(insns));
-          for (auto& mie : insns) {
-            IROpcode op = mie.insn->opcode();
-            EXPECT_NE(op, OPCODE_IF_EQZ);
-          }
+          EXPECT_FALSE(has_conditional_branch(dm));
+          after_methods++;
         } else if (strcmp(dm->get_name()->c_str(), "if_true") == 0) {
-          const auto& insns = InstructionIterable(dm->get_code());
-          TRACE(CONSTP, 1, "%s", SHOW(insns));
-          for (auto& mie : insns) {
-            IROpcode op = mie.insn->opcode();
-            EXPECT_NE(op, OPCODE_IF_EQZ);
-          }
+          EXPECT_FALSE(has_conditional_branch(dm));
+          after_methods++;
         } else if (strcmp(dm->get_name()->c_str(), "if_unknown") == 0) {
-          const auto& insns = InstructionIterable(dm->get_code());
-          TRACE(CONSTP, 1, "%s", SHOW(insns));
-          bool has_if = false;
-          for (auto& mie : insns) {
-            IROpcode op = mie.insn->opcode();
-            if (opcode::is_a_conditional_branch(op)) {
-              has_if = true;
-            }
-          }
-          EXPECT_TRUE(has_if);
+          EXPECT_TRUE(has_conditional_branch(dm));
+          after_methods++;
         }
       }
     }
   }
+  EXPECT_EQ(after_methods, 3);
 }
