45792a9e019813e5997e270da75628e78a56e136
Sushan Jiang <suree404@meta.com>
2025-02-14 16:20:15 -0800

    log the method code in type def checker assert
    Summary: Not easy to debug in default case
    
    Reviewed By: thezhangwei
    
    Differential Revision: D69631407
    
    fbshipit-source-id: 5fc77c0d75168c46e23dfb2de2c246775b532e4c

--- opt/typedef-anno-checker/TypedefAnnoCheckerPass.cpp
+++ opt/typedef-anno-checker/TypedefAnnoCheckerPass.cpp
@@ -679,7 +679,8 @@ bool TypedefAnnoChecker::check_typedef_value(
     }
     default: {
       std::ostringstream out;
-      out << "TypedefAnnoCheckerPass: the method " << show(m)
+      out << "TypedefAnnoCheckerPass: the method " << show(m) << "\n"
+          << show(m->get_code()->cfg(), true) << "\n"
           << "\n does not guarantee value safety for the value with typedef "
              "annotation "
           << show(annotation)
