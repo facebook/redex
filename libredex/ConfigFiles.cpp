5bf5bd7f3c20ed6994b18dfa57574202b5b2c5ae
Koby Chan <koby@meta.com>
2025-04-25 08:43:18 -0700

    Fix filename bug for writing baseline profiles classes
    Summary: I messed up earlier and made some refactoring without fixing how we copy classes. This led to baseline profiles that differed from the old infra. Fixing it in this diff and then rerunning oxygen tests next week :(
    
    Reviewed By: jimmycFB
    
    Differential Revision: D73600501
    
    fbshipit-source-id: 0fc6e226eaa0db7e17558d72a1c52f4dab8d5029

--- libredex/ConfigFiles.cpp
+++ libredex/ConfigFiles.cpp
@@ -324,7 +324,7 @@ std::vector<std::string> ConfigFiles::load_coldstart_methods() {
 std::string ConfigFiles::get_preprocessed_baseline_profile_file(
     const std::string& config_name) {
   return m_preprocessed_baseline_profile_directory + "/" + config_name +
-         "/baseline-profile.txt";
+         "-baseline-profile.txt";
 }
 
 /**
