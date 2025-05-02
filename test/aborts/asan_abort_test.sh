979381c4eba4fea3ed5e52e28249b3b141ee9a6d
Andreas Gampe <agampe@meta.com>
2025-05-02 08:42:05 -0700

    Output temp file on failure
    Summary: As title.
    
    Reviewed By: wsanville
    
    Differential Revision: D73959881
    
    fbshipit-source-id: 5bc13caabc911ef6905542fa6a9d755fe456e078

--- test/aborts/asan_abort_test.sh
+++ test/aborts/asan_abort_test.sh
@@ -16,6 +16,6 @@ OUT_FILE="$(dirname "$SCRIPT")/test.out"
 "$REDEX_BINARY" --asan-abort test.dex >"$OUT_FILE" 2>&1 && ( echo "Redex did not fail" ; exit 1 ; )
 
 # Check for asan message.
-grep 'ERROR: AddressSanitizer: stack-use-after-scope' "$OUT_FILE" || ( echo "Did not find ASAN header" ; exit 1 ; )
-grep 'WRITE of size 4 ' "$OUT_FILE" || ( echo "Did not find write message" ; exit 1 ; )
-grep '==ABORTING' "$OUT_FILE" || ( echo "Did not find abort message" ; exit 1 ; )
+grep 'ERROR: AddressSanitizer: stack-use-after-scope' "$OUT_FILE" || ( echo "Did not find ASAN header" ; cat "$OUT_FILE" ; exit 1 ; )
+grep 'WRITE of size 4 ' "$OUT_FILE" || ( echo "Did not find write message" ; cat "$OUT_FILE" ; exit 1 ; )
+grep '==ABORTING' "$OUT_FILE" || ( echo "Did not find abort message" ; cat "$OUT_FILE" ; exit 1 ; )
