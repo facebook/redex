40284facfd2a8ceca3abaec9320621e50cf46f7e
Nikolai Tillmann <nikolait@meta.com>
2025-05-05 17:18:50 -0700

    metric for huge methods
    Reviewed By: ssj933
    
    Differential Revision: D74151063
    
    fbshipit-source-id: 8e3cbcfbc8ceda8d6e3896da394c5c7ce5c2bce2

--- libredex/DexAssessments.h
+++ libredex/DexAssessments.h
@@ -21,6 +21,10 @@
 
 namespace assessments {
 
+// Why 9000? Because that's the default cut-off for SplitHugeSwitchPass to
+// start splitting.
+constexpr const uint32_t HUGE_METHOD_THRESHOLD = 9000;
+
 using DexAssessment = UnorderedMap<std::string, uint64_t>;
 using DexAssessmentItem = std::pair<std::string, uint64_t>;
 
