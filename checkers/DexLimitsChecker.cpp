ef769eaed7bdd1d29b114a520541e05c17f998e4
Andreas Gampe <agampe@meta.com>
2025-05-09 14:57:09 -0700

    Reduce storage overhead in DexLimitChecker
    Summary: Sets have high per-item overhead. Only convert to set when an issue was found.
    
    Reviewed By: ssj933
    
    Differential Revision: D74346278
    
    fbshipit-source-id: f86e451a094706e6fe471e27c1d86f57c71a9a78

--- checkers/DexLimitsChecker.cpp
+++ checkers/DexLimitsChecker.cpp
@@ -29,13 +29,25 @@ using dex_data_map_t =
     UnorderedMap<std::string, std::vector<DexLimitsChecker::DexData>>;
 
 template <typename T>
-UnorderedSet<T> extract(const UnorderedMap<T, size_t>& input) {
-  // No insert iterator for UnorderedSet, have to go through vector.
+std::vector<T> extract(const UnorderedMap<T, size_t>& input) {
   std::vector<T> tmp{};
   tmp.reserve(input.size());
-  unordered_transform(
-      input, std::back_inserter(tmp), [](auto& p) { return p.first; });
-  return UnorderedSet<T>(tmp.begin(), tmp.end());
+  unordered_transform(input, std::back_inserter(tmp),
+                      [](auto& p) { return p.first; });
+  return tmp;
+}
+
+// Avoid sorting overhead, but still store in vector.
+template <class Collection,
+          class Value =
+              typename std::remove_const<typename Collection::value_type>::type>
+std::vector<Value> unordered_to_vector(Collection& collection) {
+  std::vector<Value> result;
+  result.reserve(collection.size());
+  for (auto& entry : UnorderedIterable(collection)) {
+    result.emplace_back(entry);
+  }
+  return result;
 }
 
 dex_data_map_t create_data(
@@ -77,10 +89,55 @@ struct IssueIndex {
   bool type_overflow{false};
 };
 
-std::string print_new_entries(const dex_data_map_t& old_map,
-                              const dex_data_map_t& new_map,
+template <typename T>
+UnorderedSet<T> to_set(const std::vector<T>& in) {
+  return UnorderedSet<T>{in.begin(), in.end()};
+}
+
+std::string print_new_entries(const dex_data_map_t& old_map_vec,
+                              const dex_data_map_t& new_map_vec,
                               const std::vector<IssueIndex>& issues) {
   std::ostringstream oss;
+
+  struct DexDataWithSet {
+    // These should be sets, but for overhead reasons we keep these as vectors
+    // and only translate them when we find an issue.
+    UnorderedSet<DexFieldRef*> fields;
+    UnorderedSet<DexMethodRef*> methods;
+    UnorderedSet<DexType*> types;
+    // Share memory. Lifetimes are OK because confined to this function.
+    const UnorderedSet<DexType*>& pending_init_class_fields;
+    const UnorderedSet<DexType*>& pending_init_class_types;
+
+    DexDataWithSet(UnorderedSet<DexFieldRef*> f,
+                   UnorderedSet<DexMethodRef*> m,
+                   UnorderedSet<DexType*> t,
+                   const UnorderedSet<DexType*>& p_f,
+                   const UnorderedSet<DexType*>& p_t)
+        : fields(std::move(f)),
+          methods(std::move(m)),
+          types(std::move(t)),
+          pending_init_class_fields(p_f),
+          pending_init_class_types(p_t) {}
+  };
+  auto to_set_type = [](const auto& in) {
+    UnorderedMap<std::string, std::vector<DexDataWithSet>> ret;
+    for (const auto& p : UnorderedIterable(in)) {
+      std::vector<DexDataWithSet> tmp_vec;
+      tmp_vec.reserve(p.second.size());
+      for (const auto& vec_elem : p.second) {
+        tmp_vec.emplace_back(to_set(vec_elem.fields),
+                             to_set(vec_elem.methods),
+                             to_set(vec_elem.types),
+                             vec_elem.pending_init_class_fields,
+                             vec_elem.pending_init_class_types);
+      }
+      ret.emplace(p.first, std::move(tmp_vec));
+    }
+    return ret;
+  };
+  auto old_map = to_set_type(old_map_vec);
+  auto new_map = to_set_type(new_map_vec);
 
   for (auto& i : issues) {
     auto& store_name = i.store->get_name();
@@ -101,31 +158,31 @@ std::string print_new_entries(const dex_data_map_t& old_map,
     }
 
     auto new_st_it = new_map.find(store_name);
-    redex_assert(new_st_it != new_map.end());
+    redex_assert(new_st_it != new_map.cend());
 
     auto& new_dexes = new_st_it->second;
     redex_assert(new_dexes.size() > i.dex_id);
 
-    auto print_differences =
-        [&](const auto& old_data, const auto& new_data, const char* prefix) {
-          // Won't be sorted, but sorting would be a template pain.
-          bool have_changes = false;
-          for (auto* entry : UnorderedIterable(new_data)) {
-            if (old_data.count(entry) != 0) {
-              continue;
-            }
-            if (!have_changes) {
-              have_changes = true;
-              oss << prefix << show(entry);
-            } else {
-              oss << ", " << show(entry);
-            }
-          }
-          if (have_changes) {
-            oss << "\n";
-          }
-          return have_changes;
-        };
+    auto print_differences = [&](const auto& old_data, const auto& new_data,
+                                 const char* prefix) {
+      // Won't be sorted, but sorting would be a template pain.
+      bool have_changes = false;
+      for (auto* entry : UnorderedIterable(new_data)) {
+        if (old_data.count(entry) != 0) {
+          continue;
+        }
+        if (!have_changes) {
+          have_changes = true;
+          oss << prefix << show(entry);
+        } else {
+          oss << ", " << show(entry);
+        }
+      }
+      if (have_changes) {
+        oss << "\n";
+      }
+      return have_changes;
+    };
 
     bool had_fields{false};
     if (i.field_overflow) {
@@ -168,8 +225,8 @@ std::string print_new_entries(const dex_data_map_t& old_map,
     if (!had_fields && !had_methods && !had_types) {
       // Run the other things, maybe there's a misdetection.
       if (!i.field_overflow) {
-        print_differences(
-            old_dexes[i.dex_id].fields, new_dexes[i.dex_id].fields, "Fields: ");
+        print_differences(old_dexes[i.dex_id].fields,
+                          new_dexes[i.dex_id].fields, "Fields: ");
         print_differences(old_dexes[i.dex_id].pending_init_class_fields,
                           new_dexes[i.dex_id].pending_init_class_fields,
                           "Pending init-class Fields For: ");
@@ -180,8 +237,8 @@ std::string print_new_entries(const dex_data_map_t& old_map,
                           "Methods: ");
       }
       if (!i.type_overflow) {
-        print_differences(
-            old_dexes[i.dex_id].types, new_dexes[i.dex_id].types, "Types: ");
+        print_differences(old_dexes[i.dex_id].types, new_dexes[i.dex_id].types,
+                          "Types: ");
         print_differences(old_dexes[i.dex_id].pending_init_class_types,
                           new_dexes[i.dex_id].pending_init_class_types,
                           "Pending init-class Types: ");
@@ -218,10 +275,10 @@ void DexLimitsChecker::run_checker(DexStoresVector& stores,
             scope, conf.create_init_class_insns());
   }
 
-  auto check_ref_num = [&init_classes_with_side_effects, &pass_name, &result](
-                           const DexClasses& classes,
-                           const DexStore& store,
-                           size_t dex_id) -> std::optional<IssueIndex> {
+  auto check_ref_num = [&init_classes_with_side_effects, &pass_name,
+                        &result](const DexClasses& classes,
+                                 const DexStore& store,
+                                 size_t dex_id) -> std::optional<IssueIndex> {
     DexLimitsInfo dex_limits(init_classes_with_side_effects.get());
     bool field_overflow{false};
     bool method_overflow{false};
