5c1bfb9ea4fcac3530364efead0a7545afdd1b1d
William Sanville <wsanville@meta.com>
2025-05-16 15:06:58 -0700

    ApkResources test case for fully qualifying elements
    Summary: Refactor the tmp dir setup functionality from the existing test case, add a new test case that asserts that ".MainActivity" here https://fburl.com/code/8kpq54e8 is resolved to an actual valid class name (which was *not* happening before).
    
    Reviewed By: agampe
    
    Differential Revision: D74899782
    
    fbshipit-source-id: 771b855dfdff401d4a497fcd4cdcb7d8b0fe51a0

--- test/unit/resources/ManifestClassesTest.cpp
+++ test/unit/resources/ManifestClassesTest.cpp
@@ -14,10 +14,12 @@
 
 #include <boost/filesystem.hpp>
 
-TEST(ManifestClassesTest, exported) {
-  // Setup a normal looking apk directory
+// Setup a "normal" looking unpacked apk directory; creates a reader for the
+// callback.
+void run_with_manifest(const std::string& env_var,
+                       const std::function<void(ApkResources*)>& callback) {
   auto tmp_dir = redex::make_tmp_dir("ManifestClassesTest%%%%%%%%");
-  std::ifstream src_stream(std::getenv("test_manifest_path"), std::ios::binary);
+  std::ifstream src_stream(std::getenv(env_var.c_str()), std::ios::binary);
   auto dest = tmp_dir.path + "/AndroidManifest.xml";
   {
     std::ofstream dest_stream(dest, std::ios::binary);
@@ -25,35 +27,56 @@ TEST(ManifestClassesTest, exported) {
   }
 
   ApkResources resources(tmp_dir.path);
-  const auto& class_info = resources.get_manifest_class_info();
+  callback(&resources);
+}
 
-  const auto& tag_infos = class_info.component_tags;
-  EXPECT_EQ(tag_infos.size(), 5);
+TEST(ManifestClassesTest, exported) {
+  run_with_manifest("test_manifest_path", [](ApkResources* resources) {
+    const auto& class_info = resources->get_manifest_class_info();
 
-  EXPECT_EQ(tag_infos[0].tag, ComponentTag::Activity);
-  EXPECT_EQ(tag_infos[0].classname, "Ltest1;");
-  EXPECT_EQ(tag_infos[0].is_exported, BooleanXMLAttribute::True);
-  EXPECT_FALSE(tag_infos[0].has_intent_filters);
+    const auto& tag_infos = class_info.component_tags;
+    EXPECT_EQ(tag_infos.size(), 5);
 
-  EXPECT_EQ(tag_infos[1].tag, ComponentTag::Activity);
-  EXPECT_EQ(tag_infos[1].classname, "Ltest2;");
-  EXPECT_EQ(tag_infos[1].is_exported, BooleanXMLAttribute::Undefined);
-  EXPECT_FALSE(tag_infos[1].has_intent_filters);
+    EXPECT_EQ(tag_infos[0].tag, ComponentTag::Activity);
+    EXPECT_EQ(tag_infos[0].classname, "Ltest1;");
+    EXPECT_EQ(tag_infos[0].is_exported, BooleanXMLAttribute::True);
+    EXPECT_FALSE(tag_infos[0].has_intent_filters);
 
-  EXPECT_EQ(tag_infos[2].tag, ComponentTag::Activity);
-  EXPECT_EQ(tag_infos[2].classname, "Ltest3;");
-  EXPECT_EQ(tag_infos[2].is_exported, BooleanXMLAttribute::Undefined);
-  EXPECT_TRUE(tag_infos[2].has_intent_filters);
+    EXPECT_EQ(tag_infos[1].tag, ComponentTag::Activity);
+    EXPECT_EQ(tag_infos[1].classname, "Ltest2;");
+    EXPECT_EQ(tag_infos[1].is_exported, BooleanXMLAttribute::Undefined);
+    EXPECT_FALSE(tag_infos[1].has_intent_filters);
 
-  EXPECT_EQ(tag_infos[3].tag, ComponentTag::Activity);
-  EXPECT_EQ(tag_infos[3].classname, "Ltest4;");
-  EXPECT_EQ(tag_infos[3].is_exported, BooleanXMLAttribute::False);
-  EXPECT_FALSE(tag_infos[3].has_intent_filters);
+    EXPECT_EQ(tag_infos[2].tag, ComponentTag::Activity);
+    EXPECT_EQ(tag_infos[2].classname, "Ltest3;");
+    EXPECT_EQ(tag_infos[2].is_exported, BooleanXMLAttribute::Undefined);
+    EXPECT_TRUE(tag_infos[2].has_intent_filters);
 
-  EXPECT_EQ(tag_infos[4].tag, ComponentTag::Provider);
-  EXPECT_EQ(tag_infos[4].classname, "Lcom/example/x/Foo;");
-  EXPECT_EQ(tag_infos[4].is_exported, BooleanXMLAttribute::Undefined);
-  EXPECT_THAT(unordered_unsafe_unwrap(tag_infos[4].authority_classes),
-              ::testing::UnorderedElementsAre("Lcom/example/x/Foo;",
-                                              "Lcom/example/y/Bar;"));
+    EXPECT_EQ(tag_infos[3].tag, ComponentTag::Activity);
+    EXPECT_EQ(tag_infos[3].classname, "Ltest4;");
+    EXPECT_EQ(tag_infos[3].is_exported, BooleanXMLAttribute::False);
+    EXPECT_FALSE(tag_infos[3].has_intent_filters);
+
+    EXPECT_EQ(tag_infos[4].tag, ComponentTag::Provider);
+    EXPECT_EQ(tag_infos[4].classname, "Lcom/example/x/Foo;");
+    EXPECT_EQ(tag_infos[4].is_exported, BooleanXMLAttribute::Undefined);
+    EXPECT_THAT(unordered_unsafe_unwrap(tag_infos[4].authority_classes),
+                ::testing::UnorderedElementsAre("Lcom/example/x/Foo;",
+                                                "Lcom/example/y/Bar;"));
+  });
+}
+
+TEST(ManifestClassesTest, fully_qualify_class_names) {
+  // This manifest file has attribute values like ".ActivityB" which need to be
+  // turned into real class names.
+  run_with_manifest("test_manifest_exported_path", [](ApkResources* resources) {
+    const auto& class_info = resources->get_manifest_class_info();
+    std::set<std::string> class_names;
+    for (auto& component : class_info.component_tags) {
+      class_names.emplace(component.classname);
+    }
+    EXPECT_THAT(class_names,
+                ::testing::ElementsAre("Lcom/fb/exportedtest/ActivityB;",
+                                       "Lcom/fb/exportedtest/MainActivity;"));
+  });
 }
