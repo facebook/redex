ab7f6cbb5ff61c72d1e0770fb6450bd05a5b6dc6
Hong Xu <hongx@meta.com>
2025-06-02 13:42:08 -0700

    Add sanity checks for kotlin_default_args integration test
    Summary: With some additional cleanup.
    
    Reviewed By: thezhangwei
    
    Differential Revision: D75715516
    
    fbshipit-source-id: 65f08ff4e48fd7bd0cf608e79c6ad3a7ed01164a

--- test/integ/KotlinDefaultArgsTest.cpp
+++ test/integ/KotlinDefaultArgsTest.cpp
@@ -5,6 +5,8 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+#include <string_view>
+
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
@@ -14,42 +16,73 @@
 #include "Resolver.h"
 #include "Show.h"
 
-using ::testing::HasSubstr;
-using ::testing::Not;
-
-class KotlinDefaultArgsTest : public RedexIntegrationTest {
- protected:
-  void set_root_method(const std::string& full_name) {
-    auto method = DexMethod::get_method(full_name)->as_def();
-    ASSERT_NE(nullptr, method);
-    method->rstate.set_root();
-  }
-};
 namespace {
 
-TEST_F(KotlinDefaultArgsTest, GreetDoesNotHaveBlock) {
-  auto scope = build_class_scope(stores);
+using ::testing::HasSubstr;
+using ::testing::Not;
+using ::testing::NotNull;
 
-  set_root_method("LKotlinDefaultArgs;.main:()V");
-  const auto main_method =
-      DexMethod::get_method("LKotlinDefaultArgs;.main:()V")->as_def();
-  auto code_main = main_method->get_code();
-  ASSERT_NE(nullptr, code_main);
+class KotlinDefaultArgsTest : public RedexIntegrationTest {
+ private:
+  void set_root_method(std::string_view full_name) {
+    auto method = DexMethod::get_method(full_name)->as_def();
+    ASSERT_THAT(method, NotNull()) << "method " << full_name << " not found";
+    method->rstate.set_root();
+  }
 
-  const auto default_arg_method = DexMethod::get_method(
-      "LKotlinDefaultArgs;.greet:(Ljava/lang/String;Ljava/lang/String;)V");
+ protected:
+  void SetUp() override {
+    static constexpr std::string_view main_method_sig =
+        "LKotlinDefaultArgs;.main:()V";
+    RedexIntegrationTest::SetUp();
+
+    set_root_method(main_method_sig);
+    const auto main_method = DexMethod::get_method(main_method_sig)->as_def();
+    auto code_main = main_method->get_code();
+    ASSERT_THAT(code_main, NotNull()) << "main method not found";
+  }
+
+  static constexpr std::string_view greet_method_signature =
+      "LKotlinDefaultArgs;.greet:(Ljava/lang/String;Ljava/lang/String;)V";
+  static constexpr std::string_view greet_default_method_signature =
+      "LKotlinDefaultArgs;.greet$default:(LKotlinDefaultArgs;Ljava/lang/"
+      "String;Ljava/lang/String;ILjava/lang/Object;)V";
+};
+
+TEST_F(KotlinDefaultArgsTest, UnoptimizedGreetHasHelloAnd) {
+  // Sanity check on unoptimized code.
+
+  const auto default_arg_method = DexMethod::get_method(greet_method_signature);
   auto code_default_arg = default_arg_method->as_def()->get_code();
-  ASSERT_NE(nullptr, code_default_arg);
+  ASSERT_THAT(code_default_arg, NotNull());
+
+  const auto syn_default_arg_method =
+      DexMethod::get_method(greet_default_method_signature);
+  auto code_syn_default_arg = syn_default_arg_method->as_def()->get_code();
+  ASSERT_THAT(code_syn_default_arg, NotNull())
+      << "Synthetic default method not found";
+  EXPECT_THAT(assembler::to_string(code_syn_default_arg), HasSubstr("Hello"))
+      << "\"Hello\" is the default value of the second arg \"greeting\", but "
+         "is missing before optimization";
+  EXPECT_THAT(assembler::to_string(code_syn_default_arg), HasSubstr("and-"))
+      << "The synthetic default method does not contain \"and-*\" instructions "
+         "before optimization";
+}
+
+TEST_F(KotlinDefaultArgsTest, OptimizedGreetDoesNotHaveHelloAnd) {
+  const auto default_arg_method = DexMethod::get_method(greet_method_signature);
+  auto code_default_arg = default_arg_method->as_def()->get_code();
+  ASSERT_THAT(code_default_arg, NotNull());
 
   Pass* constp = new constant_propagation::interprocedural::PassImpl();
   std::vector<Pass*> passes = {constp};
   run_passes(passes);
 
-  const auto syn_default_arg_method = DexMethod::get_method(
-      "LKotlinDefaultArgs;.greet$default:(LKotlinDefaultArgs;Ljava/lang/"
-      "String;Ljava/lang/String;ILjava/lang/Object;)V");
+  const auto syn_default_arg_method =
+      DexMethod::get_method(greet_default_method_signature);
   auto code_syn_default_arg = syn_default_arg_method->as_def()->get_code();
-  ASSERT_NE(nullptr, code_syn_default_arg);
+  ASSERT_THAT(code_syn_default_arg, NotNull())
+      << "Synthetic default method not found";
   EXPECT_THAT(assembler::to_string(code_syn_default_arg), HasSubstr("Guest"))
       << "Default arg \"name\" is used, but the synthetic default method has "
          "dropped its default value \"Guest\"";
