bf2a012368519ba5fb1d4aa2c43e80802231c1f5
William Sanville <wsanville@meta.com>
2025-06-18 13:21:30 -0700

    Promote "Populate env with java.lang.String for const-string opcodes during enum upcast analysis"
    Summary: Promote {bb86a08ce1}.
    
    Reviewed By: thezhangwei
    
    Differential Revision: D76874607
    
    fbshipit-source-id: 9d27f0d8c0743aaaf93fa0af9a026cacf4070a8f

---
+++ test/instr/UseKtEnumVerify.cpp
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include "VerifyUtil.h"
+#include <gtest/gtest.h>
+
+TEST_F(PostVerify, EnumShouldStillExist) {
+  auto enum_name = "Lredex/D;";
+  auto d = find_class_named(classes, enum_name);
+  EXPECT_NE(d, nullptr) << enum_name << " should still exist!";
+  // OptimizeEnumsPass seems to clear the flag on the class, and hangs various
+  // helper methods on it. Verify that it looks untouched.
+  EXPECT_TRUE(is_enum(d)) << enum_name << " should not have been optimized!";
+}
