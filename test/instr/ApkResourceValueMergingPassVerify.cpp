0d247eb084de54f2ff80f472d7bcae7eef1078b9
Aayush Kapoor <aayushkapoor@meta.com>
2025-07-09 18:35:48 -0700

    Fix variable naming in resource value merging pass apk instr test
    Summary: Fix variable naming
    
    Reviewed By: wsanville
    
    Differential Revision: D78042342
    
    fbshipit-source-id: e0f0ceb97f617de18b3bbb589cc4ab9fc97d6260

--- test/instr/ApkResourceValueMergingPassVerify.cpp
+++ test/instr/ApkResourceValueMergingPassVerify.cpp
@@ -17,16 +17,16 @@ TEST_F(PreVerify, ApkResourceValueMergingPassTest) {
   auto tmp_dir =
       redex::make_tmp_dir("ApkResourceValueMergingPassVerify%%%%%%%%");
   boost::filesystem::path tmp_path(tmp_dir.path);
-  auto local_resources_pb = tmp_path / "resources.arsc";
+  auto local_resources_apk = tmp_path / "resources.arsc";
   auto local_andriod_xml = tmp_path / "AndroidManifest.xml";
 
   const auto& source_resource_arsc = resources.at("resources.arsc");
   const auto& source_andriod_xml = resources.at("AndroidManifest.xml");
 
-  redex::copy_file(source_resource_arsc, local_resources_pb.string());
+  redex::copy_file(source_resource_arsc, local_resources_apk.string());
   redex::copy_file(source_andriod_xml, local_andriod_xml.string());
 
-  auto res_table = ResourcesArscFile(local_resources_pb);
+  auto res_table = ResourcesArscFile(local_resources_apk);
 
   StyleAnalysis style_analysis = create_style_analysis(tmp_path, classes);
   resource_value_merging_PreVerify(&res_table, &style_analysis);
