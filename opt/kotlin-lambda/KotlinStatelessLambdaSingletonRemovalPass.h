c41d15387fc8b78ee106d18d6fcbf0f636014c52
Wei Zhang (Devinfra) <zwei@meta.com>
2025-08-05 18:41:40 -0700

    Add KotlinStatelessLambdaSingletonRemovalPass
    Summary:
    Javac no longer generates anonymous classes or "inner classes" to desugar lambdas. It defers the generation of these classes until runtime via LambdaMetafactory. Based on the [JDK doc](https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/lang/invoke/LambdaMetafactory.html), the identity of a produced function object at runtime by LambdaMetafactory is "unpredictable".
    
    Kotlinc, since version 2.0 by default also stopped generating anonymous classes for lambdas. Similar to javac, it relies on invoke-dynamic and LambdaMetafactory to generate the classes at runtime on JVM. However, this feature is not supported by Android runtime. To execute the same code on Android, D8 still needs to desugar lambdas back to an anonymous class.
    
    Whiles D8 desugars stateless lambdas or "non-capturing lambdas", it does not scaldfold the singleton pattern anymore like it used to do (https://issuetracker.google.com/u/2/issues/222081665).
    The rationale is that the singleton pattern was inherited from the old javac behavior which is optimized for JVM server workload aiming for high throughput rather than low latency. On Android devices, peak performance throughput is less relevant than initial startup latency. Therefore, the singleton pattern is not no longer desirable.
    
    Reviewed By: ssj933
    
    Differential Revision: D79295199
    
    fbshipit-source-id: 9afa2b847e3e53669259400572b3b023608bd210

---
+++ opt/kotlin-lambda/KotlinStatelessLambdaSingletonRemovalPass.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include "Pass.h"
+
+class KotlinStatelessLambdaSingletonRemovalPass : public Pass {
+
+ public:
+  KotlinStatelessLambdaSingletonRemovalPass()
+      : Pass("KotlinStatelessLambdaSingletonRemovalPass") {}
+
+  redex_properties::PropertyInteractions get_property_interactions()
+      const override {
+    using namespace redex_properties::interactions;
+    using namespace redex_properties::names;
+    return {
+        {NoResolvablePureRefs, Preserves},
+    };
+  }
+
+  std::string get_config_doc() override {
+    return trim(R"(
+Javac no longer generates anonymous classes or "inner classes" to desugar lambdas. It defers the generation of these classes until runtime via LambdaMetafactory.
+Based on the [JDK doc](https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/lang/invoke/LambdaMetafactory.html), the identity of a produced function object at runtime by LambdaMetafactory is "unpredictable".
+
+Kotlinc, since version 2.0 by default also stopped generating anonymous classes for lambdas. Similar to javac, it relies on invoke-dynamic and LambdaMetafactory to generate the classes at runtime on JVM.
+However, this feature is not supported by Android runtime. To execute the same code on Android, D8 still needs to desugar lambdas back to an anonymous class.
+Whiles D8 desugars stateless lambdas or "non-capturing lambdas", it does not scaldfold the singleton pattern anymore like it used to do (https://issuetracker.google.com/u/2/issues/222081665).
+The rationale is that the singleton pattern was inherited from the old javac behavior which is optimized for JVM server workload aiming for high throughput rather than low latency.
+On Android devices, peak performance throughput is less relevant than initial startup latency. Therefore, the singleton pattern is not no longer desirable.
+
+Internally, our current setup still relies on the old kotlinc option (`-Xlambdas=anon`) when it comes to lambda generation. We still rely on kotlinc to generate the anonymous classes for lambdas.
+When doing that, kotlinc automatically generates a singleton pattern for stateless lambdas. In this context, we should proactively remove the singleton pattern to enjoy the benefits that come with it.
+
+This godbolt [example](https://godbolt.org/z/Mznrzs8T4) shows the singleton pattern produced by our current kotlinc setup. This pass removes the singleton pattern shown in the example.
+    )");
+  }
+
+  void run_pass(DexStoresVector&, ConfigFiles&, PassManager&) override;
+};
