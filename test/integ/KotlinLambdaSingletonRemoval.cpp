37fa9f1116bb713833e41749498b672b9432b6dc
Hong Xu <hongx@meta.com>
2025-08-15 16:43:42 -0700

    Completely test the generated instructions for creating an anonymous class in `kotlin_lambda_singleton_removal` test
    Summary: Ensure that `sget` is replaced by the expected sequence of instructions.
    
    Reviewed By: thezhangwei
    
    Differential Revision: D80368091
    
    fbshipit-source-id: d02a6a629eb4135848b2666f9262acdbd57e6af8

--- test/integ/KotlinLambdaSingletonRemoval.cpp
+++ test/integ/KotlinLambdaSingletonRemoval.cpp
@@ -49,29 +49,74 @@ class KotlinLambdaOptTest : public RedexIntegrationTest {
           << "SGET found in " << SHOW(code);
     }
   }
+  // Check that instructions for creating a new instance are used.
+  void check_removal_result_new_instance(const IRCode* code,
+                                         std::string_view lambda_name) {
+    const auto ii = InstructionIterable(code);
+    const auto new_instance_instruction = std::find_if(
+        ii.begin(), ii.end(), [lambda_name](const MethodItemEntry& mie) {
+          return mie.insn->opcode() == OPCODE_NEW_INSTANCE &&
+                 mie.insn->get_type() == DexType::get_type(lambda_name);
+        });
+    ASSERT_NE(new_instance_instruction, ii.end())
+        << "new-instance not found in " << SHOW(code);
+
+    const auto move_pseudo_result_instruction =
+        std::next(new_instance_instruction);
+    ASSERT_EQ(move_pseudo_result_instruction->insn->opcode(),
+              IOPCODE_MOVE_RESULT_PSEUDO_OBJECT)
+        << "move-result-pseudo-object not following new-instance in "
+        << SHOW(code);
+    const auto result_reg = move_pseudo_result_instruction->insn->dest();
+
+    const auto invoke_direct_instruction =
+        std::next(move_pseudo_result_instruction);
+    ASSERT_EQ(invoke_direct_instruction->insn->opcode(), OPCODE_INVOKE_DIRECT)
+        << "invoke-direct not following move-result-pseudo-object in "
+        << SHOW(code);
+    ASSERT_EQ(invoke_direct_instruction->insn->srcs_size(), 1u)
+        << "Sanity check: Unexpected number of sources for invoke-direct in "
+        << SHOW(code);
+    ASSERT_EQ(invoke_direct_instruction->insn->src(0), result_reg)
+        << "invoke-direct following move-result-pseudo-object does not apply "
+           "to the dest register of move-result-pseudo-object in "
+        << SHOW(code);
+    ASSERT_EQ(invoke_direct_instruction->insn->get_method()->get_class()->str(),
+              lambda_name)
+        << "invoke-direct following move-result-pseudo-object does not apply "
+           "to "
+        << lambda_name << " in " << SHOW(code);
+    ASSERT_EQ(invoke_direct_instruction->insn->get_method()->get_name()->str(),
+              "<init>")
+        << "invoke-direct following move-result-pseudo-object does not call "
+           "<init> in "
+        << SHOW(code);
+  }
 };
 
 TEST_F(KotlinLambdaOptTest, LambdaSingletonIsRemoved) {
   auto scope = build_class_scope(stores);
+  constexpr std::string_view lambda_class_name =
+      "LKotlinLambdaSingletonRemoval$foo$1;";
   constexpr std::string_view root_method_name =
       "LKotlinLambdaSingletonRemoval;.foo:()V";
   set_root_method(root_method_name);
 
-  auto* lambda_class =
-      type_class(DexType::make_type("LKotlinLambdaSingletonRemoval$foo$1;"));
+  auto* lambda_class = type_class(DexType::make_type(lambda_class_name));
   ASSERT_THAT(lambda_class, NotNull());
-  lambda_class->set_deobfuscated_name("LKotlinLambdaSingletonRemoval$foo$1;");
+  lambda_class->set_deobfuscated_name(lambda_class_name);
 
-  auto x_method = DexMethod::get_method(root_method_name)->as_def();
-  auto codex = x_method->get_code();
-  ASSERT_THAT(codex, NotNull());
-  check_sget_available(codex);
+  auto root_method = DexMethod::get_method(root_method_name)->as_def();
+  auto code_root = root_method->get_code();
+  ASSERT_THAT(code_root, NotNull());
+  check_sget_available(code_root);
 
   auto klr = new KotlinStatelessLambdaSingletonRemovalPass();
   std::vector<Pass*> passes{klr};
   run_passes(passes);
 
-  check_sget_not_available(codex);
+  check_sget_not_available(code_root);
+  check_removal_result_new_instance(code_root, lambda_class_name);
 }
 
 TEST_F(KotlinLambdaOptTest, NoEffectOnNamedClass) {
