8015ea31842feeaa2b88130cad4e9eeb167907db
Hong Xu <hongx@meta.com>
2025-08-21 13:52:32 -0700

    Promote "Update is_kotlin_lambda check"
    Summary: Promote {D79310074}.
    
    Reviewed By: thezhangwei
    
    Differential Revision: D80665653
    
    fbshipit-source-id: 4226cd27091c7cc3c9777d9bf9ac15ca4474b831

--- opt/kotlin-lambda/KotlinStatelessLambdaSingletonRemovalPass.cpp
+++ opt/kotlin-lambda/KotlinStatelessLambdaSingletonRemovalPass.cpp
@@ -7,7 +7,36 @@
 
 #include "KotlinStatelessLambdaSingletonRemovalPass.h"
 
+#include "DexUtil.h"
+#include "KotlinInstanceRewriter.h"
+#include "TypeUtil.h"
+
+namespace {
+
+bool is_kotlin_stateless_lambda(const DexClass* cls) {
+  return type::is_kotlin_non_capturing_lambda(cls);
+}
+
+} // namespace
+
 void KotlinStatelessLambdaSingletonRemovalPass::run_pass(
-    DexStoresVector& stores, ConfigFiles& conf, PassManager& mgr) {}
+    DexStoresVector& stores, ConfigFiles& conf, PassManager& mgr) {
+  Scope scope = build_class_scope(stores);
+  KotlinInstanceRewriter rewriter;
+  ConcurrentMap<DexFieldRef*, std::set<std::pair<IRInstruction*, DexMethod*>>>
+      concurrentLambdaMap;
+
+  auto do_not_consider_type = [](DexClass* cls) -> bool {
+    return !is_kotlin_stateless_lambda(cls);
+  };
+
+  // TODO: Update the rewriter logic
+  KotlinInstanceRewriter::Stats stats = rewriter.collect_instance_usage(
+      scope, concurrentLambdaMap, do_not_consider_type);
+
+  stats += rewriter.remove_escaping_instance(scope, concurrentLambdaMap);
+  stats += rewriter.transform(concurrentLambdaMap);
+  stats.report(mgr);
+}
 
 static KotlinStatelessLambdaSingletonRemovalPass s_pass;
