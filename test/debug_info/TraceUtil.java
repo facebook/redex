bab6b0dd1becd4bb170a8cdfbc0cf736f4ce8aa0
generatedunixname537391475639613 <noreply+537391475639613@fb.com>
2025-09-15 23:38:27 -0700

    fbandroid/native/redex/test/debug_info/TraceUtil.java
    Differential Revision: D82429449
    
    fbshipit-source-id: 7955fe289b23a8061d72a805246711271262828b

--- test/debug_info/TraceUtil.java
+++ test/debug_info/TraceUtil.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class TraceUtil {
   /** Stringifies and returns the top N elements of a stack trace. */
   public static ArrayList<String> traceToString(List<StackTraceElement> trace, int limit) {
-    ArrayList<String> result = new ArrayList();
+    ArrayList<String> result = new ArrayList<>();
     for (int i = 0; i < limit && i < trace.size(); ++i) {
       result.add(trace.get(i).toString().replace(":0", ""));
     }
