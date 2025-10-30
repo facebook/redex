ca3ed1a1e8dc0a26d87c923219f42be8bc199226
Patrick Yin <pty@meta.com>
2025-10-29 20:58:32 -0700

    Remove unused DexInstruction.h include from OptimizeEnumSwitchMapTestVerify.cpp
    Summary:
    Removed the unused `#include "DexInstruction.h"` from OptimizeEnumSwitchMapTestVerify.cpp as identified by the facebook-unused-include-check linter.
    
    The file only uses:
    - `DexMethod::get_method()` - provided by DexClass.h (included via VerifyUtil.h)
    - `find_class_named()` - provided by VerifyUtil.h
    - `collect_const_branch_cases()`, `BranchSource`, `BranchCase` - provided by OptimizeEnumCommon.h
    
    Since VerifyUtil.h already includes both DexClass.h and DexInstruction.h, all necessary symbols remain available transitively. The direct include of DexInstruction.h was redundant and served only as an umbrella header.
    
    This change reduces unnecessary dependencies and improves build times.
    
    [Session trajectory link](https://www.internalfb.com/intern/devai/devmate/inspector/?id=T243011211-60a83756-4c94-48f3-8fc4-9eae6af36427)
    
    Reviewed By: xuhdev
    
    Differential Revision: D85804879
    
    fbshipit-source-id: f9abca15f3ea673ffd1a2db70907effd6b90584f

--- test/instr/OptimizeEnumSwitchMapTestVerify.cpp
+++ test/instr/OptimizeEnumSwitchMapTestVerify.cpp
@@ -5,7 +5,6 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-#include "DexInstruction.h"
 #include "Show.h"
 #include "verify/OptimizeEnumCommon.h"
 #include "verify/VerifyUtil.h"
