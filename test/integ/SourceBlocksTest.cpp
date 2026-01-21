543a2c7ffb7ed237cd1c546b90d7121c28cf1fb8
Koby Chan <koby@meta.com>
2026-01-20 17:05:49 -0800

    Add test for hot_no_hot_pred counting violations
    Summary: Adds a test for making sure hot_no_hot_pred violations works with counting
    
    Reviewed By: xuhdev
    
    Differential Revision: D89509858
    
    fbshipit-source-id: f4338586fa249c602abace367fbc1561f46f6a99

--- test/integ/SourceBlocksTest.cpp
+++ test/integ/SourceBlocksTest.cpp
@@ -1129,6 +1129,33 @@ TEST_F(SourceBlocksTest, test_counting_with_idom_violations) {
   ASSERT_EQ(violations, 1);
 }
 
+TEST_F(SourceBlocksTest, test_counting_with_hot_no_hot_pred_violations) {
+  auto* profile_path = std::getenv("hot-no-hot-pred-counting-violations");
+  ASSERT_NE(profile_path, nullptr) << "Missing profile path.";
+
+  auto* type = DexType::get_type(
+      "Lcom/facebook/redextest/SourceBlocksTest$IDomBlockCounting;");
+  ASSERT_NE(type, nullptr);
+  auto* cls = type_class(type);
+  ASSERT_NE(cls, nullptr);
+
+  InsertSourceBlocksPass isbp{};
+  run_passes({&isbp}, nullptr, Json::nullValue, [&](const auto&) {
+    enable_pass(isbp);
+    set_insert_after_excs(isbp, false);
+    set_profile(isbp, profile_path);
+    set_force_serialize(isbp);
+  });
+
+  auto* method = cls->find_method_from_simple_deobfuscated_name("idom");
+  method->get_code()->build_cfg();
+  auto violations = source_blocks::compute(
+      source_blocks::ViolationsHelper::Violation::kHotNoHotPred,
+      method->get_code()->cfg());
+
+  ASSERT_EQ(violations, 2);
+}
+
 TEST_F(SourceBlocksTest, test_methods_with_intermethod_violations) {
   auto* profile_path = std::getenv("intermethod-violations");
   ASSERT_NE(profile_path, nullptr) << "Missing profile path.";
