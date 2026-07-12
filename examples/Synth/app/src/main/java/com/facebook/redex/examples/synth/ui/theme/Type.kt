b7eacfd475b5e03af6ced015e5670d86038040ed
Nivaldo Bondança <nbondanca@meta.com>
2026-07-12 14:57:47 -0700

    Update ktfmt component on FBS:master
    Reviewed By: cortinico
    
    Differential Revision: D110624565
    
    fbshipit-source-id: c18b2b2fc22a8b060a30ae9bd2bc18af862e08f2

--- examples/Synth/app/src/main/java/com/facebook/redex/examples/synth/ui/theme/Type.kt
+++ examples/Synth/app/src/main/java/com/facebook/redex/examples/synth/ui/theme/Type.kt
@@ -13,14 +13,13 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-val Typography =
-    Typography(
-        bodyLarge =
-            TextStyle(
-                fontFamily = FontFamily.Default,
-                fontWeight = FontWeight.Normal,
-                fontSize = 16.sp,
-                lineHeight = 24.sp,
-                letterSpacing = 0.5.sp,
-            )
-    )
+val Typography = Typography(
+    bodyLarge =
+        TextStyle(
+            fontFamily = FontFamily.Default,
+            fontWeight = FontWeight.Normal,
+            fontSize = 16.sp,
+            lineHeight = 24.sp,
+            letterSpacing = 0.5.sp,
+        )
+)
