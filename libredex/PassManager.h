6328c4f9d726afec811308212f28d9ef56c80f1d
Andreas Gampe <agampe@meta.com>
2026-08-14 22:24:37 -0700

    Promote "Move check_no_new_dex_features into RunPassesContext"
    Summary: Promote {D115482860}.
    
    Reviewed By: danjin250
    
    Differential Revision: D115979680
    
    fbshipit-source-id: a7971b160037fd3a01333d06a3e1e1b4581e89b0

--- libredex/PassManager.h
+++ libredex/PassManager.h
@@ -211,9 +211,6 @@ class PassManager {
   class RunPassesContext;
 
   void init(const ConfigFiles& config);
-  void check_no_new_dex_features(const DexStoresVector& stores,
-                                 const Pass* pass,
-                                 int check_against_version);
 
   void check_unreleased_reserved_refs();
 
@@ -227,9 +224,6 @@ class PassManager {
   PassInfo* m_current_pass_info;
 
   std::unique_ptr<const keep_rules::ProguardConfiguration> m_pg_config;
-  std::optional<bool> m_has_dex37_features;
-  std::optional<bool> m_has_dex38_features;
-  std::optional<bool> m_has_dex39_features;
   const RedexOptions m_redex_options;
   bool m_regalloc_has_run{false};
   bool m_nopper_has_run{false};
