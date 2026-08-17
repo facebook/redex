220e19ab0923bf249f15e552614ed8adb4cf6e7b
generatedunixname1247705480584755 <noreply+1247705480584755@fb.com>
2026-08-17 15:38:32 -0700

    Fix clang-tidy error in test
    Reviewed By: xuhdev
    
    Differential Revision: D116270704
    
    fbshipit-source-id: ce431ae19a3a783040f3044dec140f2b16847cba

--- test/unit/AtomicFieldUpdaterLoweringTest.cpp
+++ test/unit/AtomicFieldUpdaterLoweringTest.cpp
@@ -6,7 +6,6 @@
  */
 
 #include <gtest/gtest.h>
-#include <set>
 #include <string>
 
 #include "AtomicFieldUpdaterLoweringPass.h"
@@ -16,7 +15,6 @@
 #include "DexClass.h"
 #include "IRAssembler.h"
 #include "IRCode.h"
-#include "IROpcode.h"
 #include "IRTemplate.h"
 #include "PassManager.h"
 #include "RedexTest.h"
